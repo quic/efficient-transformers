18c973ca529dc474573d310d67e602b71a6f6ca0
Mamta Singh <168400541+quic-mamta@users.noreply.github.com>
2024-05-21 17:01:22 +0530

    Update execute.py
    Signed-off-by: Mamta Singh <168400541+quic-mamta@users.noreply.github.com>

--- QEfficient/cloud/execute.py
+++ QEfficient/cloud/execute.py
@@ -39,7 +39,7 @@ def main(
         login(hf_token)
 
     # Download tokenizer along with model if it doesn't exist
-    model_hf_path = hf_download(repo_id=model_name, cache_dir=cache_dir, allow_patterns=["*.json", "*.py"])
+    model_hf_path = hf_download(repo_id=model_name, cache_dir=cache_dir, allow_patterns=["*.json", "*.py", "*token*"])
     tokenizer = AutoTokenizer.from_pretrained(model_hf_path, use_cache=True, padding_side="left")
 
     # Execute
