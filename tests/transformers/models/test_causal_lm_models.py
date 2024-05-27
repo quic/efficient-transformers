c4f936c02b517023cc2beebfbd648f6aa48d1f85
Vinayak Baddi <quic_vbaddi@quicinc.com>
2024-05-27 13:58:30 +0530

    rename test names, and get the config params directly, modify json to include model names only
    Signed-off-by: vbaddi <vbaddi@qti.qualcomm.com>

--- tests/transformers/models/test_causal_lm_models.py
+++ tests/transformers/models/test_causal_lm_models.py
@@ -10,7 +10,7 @@ import os
 import unittest
 
 import pytest
-from transformers import AutoModelForCausalLM
+from transformers import AutoConfig, AutoModelForCausalLM
 
 from QEfficient.utils.constants import ROOT_DIR, Constants
 from QEfficient.utils.device_utils import get_available_device_id
@@ -23,23 +23,25 @@ def get_config(model_config):
     :param model_config: Dict containing model configuration
     :return model_config - Dict
     """
-    n_heads = model_config.get("n_head")
-    if n_heads is not None:  # Assuming n_head is a key in the config (GPTs/CodeGen)
-        d_head = model_config["n_embd"] // n_heads
-        model_config["model_class"] = AutoModelForCausalLM
-        model_config["padding_shape"] = [1, n_heads, Constants.CTX_LEN, d_head]
-    elif model_config.get("num_key_value_heads") is not None:  # Check for num_key_value_heads (Llama/Mistral)
-        n_heads = model_config["num_key_value_heads"]
-        d_head = model_config["hidden_size"] // model_config["num_attention_heads"]
-        model_config["model_class"] = AutoModelForCausalLM
-        model_config["padding_shape"] = [1, n_heads, Constants.CTX_LEN, d_head]
-    elif model_config.get("n_heads") is not None:  # Check for n_heads and d_model in the config (MPT Model)
-        n_heads = model_config["n_heads"]
-        d_head = model_config["d_model"] // n_heads
-        model_config["model_class"] = AutoModelForCausalLM
-        model_config["padding_shape"] = [1, n_heads, Constants.CTX_LEN, d_head]
+    config = AutoConfig.from_pretrained(model_config["model_name"])
+    if hasattr(config, "n_head"):  # Assuming n_head is a key in the config (GPTs/CodeGen)
+        n_heads = config.n_head
+        d_head = config.n_embd // config.n_head
+        n_layer = config.n_layer
+    elif hasattr(config, "num_key_value_heads"):  # Check for num_key_value_heads (Llama/Mistral)
+        n_heads = config.num_key_value_heads
+        d_head = config.hidden_size // config.num_attention_heads
+        n_layer = config.num_hidden_layers
+    elif hasattr(config, "n_heads"):  # Check for n_heads and d_model in the config (MPT Model)
+        n_heads = config.n_heads
+        d_head = config.d_model // config.n_heads
+        n_layer = config.n_layers
     else:
         raise ValueError("Invalid model configuration: n_head/n_heads or num_key_value_heads not found.")
+
+    model_config["n_layer"] = n_layer
+    model_config["model_class"] = AutoModelForCausalLM
+    model_config["padding_shape"] = [1, n_heads, Constants.CTX_LEN, d_head]
 
     return model_config
 
@@ -49,6 +51,7 @@ class TestQEfficientModels(unittest.TestCase):
     def setUpClass(cls):
         """
         Set up function to set up the test environment for TestQEfficientModels class
+        :param cls
         """
         cls.model_configs = []
         test_config_file_path = os.path.join(ROOT_DIR, "tests", "config.json")
@@ -62,6 +65,7 @@ class TestQEfficientModels(unittest.TestCase):
     def test_qefficient_model_torch(self):
         """
         Test function to validate the model before and after KV changes on Pytorch
+        :param None
         """
         for setup_info in self.setup_infos:
             assert (
@@ -71,6 +75,7 @@ class TestQEfficientModels(unittest.TestCase):
     def test_qefficient_model_onnx(self):
         """
         Test function to validate the model before and after KV changes on ONNXRT
+        :param None
         """
         for setup_info in self.setup_infos:
             assert (
@@ -81,6 +86,7 @@ class TestQEfficientModels(unittest.TestCase):
     def test_qefficient_model_cloud_ai_100(self):
         """
         Test function to validate the model before and after KV changes on Cloud AI 100
+        :param None
         """
         cloud_ai_100_tokens_list = [get_cloud_ai_100_tokens(setup_info) for setup_info in self.setup_infos]
         for cloud_ai_100_tokens, setup_info in zip(cloud_ai_100_tokens_list, self.setup_infos):
