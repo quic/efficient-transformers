0f09eedaf49e9da2812a34b6d9f1d803c8cc3ba8
irajagop <quic_irajagop@quicinc.com>
2024-06-24 17:41:14 +0530

    Moving project metadata to pyproject.toml (#50)
    * Move project metadata to pyproject.toml
    
    - Use __init__.py instead of version.py, to QEfficient.__version__ can
      be read after import QEfficient.
    - Minimal setup.py only to read version
    - Move config from setup.cfg to pyproject.toml
    - Cleanup unused Makefile
    
    Signed-off-by: Ilango Rajagopal <quic_irajagop@quicinc.com>
    
    * Fix whitespaces in pyproject.toml
    
    Signed-off-by: Ilango Rajagopal <quic_irajagop@quicinc.com>
    
    * Fix pyproject.toml errors
    
    Signed-off-by: Ilango Rajagopal <quic_irajagop@quicinc.com>
    
    * Fix CONTRIBUTING.md to use main instead of master
    
    Signed-off-by: Ilango Rajagopal <quic_irajagop@quicinc.com>
    
    * Fix torch version and add python version 3.8
    
    Signed-off-by: Ilango Rajagopal <quic_irajagop@quicinc.com>
    
    * Use torch cpu version
    
    Signed-off-by: Ilango Rajagopal <quic_irajagop@quicinc.com>
    
    * fix: install [test] extra dependencies in CI
    
    Signed-off-by: Ilango Rajagopal <quic_irajagop@quicinc.com>
    
    * modify the CODEOWNERS to point to *.toml
    
    Signed-off-by: vbaddi <quic_vbaddi@quicinc.com>
    
    ---------
    
    Signed-off-by: Ilango Rajagopal <quic_irajagop@quicinc.com>
    Co-authored-by: Vinayak Baddi <quic_vbaddi@quicinc.com>
    
    
    Signed-off-by: root <root@blr-ubuntu-912.qualcomm.com>

--- setup.py
+++ setup.py
@@ -7,71 +7,11 @@
 
 import re
 
-from setuptools import find_namespace_packages, setup
+from setuptools import setup
 
-# Ensure we match the version set in QEfficient//version.py
-try:
-    filepath = "QEfficient/version.py"
-    with open(filepath) as version_file:
-        (__version__,) = re.findall('__version__ = "(.*)"', version_file.read())
-except Exception as error:
-    assert False, "Error: Could not open '%s' due %s\n" % (filepath, error)
+# Ensure we match the version set in QEfficient/__init__.py
+filepath = "QEfficient/__init__.py"
+with open(filepath) as version_file:
+    (version,) = re.findall('__version__ = "(.*)"', version_file.read())
 
-INSTALL_REQUIRES = [
-    "transformers==4.41.2",
-    "torch@https://download.pytorch.org/whl/cpu/torch-2.0.0%2Bcpu-cp38-cp38-linux_x86_64.whl#sha256=354f281351cddb590990089eced60f866726415f7b287db5105514aa3c5f71ca",
-    "datasets==2.7.0",
-    "fsspec==2023.6.0",
-    "multidict==6.0.4",
-    "urllib3<2",
-    "sentencepiece==0.1.98",
-    "onnx==1.16.0",
-    "onnxruntime==1.16.3",
-    "numpy==1.23.0",
-    "protobuf==3.20.2",
-    "onnxscript==0.1.0.dev20240327",
-    "pytest",
-    "sympy",
-]
-
-QUALITY_REQUIRES = [
-    "black",
-    "ruff",
-    "hf_doc_builder @ git+https://github.com/huggingface/doc-builder.git",
-]
-
-EXTRAS_REQUIRE = {
-    "quality": QUALITY_REQUIRES,
-}
-
-setup(
-    name="QEfficient Library",
-    version=__version__,
-    description=(
-        "QEfficient is the library interface for the Hugging Face Transformer"
-        "models for efficient inference on Qualcomm Cloud AI 100"
-    ),
-    long_description=open("README.md", "r", encoding="utf-8").read(),
-    long_description_content_type="text/markdown",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Development Status :: 5 - Development/Unstable",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Education",
-        "Operating System :: Linux",
-        "Programming Language :: Python :: 3.8",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence for Inference Accelerator",
-    ],
-    keywords="transformers, Cloud AI 100, Inference",
-    author="Qualcomm Cloud AI ML Team",
-    license="BSD-3-Clause",
-    packages=find_namespace_packages(include=["QEfficient*"]),
-    install_requires=INSTALL_REQUIRES,
-    extras_require=EXTRAS_REQUIRE,
-    include_package_data=True,
-    zip_safe=False,
-    dependency_links=[
-        "https://download.pytorch.org/whl/torch_stable.html",
-        "https://download.pytorch.org/whl/cpu",
-    ],
-)
+setup(version=version)
