70f9b7745a2883609e95ffd125e8c49d200219a6
Ilango Rajagopal <quic_irajagop@quicinc.com>
2024-06-26 18:57:57 +0530

    Lint & Format
    - Added linting and formatting github actions
    - Formatted entire codebase
    - Fixed linter errors
    - Removed `# noqa` with fix
    
    Signed-off-by: Ilango Rajagopal <quic_irajagop@quicinc.com>

--- QEfficient/cloud/export.py
+++ QEfficient/cloud/export.py
@@ -19,7 +19,13 @@ from QEfficient.utils.logging_utils import logger
 ROOT_DIR = os.path.dirname(os.path.abspath(""))
 
 
-def get_onnx_model_path(model_name: str, cache_dir: Optional[str] = None, tokenizer: Optional[Union[PreTrainedTokenizerFast, PreTrainedTokenizer]]=None, hf_token: Optional[str] = None, local_model_dir: Optional[str] = None):
+def get_onnx_model_path(
+    model_name: str,
+    cache_dir: Optional[str] = None,
+    tokenizer: Optional[Union[PreTrainedTokenizerFast, PreTrainedTokenizer]] = None,
+    hf_token: Optional[str] = None,
+    local_model_dir: Optional[str] = None,
+):
     """
     exports the model to onnx if pre-exported file is not found and returns onnx_model_path
     """
@@ -33,19 +39,21 @@ def get_onnx_model_path(model_name: str, cache_dir: Optional[str] = None, tokeni
         # Export to the Onnx
         logger.info(f"Exporting Pytorch {model_name} model to ONNX...")
         _, generated_onnx_model_path = qualcomm_efficient_converter(
-                model_name=model_name,
-                local_model_dir=local_model_dir,
-                tokenizer=tokenizer,
-                onnx_dir_path=onnx_dir_path,
-                kv=True,
-                form_factor="cloud",
-                hf_token=hf_token,
-                cache_dir=cache_dir
-            ) # type: ignore
-        logger.info(f"Generated Onnx_path {generated_onnx_model_path} \nOnnx_model_path {onnx_model_path} \nand Onnx_dir_path is {onnx_dir_path}")
+            model_name=model_name,
+            local_model_dir=local_model_dir,
+            tokenizer=tokenizer,
+            onnx_dir_path=onnx_dir_path,
+            kv=True,
+            form_factor="cloud",
+            hf_token=hf_token,
+            cache_dir=cache_dir,
+        )  # type: ignore
+        logger.info(
+            f"Generated Onnx_path {generated_onnx_model_path} \nOnnx_model_path {onnx_model_path} \nand Onnx_dir_path is {onnx_dir_path}"
+        )
         assert (
-                generated_onnx_model_path == onnx_model_path
-            ), f"ONNX files were generated at an unusual location, expected {onnx_model_path}, got {generated_onnx_model_path}"
+            generated_onnx_model_path == onnx_model_path
+        ), f"ONNX files were generated at an unusual location, expected {onnx_model_path}, got {generated_onnx_model_path}"
     return onnx_model_path
 
 
@@ -63,14 +71,16 @@ def main(
     :hf_token: str. HuggingFace login token to access private repos.
     :local_model_dir: str. Path to custom model weights and config files.
     """
-    cache_dir = check_and_assign_cache_dir(local_model_dir,cache_dir)
+    cache_dir = check_and_assign_cache_dir(local_model_dir, cache_dir)
     get_onnx_model_path(model_name=model_name, cache_dir=cache_dir, hf_token=hf_token, local_model_dir=local_model_dir)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Export script.")
     parser.add_argument("--model_name", "--model-name", required=True, help="HF Model card name/id")
-    parser.add_argument("--local-model-dir", "--local_model_dir", required=False, help="Path to custom model weights and config files")
+    parser.add_argument(
+        "--local-model-dir", "--local_model_dir", required=False, help="Path to custom model weights and config files"
+    )
     parser.add_argument(
         "--cache_dir",
         "--cache-dir",
