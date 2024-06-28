dc37a3955a295a02e3d472c393564ddc662205b7
Onkar Chougule <quic_ochougul@quicinc.com>
2024-06-28 19:05:22 +0530

    added base directory in place of src
    Signed-off-by: Onkar Chougule <quic_ochougul@quicinc.com>

---
+++ QEfficient/base/__init__.py
@@ -0,0 +1,9 @@
+# -----------------------------------------------------------------------------
+#
+# Copyright (c)  2023-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# -----------------------------------------------------------------------------
+
+from QEfficient.transformers.models.modeling_auto import QEffAutoModel, QEFFAutoModelForCausalLM  # noqa: F401
+from QEfficient.base.common import QEFFCommonLoader  # noqa: F401
