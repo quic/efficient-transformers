7ffbbcb811df106f9abcbe73e9bddeb30f7c4a80
Vinayak Baddi <quic_vbaddi@quicinc.com>
2024-07-11 08:05:14 +0000

    add option to override the save folder using env variable
    Signed-off-by: vbaddi <quic_vbaddi@quicinc.com>

--- QEfficient/utils/constants.py
+++ QEfficient/utils/constants.py
@@ -1,6 +1,6 @@
 # -----------------------------------------------------------------------------
 #
-# Copyright (c)  2023-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+# Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
 # SPDX-License-Identifier: BSD-3-Clause
 #
 # -----------------------------------------------------------------------------
@@ -10,7 +10,7 @@ import os
 UTILS_DIR = os.path.dirname(os.path.abspath(__file__))
 QEFF_DIR = os.path.dirname(UTILS_DIR)
 ROOT_DIR = os.path.dirname(QEFF_DIR)
-QEFF_MODELS_DIR = os.path.join(ROOT_DIR, "qeff_models")
+QEFF_MODELS_DIR = os.environ.get("QEFF_MODELS_DIR", os.path.join(ROOT_DIR, "qeff_models"))
 
 
 class Constants:
