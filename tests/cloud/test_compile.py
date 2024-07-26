39a4ca5c0be7e33f8171e82788943e5f9eb7254e
Ilango Rajagopal <quic_irajagop@quicinc.com>
2024-07-26 12:23:12 +0000

    Fix compile API tests
    Signed-off-by: Ilango Rajagopal <quic_irajagop@quicinc.com>

--- tests/cloud/test_compile.py
+++ tests/cloud/test_compile.py
@@ -21,8 +21,11 @@ def test_compile(setup, mocker):
     mocker: mocker is itself a pytest fixture, uses to mock or spy internal functions.
     """
     ms = setup
+    for onnx_model_path in ms.onnx_model_path():
+        if os.path.isfile(onnx_model_path):
+            break
     QEfficient.compile(
-        onnx_path=ms.onnx_model_path(),
+        onnx_path=onnx_model_path,
         qpc_path=os.path.dirname(ms.qpc_dir_path()),
         num_cores=ms.num_cores,
         device_group=ms.device_group,
