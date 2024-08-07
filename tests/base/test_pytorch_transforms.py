ac378cd00af172503b3ea18bc34896c0edddc021
Onkar Chougule <quic_ochougul@quicinc.com>
2024-08-07 15:34:16 +0530

    Added tests for all other models tests by AbuKhoyer
    Signed-off-by: Onkar Chougule <quic_ochougul@quicinc.com>

--- tests/base/test_pytorch_transforms.py
+++ tests/base/test_pytorch_transforms.py
@@ -10,8 +10,18 @@ import random
 import pytest
 import torch
 from torch import nn
+from transformers.models.codegen.modeling_codegen import CodeGenConfig, CodeGenForCausalLM
+from transformers.models.falcon.modeling_falcon import FalconConfig, FalconForCausalLM
 from transformers.models.gpt2.modeling_gpt2 import GPT2Config, GPT2LMHeadModel
+from transformers.models.gptj.modeling_gptj import GPTJConfig, GPTJForCausalLM
 from transformers.models.llama.modeling_llama import LlamaConfig, LlamaForCausalLM
+from transformers.models.mistral.modeling_mistral import MistralConfig, MistralForCausalLM
+from transformers.models.mixtral.modeling_mixtral import MixtralConfig, MixtralForCausalLM
+from transformers.models.mpt.modeling_mpt import MptConfig, MptForCausalLM
+from transformers.models.phi.modeling_phi import PhiConfig, PhiForCausalLM
+from transformers.models.phi3.modeling_phi3 import Phi3Config, Phi3ForCausalLM
+from transformers.models.qwen2.modeling_qwen2 import Qwen2Config, Qwen2ForCausalLM
+from transformers.models.starcoder2.modeling_starcoder2 import Starcoder2Config, Starcoder2ForCausalLM
 
 from QEfficient.base.pytorch_transforms import ModuleMapping
 from QEfficient.transformers.pytorch_transforms import CustomOpsTransform, KVCacheTransform
@@ -198,3 +208,281 @@ def test_kv_cache_transform_gpt2(n_layer, n_embd, n_inner, n_head, ctx_len, inpu
     hf_model = GPT2LMHeadModel(config=config)
     hf_model.eval()
     run_kv_cache_transform_and_test(hf_model, n_layer, config.vocab_size, n_embd, n_head, n_head, ctx_len, input_len)
+
+
+@pytest.mark.parametrize("input_len", [8], ids=lambda x: "input_len=" + str(x))
+@pytest.mark.parametrize("n_embd", [1024], ids=lambda x: "n_embd=" + str(x))
+@pytest.mark.parametrize("n_inner", [2048], ids=lambda x: "n_inner=" + str(x))
+@pytest.mark.parametrize("n_head", [16], ids=lambda x: "n_head=" + str(x))
+@pytest.mark.parametrize("ctx_len", [32], ids=lambda x: "ctx_len=" + str(x))
+@pytest.mark.parametrize("n_layer", [1, 3], ids=lambda x: "n_layer=" + str(x))
+def test_kv_cache_transform_codegen(n_layer, n_embd, n_inner, n_head, ctx_len, input_len) -> None:
+    # Create small model
+    config = CodeGenConfig(
+        n_embd=n_embd,
+        n_inner=n_inner,
+        n_head=n_head,
+        n_layer=n_layer,
+        use_cache=True,
+    )
+    hf_model = CodeGenForCausalLM(config=config)
+    hf_model.eval()
+    run_kv_cache_transform_and_test(hf_model, n_layer, config.vocab_size, n_embd, n_head, n_head, ctx_len, input_len)
+
+
+
+@pytest.mark.parametrize("input_len", [8], ids=lambda x: "input_len=" + str(x))
+@pytest.mark.parametrize("hidden_size", [4544], ids=lambda x: "hidden_size=" + str(x))
+@pytest.mark.parametrize("num_hidden_layers", [1, 3], ids=lambda x: "num_hidden_layers=" + str(x))
+@pytest.mark.parametrize("num_attention_heads", [71], ids=lambda x: "num_attention_heads=" + str(x))
+@pytest.mark.parametrize("ctx_len", [32], ids=lambda x: "ctx_len=" + str(x))
+def test_kv_cache_transform_falcon(hidden_size, num_hidden_layers, num_attention_heads, ctx_len, input_len) -> None:
+    # Create small model
+    config = FalconConfig(
+        hidden_size=hidden_size,
+        num_hidden_layers=num_hidden_layers,
+        num_attention_heads=num_attention_heads,
+        use_cache=True,
+    )
+    hf_model = FalconForCausalLM(config=config)
+    hf_model.eval()
+    run_kv_cache_transform_and_test(hf_model, num_hidden_layers, config.vocab_size, hidden_size, num_attention_heads, num_attention_heads, ctx_len, input_len)
+
+
+
+@pytest.mark.parametrize("input_len", [8], ids=lambda x: "input_len=" + str(x))
+@pytest.mark.parametrize("n_embd", [4096], ids=lambda x: "n_embd=" + str(x))
+@pytest.mark.parametrize("n_inner", [512], ids=lambda x: "n_inner=" + str(x))
+@pytest.mark.parametrize("n_head", [16], ids=lambda x: "n_head=" + str(x))
+@pytest.mark.parametrize("ctx_len", [32], ids=lambda x: "ctx_len=" + str(x))
+@pytest.mark.parametrize("n_layer", [1, 3], ids=lambda x: "n_layer=" + str(x))
+def test_kv_cache_transform_gptj(n_layer, n_embd, n_inner, n_head, ctx_len, input_len) -> None:
+    # Create small model
+    config = GPTJConfig(
+        n_embd=n_embd,
+        n_inner=n_inner,
+        n_head=n_head,
+        n_layer=n_layer,
+        use_cache=True,
+    )
+    hf_model = GPTJForCausalLM(config=config)
+    hf_model.eval()
+    run_kv_cache_transform_and_test(hf_model, n_layer, config.vocab_size, n_embd, n_head, n_head, ctx_len, input_len)
+
+
+@pytest.mark.parametrize("input_len", [8], ids=lambda x: "input_len=" + str(x))
+@pytest.mark.parametrize("hidden_size", [128], ids=lambda x: "hidden_size=" + str(x))
+@pytest.mark.parametrize("intermediate_size", [512], ids=lambda x: "intermediate_size=" + str(x))
+@pytest.mark.parametrize("num_key_value_heads", [8, 32], ids=lambda x: "num_key_value_heads=" + str(x))
+@pytest.mark.parametrize("num_attention_heads", [32], ids=lambda x: "num_attention_heads=" + str(x))
+@pytest.mark.parametrize("ctx_len", [32], ids=lambda x: "ctx_len=" + str(x))
+@pytest.mark.parametrize("num_hidden_layers", [1, 3], ids=lambda x: "num_hidden_layers=" + str(x))
+def test_kv_cache_transform_mistral(
+    num_hidden_layers, hidden_size, intermediate_size, num_attention_heads, num_key_value_heads, ctx_len, input_len
+) -> None:
+    # Create small model
+    config = MistralConfig(
+        hidden_size=hidden_size,
+        intermediate_size=intermediate_size,
+        num_attention_heads=num_attention_heads,
+        num_key_value_heads=num_key_value_heads,
+        num_hidden_layers=num_hidden_layers,
+        use_cache=True,
+    )
+    hf_model = MistralForCausalLM(config=config)
+    hf_model.eval()
+    run_kv_cache_transform_and_test(
+        hf_model,
+        num_hidden_layers,
+        config.vocab_size,
+        hidden_size,
+        num_attention_heads,
+        num_key_value_heads,
+        ctx_len,
+        input_len,
+    )
+
+
+@pytest.mark.parametrize("input_len", [8], ids=lambda x: "input_len=" + str(x))
+@pytest.mark.parametrize("hidden_size", [128], ids=lambda x: "hidden_size=" + str(x))
+@pytest.mark.parametrize("intermediate_size", [512], ids=lambda x: "intermediate_size=" + str(x))
+@pytest.mark.parametrize("num_key_value_heads", [8, 32], ids=lambda x: "num_key_value_heads=" + str(x))
+@pytest.mark.parametrize("num_attention_heads", [32], ids=lambda x: "num_attention_heads=" + str(x))
+@pytest.mark.parametrize("ctx_len", [32], ids=lambda x: "ctx_len=" + str(x))
+@pytest.mark.parametrize("num_hidden_layers", [1, 3], ids=lambda x: "num_hidden_layers=" + str(x))
+def test_kv_cache_transform_mixtral(
+    num_hidden_layers, hidden_size, intermediate_size, num_attention_heads, num_key_value_heads, ctx_len, input_len
+) -> None:
+    # Create small model
+    config = MixtralConfig(
+        hidden_size=hidden_size,
+        intermediate_size=intermediate_size,
+        num_attention_heads=num_attention_heads,
+        num_key_value_heads=num_key_value_heads,
+        num_hidden_layers=num_hidden_layers,
+        use_cache=True,
+    )
+    hf_model = MixtralForCausalLM(config=config)
+    hf_model.eval()
+    run_kv_cache_transform_and_test(
+        hf_model,
+        num_hidden_layers,
+        config.vocab_size,
+        hidden_size,
+        num_attention_heads,
+        num_key_value_heads,
+        ctx_len,
+        input_len,
+    )
+
+
+@pytest.mark.parametrize("input_len", [8], ids=lambda x: "input_len=" + str(x))
+@pytest.mark.parametrize("d_model", [2048], ids=lambda x: "d_model=" + str(x))
+@pytest.mark.parametrize("n_heads", [16], ids=lambda x: "n_heads=" + str(x))
+@pytest.mark.parametrize("ctx_len", [32], ids=lambda x: "ctx_len=" + str(x))
+@pytest.mark.parametrize("n_layers", [1, 3], ids=lambda x: "n_layers=" + str(x))
+def test_kv_cache_transform_mpt(n_layers, d_model, n_heads, ctx_len, input_len) -> None:
+    # Create small model
+    config = MptConfig(
+        d_model=d_model,
+        n_heads=n_heads,
+        n_layers=n_layers,
+        use_cache=True,
+    )
+    hf_model = MptForCausalLM(config=config)
+    hf_model.eval()
+    run_kv_cache_transform_and_test(
+        hf_model, n_layers, config.vocab_size, d_model, n_heads, n_heads, ctx_len, input_len
+    )
+
+
+@pytest.mark.parametrize("input_len", [8], ids=lambda x: "input_len=" + str(x))
+@pytest.mark.parametrize("hidden_size", [128], ids=lambda x: "hidden_size=" + str(x))
+@pytest.mark.parametrize("intermediate_size", [512], ids=lambda x: "intermediate_size=" + str(x))
+@pytest.mark.parametrize("num_key_value_heads", [8, 32], ids=lambda x: "num_key_value_heads=" + str(x))
+@pytest.mark.parametrize("num_attention_heads", [32], ids=lambda x: "num_attention_heads=" + str(x))
+@pytest.mark.parametrize("ctx_len", [32], ids=lambda x: "ctx_len=" + str(x))
+@pytest.mark.parametrize("num_hidden_layers", [1, 3], ids=lambda x: "num_hidden_layers=" + str(x))
+def test_kv_cache_transform_phi(
+    num_hidden_layers, hidden_size, intermediate_size, num_attention_heads, num_key_value_heads, ctx_len, input_len
+) -> None:
+    # Create small model
+    config = PhiConfig(
+        hidden_size=hidden_size,
+        intermediate_size=intermediate_size,
+        num_attention_heads=num_attention_heads,
+        num_key_value_heads=num_key_value_heads,
+        num_hidden_layers=num_hidden_layers,
+        use_cache=True,
+    )
+    hf_model = PhiForCausalLM(config=config)
+    hf_model.eval()
+    run_kv_cache_transform_and_test(
+        hf_model,
+        num_hidden_layers,
+        config.vocab_size,
+        hidden_size,
+        num_attention_heads,
+        num_key_value_heads,
+        ctx_len,
+        input_len,
+    )
+
+
+@pytest.mark.parametrize("input_len", [8], ids=lambda x: "input_len=" + str(x))
+@pytest.mark.parametrize("hidden_size", [128], ids=lambda x: "hidden_size=" + str(x))
+@pytest.mark.parametrize("intermediate_size", [512], ids=lambda x: "intermediate_size=" + str(x))
+@pytest.mark.parametrize("num_key_value_heads", [8, 32], ids=lambda x: "num_key_value_heads=" + str(x))
+@pytest.mark.parametrize("num_attention_heads", [32], ids=lambda x: "num_attention_heads=" + str(x))
+@pytest.mark.parametrize("ctx_len", [32], ids=lambda x: "ctx_len=" + str(x))
+@pytest.mark.parametrize("num_hidden_layers", [1, 3], ids=lambda x: "num_hidden_layers=" + str(x))
+def test_kv_cache_transform_phi3(
+    num_hidden_layers, hidden_size, intermediate_size, num_attention_heads, num_key_value_heads, ctx_len, input_len
+) -> None:
+    # Create small model
+    config = Phi3Config(
+        hidden_size=hidden_size,
+        intermediate_size=intermediate_size,
+        num_attention_heads=num_attention_heads,
+        num_key_value_heads=num_key_value_heads,
+        num_hidden_layers=num_hidden_layers,
+        use_cache=True,
+    )
+    hf_model = Phi3ForCausalLM(config=config)
+    hf_model.eval()
+    run_kv_cache_transform_and_test(
+        hf_model,
+        num_hidden_layers,
+        config.vocab_size,
+        hidden_size,
+        num_attention_heads,
+        num_key_value_heads,
+        ctx_len,
+        input_len,
+    )
+
+
+@pytest.mark.parametrize("input_len", [8], ids=lambda x: "input_len=" + str(x))
+@pytest.mark.parametrize("hidden_size", [128], ids=lambda x: "hidden_size=" + str(x))
+@pytest.mark.parametrize("intermediate_size", [512], ids=lambda x: "intermediate_size=" + str(x))
+@pytest.mark.parametrize("num_key_value_heads", [8, 32], ids=lambda x: "num_key_value_heads=" + str(x))
+@pytest.mark.parametrize("num_attention_heads", [32], ids=lambda x: "num_attention_heads=" + str(x))
+@pytest.mark.parametrize("ctx_len", [32], ids=lambda x: "ctx_len=" + str(x))
+@pytest.mark.parametrize("num_hidden_layers", [1, 3], ids=lambda x: "num_hidden_layers=" + str(x))
+def test_kv_cache_transform_qwen2(
+    num_hidden_layers, hidden_size, intermediate_size, num_attention_heads, num_key_value_heads, ctx_len, input_len
+) -> None:
+    # Create small model
+    config = Qwen2Config(
+        hidden_size=hidden_size,
+        intermediate_size=intermediate_size,
+        num_attention_heads=num_attention_heads,
+        num_key_value_heads=num_key_value_heads,
+        num_hidden_layers=num_hidden_layers,
+        use_cache=True,
+    )
+    hf_model = Qwen2ForCausalLM(config=config)
+    hf_model.eval()
+    run_kv_cache_transform_and_test(
+        hf_model,
+        num_hidden_layers,
+        config.vocab_size,
+        hidden_size,
+        num_attention_heads,
+        num_key_value_heads,
+        ctx_len,
+        input_len,
+    )
+
+
+@pytest.mark.parametrize("input_len", [8], ids=lambda x: "input_len=" + str(x))
+@pytest.mark.parametrize("hidden_size", [128], ids=lambda x: "hidden_size=" + str(x))
+@pytest.mark.parametrize("intermediate_size", [512], ids=lambda x: "intermediate_size=" + str(x))
+@pytest.mark.parametrize("num_key_value_heads", [8, 16], ids=lambda x: "num_key_value_heads=" + str(x))
+@pytest.mark.parametrize("num_attention_heads", [16], ids=lambda x: "num_attention_heads=" + str(x))
+@pytest.mark.parametrize("ctx_len", [32], ids=lambda x: "ctx_len=" + str(x))
+@pytest.mark.parametrize("num_hidden_layers", [1, 3], ids=lambda x: "num_hidden_layers=" + str(x))
+def test_kv_cache_transform_starcoder2(
+    num_hidden_layers, hidden_size, intermediate_size, num_attention_heads, num_key_value_heads, ctx_len, input_len
+) -> None:
+    # Create small model
+    config = Starcoder2Config(
+        hidden_size=hidden_size,
+        intermediate_size=intermediate_size,
+        num_attention_heads=num_attention_heads,
+        num_key_value_heads=num_key_value_heads,
+        num_hidden_layers=num_hidden_layers,
+        use_cache=True,
+    )
+    hf_model = Starcoder2ForCausalLM(config=config)
+    hf_model.eval()
+    run_kv_cache_transform_and_test(
+        hf_model,
+        num_hidden_layers,
+        config.vocab_size,
+        hidden_size,
+        num_attention_heads,
+        num_key_value_heads,
+        ctx_len,
+        input_len,
+    )
