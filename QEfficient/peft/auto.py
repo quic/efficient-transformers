5dd060be2ec8c9ad0c235f77eed0e048ad301f75
Ilango Rajagopal <quic_irajagop@quicinc.com>
2024-08-13 20:28:31 +0000

    peft: `.compile`, `.run_pytorch` and `.run_ort`
    Signed-off-by: Ilango Rajagopal <quic_irajagop@quicinc.com>

--- QEfficient/peft/auto.py
+++ QEfficient/peft/auto.py
@@ -5,14 +5,18 @@
 #
 # ----------------------------------------------------------------------------
 
+import json
+import logging
 import os
 import shutil
+import subprocess
 import warnings
 from typing import Dict, List, Optional
 
 import numpy as np
 import onnx
 import torch
+from onnxruntime import InferenceSession as ORTInferenceSession
 from peft import AutoPeftModelForCausalLM
 from torch import nn
 
@@ -22,8 +26,10 @@ from QEfficient.base.pytorch_transforms import PytorchTransform
 from QEfficient.peft.onnx_transforms import AdaptersAsInputsTransform
 from QEfficient.peft.pytorch_transforms import PeftModelInputsTransform
 from QEfficient.transformers.pytorch_transforms import CustomOpsTransform, KVCacheTransform
-from QEfficient.utils._utils import get_num_layers_from_config, get_padding_shape_from_config
+from QEfficient.utils._utils import get_padding_shape_from_config
 from QEfficient.utils.cache_dir import QEFF_HOME
+
+logger = logging.getLogger(__name__)
 
 
 class QEffAutoPeftModelForCausalLM(QEFFBaseModel):
@@ -34,13 +40,14 @@ class QEffAutoPeftModelForCausalLM(QEFFBaseModel):
     def __init__(self, model: nn.Module, card_name: Optional[str] = None):
         super().__init__(model)
         self.card_name = card_name
+        self.num_layers = self.model.config.num_hidden_layers
         self.transform()
 
     @classmethod
     def from_pretrained(cls, pretrained_name_or_path: str, **kwargs):
         # Base class
         card_name = kwargs.pop("card_name", None)
-        if not kwargs.get("use_cache"):
+        if kwargs.get("use_cache") is False:
             warnings.warn("Overriding to use_cache=True")
         kwargs["use_cache"] = True
         model = cls._hf_auto_class.from_pretrained(pretrained_name_or_path, **kwargs)
@@ -52,13 +59,13 @@ class QEffAutoPeftModelForCausalLM(QEFFBaseModel):
 
     def transform(self, **kwargs):
         # Base class
+        logger.info("Pytorch layers are transformed")
         for transform in self.pytorch_transforms:
             self.model, transformed = transform.apply(self.model)
 
     @property
     def sample_inputs(self) -> Dict[str, torch.Tensor]:
         kv_cache_shape = get_padding_shape_from_config(self.model.config, 1, 32)
-        num_layers = get_num_layers_from_config(self.model.config)
         inputs = {
             "input_ids": torch.zeros((1, 32), dtype=torch.int64),
             "position_ids": torch.arange(32, dtype=torch.int64).view((1, 32)),
@@ -67,19 +74,18 @@ class QEffAutoPeftModelForCausalLM(QEFFBaseModel):
                     torch.zeros(kv_cache_shape, dtype=torch.float32),
                     torch.zeros(kv_cache_shape, dtype=torch.float32),
                 )
-                for _ in range(num_layers)
+                for _ in range(self.num_layers)
             ],
         }
         return inputs
 
     @property
     def dynamic_axes(self) -> Dict[str, Dict[int, str]]:
-        num_layers = get_num_layers_from_config(self.model.config)
         dynamic_axes = {
             "input_ids": {0: "batch_size", 1: "seq_len"},
             "position_ids": {0: "batch_size", 1: "seq_len"},
         }
-        for i in range(num_layers):
+        for i in range(self.num_layers):
             dynamic_axes[f"past_key.{i}"] = {0: "batch_size", 2: "ctx_len"}
             dynamic_axes[f"past_value.{i}"] = {0: "batch_size", 2: "ctx_len"}
         return dynamic_axes
@@ -90,9 +96,8 @@ class QEffAutoPeftModelForCausalLM(QEFFBaseModel):
 
     @property
     def output_names(self) -> List[str]:
-        num_layers = get_num_layers_from_config(self.model.config)
         outputs = ["logits"]
-        for i in range(num_layers):
+        for i in range(self.num_layers):
             outputs.append(f"past_key.{i}_RetainedState")
             outputs.append(f"past_value.{i}_RetainedState")
         return outputs
@@ -100,8 +105,7 @@ class QEffAutoPeftModelForCausalLM(QEFFBaseModel):
     def export(self, export_dir: Optional[str] = None) -> str:
         # Base class
         model_name = self.card_name.replace("/", "_")
-        if export_dir is None:
-            export_dir = os.path.join(QEFF_HOME, model_name)
+        export_dir = export_dir or os.path.join(QEFF_HOME, model_name)
         self.onnx_path = os.path.join(export_dir, f"{model_name}.onnx")
         tmp_onnx_dir = os.path.join(export_dir, "onnx_tmp")
         tmp_onnx_path = os.path.join(tmp_onnx_dir, f"{model_name}.onnx")
@@ -113,7 +117,9 @@ class QEffAutoPeftModelForCausalLM(QEFFBaseModel):
             input_names=self.input_names,
             output_names=self.output_names,
             dynamic_axes=self.dynamic_axes,
+            opset_version=13,
         )
+        logger.info("Pytorch export successful")
 
         model = onnx.load(tmp_onnx_path, load_external_data=False)
         transform_kwargs = {
@@ -123,22 +129,117 @@ class QEffAutoPeftModelForCausalLM(QEFFBaseModel):
         }
         for transform in self.onnx_transforms:
             model, transformed = transform.apply(model, **transform_kwargs)
-        onnx.save(model, self.onnx_path)
+        logger.info("ONNX transforms applied")
 
+        onnx.save(model, self.onnx_path)
         shutil.rmtree(tmp_onnx_dir)
+        logger.info("Transformed onnx saved")
+
         return self.onnx_path
 
-    def compile(self, **kwargs) -> str:
+    def _compile(self, onnx_path, **kwargs):
         # Base class
-        pass
+        command = ["/opt/qti-aic/exec/qaic-exec", f"-m={onnx_path}", "-aic-hw", "-aic-hw-version=2.0"]
+        for key, value in kwargs.items():
+            option = "-" + key.replace("_", "-")
+            if isinstance(value, bool):
+                if value:
+                    command.append(option)
+                continue
+            command.append(f"{option}={value}")
+        logger.info(f"Running compiler: {command}")
+        subprocess.run(command).check_returncode()
+
+    def compile(
+        self,
+        *,
+        batch_size: int = 1,
+        prefill_seq_len: int,
+        ctx_len: int,
+        onnx_path: Optional[str] = None,
+        binary_path: Optional[str] = None,
+        num_devices: int = 1,
+        num_cores: int = 16,
+        mxfp6_matmul: bool = False,
+        mxint8_kv_cache: bool = False,
+        **compiler_options,
+    ) -> str:
+        model_name = self.card_name.replace("/", "_")
+        model_dir = os.path.join(QEFF_HOME, model_name)
+        onnx_path = onnx_path or self.onnx_path
+        binary_path = binary_path or os.path.join(model_dir, "qpc")
+
+        # Specializations
+        specializations_json = os.path.join(model_dir, "specializations.json")
+        with open(specializations_json, "w") as fp:
+            json.dump(
+                {
+                    "specializations": [
+                        {"batch_size": str(batch_size), "seq_len": str(prefill_seq_len), "ctx_len": str(ctx_len)},
+                        {"batch_size": str(batch_size), "seq_len": "1", "ctx_len": str(ctx_len)},
+                    ]
+                },
+                fp,
+                indent=4,
+            )
+
+        # Custom IO
+        kv_cache_dtype = "mxint8" if mxint8_kv_cache else "float16"
+        custom_io_yaml = os.path.join(model_dir, f"custom_io_{kv_cache_dtype}.yaml")
+        with open(custom_io_yaml, "w") as fp:
+            for suffix in ["", "_RetainedState"]:
+                for i in range(self.num_layers):
+                    for kv in ["key", "value"]:
+                        fp.write(f" - IOName: past_{kv}.{i}{suffix}\n   Precision: {kv_cache_dtype}\n\n")
+
+        # MDP
+        if num_devices > 1:
+            mdp_ts_json = os.path.join(model_dir, f"mdp_ts_{num_devices}.json")
+            with open(mdp_ts_json, "w") as fp:
+                json.dump(
+                    {
+                        "connections": [{"devices": list(range(num_devices)), "type": "p2p"}],
+                        "partitions": [
+                            {
+                                "name": "Partition0",
+                                "devices": [{"deviceId": d, "numCores": num_cores} for d in range(num_devices)],
+                            }
+                        ],
+                    },
+                    fp,
+                    indent=4,
+                )
+            compiler_options["mdp_load_partition_config"] = mdp_ts_json
+
+        self._run_compiler(
+            onnx_path,
+            network_specialization_config=specializations_json,
+            compile_only=True,
+            aic_binary_dir=binary_path,
+            convert_to_fp16=True,
+            mxfp6_matmul=mxfp6_matmul,
+            custom_IO_list_file=custom_io_yaml,
+            retained_state=True,
+            aic_num_cores=num_cores,
+            **compiler_options,
+        )
+
+        self.binary_path = binary_path
 
     def run_pytorch(self, inputs: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
         # Base class
-        return dict(self.model(inputs))
+        with torch.inference_mode():
+            outputs = self.model(**inputs)
+        return dict(outputs)
 
     def run_ort(self, inputs: Dict[str, np.ndarray]) -> Dict[str, np.ndarray]:
         # Base class
-        pass
+        if not getattr(self, "ort_session", None):
+            if self.onnx_path is None:
+                self.export()
+            self.ort_session = ORTInferenceSession(self.onnx_path)
+        outputs = self.ort_session.run(None, inputs)
+        return dict(zip(self.output_names, outputs))
 
     def run_cloud_ai_100(self, inputs: Dict[str, np.ndarray]) -> Dict[str, np.ndarray]:
         # Base class
