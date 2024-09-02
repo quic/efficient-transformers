76e8e085d8225175cbd922c6580dad36b1408904
Ann Kuruvilla <quic_akuruvil@quicinc.com>
2024-09-02 05:22:20 +0000

    Rebased with main
    Signed-off-by: Ann <quic_akuruvil@quicinc.com>

--- QEfficient/customop/__init__.py
+++ QEfficient/customop/__init__.py
@@ -5,15 +5,16 @@
 #
 # -----------------------------------------------------------------------------
 
-<<<<<<< HEAD
-from QEfficient.customop.ctx_scatter_gather import CtxGatherFunc, CtxScatterFunc
+from QEfficient.customop.ctx_scatter_gather import CtxGatherFunc, CtxGatherFunc3D, CtxScatterFunc, CtxScatterFunc3D
 from QEfficient.customop.ctx_scatter_gather_cb import CtxGatherFuncCB, CtxScatterFuncCB
 from QEfficient.customop.rms_norm import CustomRMSNormAIC
 
-__all__ = ["CtxGatherFunc", "CtxScatterFunc", "CustomRMSNormAIC", "CtxGatherFuncCB", "CtxScatterFuncCB"]
-=======
-from QEfficient.customop.ctx_scatter_gather import CtxGatherFunc, CtxGatherFunc3D, CtxScatterFunc, CtxScatterFunc3D
-from QEfficient.customop.rms_norm import CustomRMSNormAIC
-
-__all__ = ["CtxGatherFunc", "CtxScatterFunc", "CtxGatherFunc3D", "CtxScatterFunc3D", "CustomRMSNormAIC"]
->>>>>>> d8ce332 (Add support for model granite-Starcoder1 arch)
+__all__ = [
+    "CtxGatherFunc",
+    "CtxScatterFunc",
+    "CtxGatherFunc3D",
+    "CtxScatterFunc3D",
+    "CustomRMSNormAIC",
+    "CtxGatherFuncCB",
+    "CtxScatterFuncCB",
+]
