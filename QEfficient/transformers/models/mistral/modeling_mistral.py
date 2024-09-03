5f6298b3136a05c03ec40a960c22dfa7ba163564
Vinayak Baddi <quic_vbaddi@quicinc.com>
2024-09-03 07:51:57 +0000

    remove bool from the attention_mask
    Signed-off-by: vbaddi <quic_vbaddi@quicinc.com>

--- QEfficient/transformers/models/mistral/modeling_mistral.py
+++ QEfficient/transformers/models/mistral/modeling_mistral.py
@@ -170,7 +170,7 @@ class QEffMistralAttention(MistralAttention):
         attn_weights = torch.matmul(query_states, key_states.transpose(2, 3)) / math.sqrt(self.head_dim)
 
         if attention_mask is not None:  # no matter the length, we just slice it
-            attn_weights = torch.where(attention_mask.bool(), torch.tensor(-10000.0, dtype=torch.float32), attn_weights)
+            attn_weights = torch.where(attention_mask, torch.tensor(-10000.0, dtype=torch.float32), attn_weights)
 
         # upcast attention to fp32
         attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
