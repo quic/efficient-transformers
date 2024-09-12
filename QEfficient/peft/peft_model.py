6cf39a6471e5da7cbaacf6f963ed45a622a6b8b2
Ilango Rajagopal <quic_irajagop@quicinc.com>
2024-09-12 16:06:35 +0530

    Peft Lora implementation (#85)
    AutoPeftModelForCausalLM for loading LoRA models
    
    Better export code that can be utilized for other auto classes
    Hashing model cache location to avoid exporting again
    Hashing model compile location to avoid compiling again
    
    ---------
    
    Signed-off-by: Ilango Rajagopal <quic_irajagop@quicinc.com>
    Signed-off-by: Amit Raj <quic_amitraj@quicinc.com>

---
+++ QEfficient/peft/peft_model.py
@@ -0,0 +1,61 @@
+# -----------------------------------------------------------------------------
+#
+# Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# ----------------------------------------------------------------------------
+
+from peft import PeftModelForCausalLM, PeftType
+
+
+class QEffPeftModelForCausalLM(PeftModelForCausalLM):
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        position_ids=None,
+        past_key_values=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+        task_ids=None,
+        **kwargs,
+    ):
+        peft_config = self.active_peft_config
+        if not peft_config.is_prompt_learning:
+            if self.base_model.config.model_type == "mpt":
+                if inputs_embeds is not None:
+                    raise AssertionError("forward in MPTForCausalLM does not support inputs_embeds")
+                return self.base_model(
+                    input_ids=input_ids,
+                    attention_mask=attention_mask,
+                    position_ids=position_ids,
+                    past_key_values=past_key_values,
+                    labels=labels,
+                    output_attentions=output_attentions,
+                    output_hidden_states=output_hidden_states,
+                    return_dict=return_dict,
+                    **kwargs,
+                )
+
+            if peft_config.peft_type == PeftType.POLY:
+                kwargs["task_ids"] = task_ids
+
+            with self._enable_peft_forward_hooks(**kwargs):
+                kwargs = {k: v for k, v in kwargs.items() if k not in self.special_peft_forward_args}
+                return self.base_model(
+                    input_ids=input_ids,
+                    attention_mask=attention_mask,
+                    position_ids=position_ids,
+                    past_key_values=past_key_values,
+                    inputs_embeds=inputs_embeds,
+                    labels=labels,
+                    output_attentions=output_attentions,
+                    output_hidden_states=output_hidden_states,
+                    return_dict=return_dict,
+                    **kwargs,
+                )
+
+        raise NotImplementedError("Prompt learning methods are not supported from QEfficient")
