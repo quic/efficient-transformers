b745d2db38659885a6c6e2ded5dc6fcdb191bfa5
Onkar Chougule <168134249+ochougul@users.noreply.github.com>
2024-09-14 00:23:01 +0530

    AWQ+GPTQ (#101)
    * Awq feature (#100)
    
    * added preprocess layer before loading quantized awq weights
    
    Signed-off-by: Onkar Chougule <quic_ochougul@quicinc.com>
    
    * added onnx export
    
    Signed-off-by: Onkar Chougule <quic_ochougul@quicinc.com>
    
    * added ScaledActivation class
    
    Signed-off-by: Onkar Chougule <quic_ochougul@quicinc.com>
    
    * refactoring the code to right places and added one single test for now
    
    Signed-off-by: Onkar Chougule <quic_ochougul@quicinc.com>
    
    * cleaned code
    
    Signed-off-by: Onkar Chougule <quic_ochougul@quicinc.com>
    
    * added proper tests, added decorator for updating quantizers, cleaned code
    
    Signed-off-by: Onkar Chougule <quic_ochougul@quicinc.com>
    
    * fixed CLI
    
    Signed-off-by: Onkar Chougule <quic_ochougul@quicinc.com>
    
    * added auto file for decorator
    
    Signed-off-by: Onkar Chougule <quic_ochougul@quicinc.com>
    
    ---------
    
    Signed-off-by: Onkar Chougule <quic_ochougul@quicinc.com>
    
    * bugfix for tests
    
    Signed-off-by: Onkar Chougule <quic_ochougul@quicinc.com>
    
    * fixed tests for AWQ model
    
    Signed-off-by: Onkar Chougule <quic_ochougul@quicinc.com>
    
    * Adding support for GPTQ models (#103)
    
    * Adding support for gptq models
    
    Signed-off-by: Amit Raj <quic_amitraj@quicinc.com>
    
    * Code cleaning and formating
    
    Signed-off-by: Amit Raj <quic_amitraj@quicinc.com>
    
    * ruff format and fixed some bug
    
    Signed-off-by: Amit Raj <quic_amitraj@quicinc.com>
    
    * Added tests for gptq
    
    Signed-off-by: Amit Raj <quic_amitraj@quicinc.com>
    
    * Bug-fix-1
    
    Signed-off-by: Amit Raj <quic_amitraj@quicinc.com>
    
    * fixed bugs-2
    
    Signed-off-by: Amit Raj <quic_amitraj@quicinc.com>
    
    * fixed bug-3
    
    Signed-off-by: Amit Raj <quic_amitraj@quicinc.com>
    
    * Added docstring
    
    Signed-off-by: Amit Raj <quic_amitraj@quicinc.com>
    
    * Addressed comments
    
    Signed-off-by: Amit Raj <quic_amitraj@quicinc.com>
    
    * Addressed comments
    
    Signed-off-by: Amit Raj <quic_amitraj@quicinc.com>
    
    * fixed bugs-3
    
    Signed-off-by: Amit Raj <quic_amitraj@quicinc.com>
    
    * ruff check and format
    
    Signed-off-by: Amit Raj <quic_amitraj@quicinc.com>
    
    * Addressed comments-3
    
    Signed-off-by: Amit Raj <quic_amitraj@quicinc.com>
    
    ---------
    
    Signed-off-by: Amit Raj <quic_amitraj@quicinc.com>
    Signed-off-by: Onkar Chougule <quic_ochougul@quicinc.com>
    
    * added liscence at top for missing file
    
    Signed-off-by: Onkar Chougule <quic_ochougul@quicinc.com>
    
    * added export_and_compile and fixed bugs
    
    Signed-off-by: Onkar Chougule <quic_ochougul@quicinc.com>
    
    * removed GPTQ test
    
    Signed-off-by: Onkar Chougule <quic_ochougul@quicinc.com>
    
    * removed threading from pytest
    
    Signed-off-by: Onkar Chougule <quic_ochougul@quicinc.com>
    
    ---------
    
    Signed-off-by: Onkar Chougule <quic_ochougul@quicinc.com>
    Signed-off-by: Amit Raj <quic_amitraj@quicinc.com>
    Co-authored-by: Amit Raj <168538872+quic-amitraj@users.noreply.github.com>
    Signed-off-by: Amit Raj <quic_amitraj@quicinc.com>

---
+++ QEfficient/transformers/quantizers/auto.py
@@ -0,0 +1,41 @@
+# -----------------------------------------------------------------------------
+#
+# Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# ----------------------------------------------------------------------------
+
+from transformers.quantizers.auto import AUTO_QUANTIZATION_CONFIG_MAPPING, AUTO_QUANTIZER_MAPPING
+
+from QEfficient.transformers.quantizers.quantizer_awq import QEffAwqConfig, QEffAwqQuantizer
+from QEfficient.transformers.quantizers.quantizer_gptq import QEffGPTQConfig, QEffGPTQQuantizer
+
+QEFF_AUTO_QUANTIZER_MAPPING = {"awq": QEffAwqQuantizer, "gptq": QEffGPTQQuantizer}
+QEFF_AUTO_QUANTIZATION_CONFIG_MAPPING = {"awq": QEffAwqConfig, "gptq": QEffGPTQConfig}
+
+
+def with_replaced_quantizers(func):
+    def wrapper(*args, **kwargs):
+        transformers_replaced_quantization_config_mapping = dict()
+        transformers_replaced_quantizer_mapping = dict()
+
+        for k in QEFF_AUTO_QUANTIZATION_CONFIG_MAPPING.keys():
+            # Replace quantization config
+            transformers_replaced_quantization_config_mapping[k] = AUTO_QUANTIZATION_CONFIG_MAPPING[k]
+            AUTO_QUANTIZATION_CONFIG_MAPPING[k] = QEFF_AUTO_QUANTIZATION_CONFIG_MAPPING[k]
+
+            # Replace quantizer
+            transformers_replaced_quantizer_mapping[k] = AUTO_QUANTIZER_MAPPING[k]
+            AUTO_QUANTIZER_MAPPING[k] = QEFF_AUTO_QUANTIZER_MAPPING[k]
+
+        # Call the function for loading quantized models here
+        out = func(*args, **kwargs)
+
+        # Put back quantization config and quantizer
+        for k in QEFF_AUTO_QUANTIZATION_CONFIG_MAPPING.keys():
+            AUTO_QUANTIZATION_CONFIG_MAPPING[k] = transformers_replaced_quantization_config_mapping[k]
+            AUTO_QUANTIZER_MAPPING[k] = transformers_replaced_quantizer_mapping[k]
+
+        return out
+
+    return wrapper
---
+++ QEfficient/transformers/quantizers/quantizer_awq.py
@@ -0,0 +1,84 @@
+# -----------------------------------------------------------------------------
+#
+# Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# -----------------------------------------------------------------------------
+
+import torch
+from transformers.quantizers.quantizer_awq import AwqQuantizer
+from transformers.utils.quantization_config import AwqBackendPackingMethod, AwqConfig, AWQLinearVersion
+
+from QEfficient.transformers.quantizers.awq import WQLinear_GEMM
+from QEfficient.transformers.quantizers.quantizer_utils import (
+    get_keys_to_not_convert,
+    replace_linear_layer_with_target_layer,
+    replace_quantization_scales,
+)
+from QEfficient.utils.logging_utils import logger
+
+
+class QEffAwqConfig(AwqConfig):
+    def post_init(self):
+        """
+        Safety checker that arguments are correct
+        """
+
+        if self.backend not in [AwqBackendPackingMethod.AUTOAWQ]:
+            raise ValueError(
+                f"Only quantization backend {AwqBackendPackingMethod.AUTOAWQ} is supported - not recognized backend {self.backend}"
+            )
+
+        self.version = AWQLinearVersion.from_str(self.version)
+        if self.version not in [AWQLinearVersion.GEMM]:
+            raise ValueError(
+                f"Only {AWQLinearVersion.GEMM} version in supported - not recognized version {self.version}"
+            )
+
+        if self.do_fuse or self.fuse_max_seq_len is not None:
+            raise ValueError(
+                f"fused modules are not supported, got do_fuse={self.do_fuse}, fuse_max_seq_len={self.fuse_max_seq_len}"
+            )
+
+        if self.bits != 4:
+            raise ValueError(f"Only 4-bit AWQ quantization is supported, got bits={self.bits}")
+
+
+class QEffAwqQuantizer(AwqQuantizer):
+    target_cls = WQLinear_GEMM
+
+    def __init__(self, quantization_config: QEffAwqConfig, **kwargs):
+        super().__init__(quantization_config, **kwargs)
+
+    def validate_environment(self, device_map, **kwargs):
+        # No need to validate as we will always use pytorch CPU version.
+        return True
+
+    @property
+    def is_trainable(self):
+        return False
+
+    def update_torch_dtype(self, torch_dtype):
+        if torch_dtype not in [None, torch.float32]:
+            logger.warning(f"Requested dtype {torch_dtype} is not supported, overriding to None")
+        return None
+
+    def _process_model_before_weight_loading(self, model, **kwargs):
+        self.modules_to_not_convert = get_keys_to_not_convert(model)
+
+        if self.quantization_config.modules_to_not_convert is not None:
+            self.modules_to_not_convert.extend(self.quantization_config.modules_to_not_convert)
+
+        model, has_been_replaced = replace_linear_layer_with_target_layer(
+            model,
+            target_cls=self.target_cls,
+            quantization_config=self.quantization_config,
+            modules_to_not_convert=self.modules_to_not_convert,
+        )
+
+        model = replace_quantization_scales(model, model.config.model_type)
+        if not has_been_replaced:
+            logger.warning(
+                "You are loading an AWQ model but no linear modules were found in your model."
+                " Please double check your model architecture, or submit an issue on github if you think this is a bug."
+            )
