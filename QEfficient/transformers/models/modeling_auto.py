c7a31d074d2c75f9f8630e6d1c9cf186f4d73bc6
Onkar Chougule <quic_ochougul@quicinc.com>
2024-09-13 19:34:42 +0530

    added export_and_compile and fixed bugs
    Signed-off-by: Onkar Chougule <quic_ochougul@quicinc.com>

--- QEfficient/transformers/models/modeling_auto.py
+++ QEfficient/transformers/models/modeling_auto.py
@@ -5,6 +5,7 @@
 #
 # ----------------------------------------------------------------------------
 
+import hashlib
 import os
 from typing import Any, List, Optional, Union
 
@@ -19,6 +20,7 @@ from QEfficient.transformers.quantizers.quant_transforms import AwqToMatmulNbits
 from QEfficient.transformers.quantizers.quantizer_awq import QEffAwqConfig
 from QEfficient.transformers.quantizers.quantizer_gptq import QEffGPTQConfig
 from QEfficient.utils import get_qpc_dir_path, load_hf_tokenizer
+from QEfficient.utils.constants import QEFF_MODELS_DIR
 from QEfficient.utils.logging_utils import logger
 
 # Dictionary that defines the interface from transformers to be used underneath the QEFF interface
@@ -46,12 +48,16 @@ class QEFFTransformersBase(QEFFBaseModel):
         self.pretrained_model_name_or_path = pretrained_model_name_or_path
 
         # Set model card name, which is used to decide ONNX, QPC files path during export and compile resp.
-        model_card_name = kwargs.pop("model_card_name", None)
-        self.model_card_name = (
-            model_card_name
-            if model_card_name
-            else (self.pretrained_model_name_or_path if not os.path.isdir(self.pretrained_model_name_or_path) else None)
-        )
+        if model_card_name := kwargs.pop("model_card_name", None):
+            self.model_card_name = model_card_name
+        elif os.path.isdir(self.pretrained_model_name_or_path):
+            hash_object = hashlib.sha256()
+            hash_object.update(self.pretrained_model_name_or_path.encode("utf-8"))
+            self.model_card_name = hash_object.hexdigest()
+        else:
+            self.model_card_name = self.pretrained_model_name_or_path
+
+        self.full_batch_size = kwargs.get("full_batch_size", None)
         self.kwargs = kwargs
         self._tokenizer = None
         self.is_transformed = False
@@ -74,7 +80,7 @@ class QEFFTransformersBase(QEFFBaseModel):
         ``Mandatory`` Args:
             :transform (bool): Whether to optimize model for KV retention; default is ``True``. Pass ``False`` to get BertStyle model.
             :model_card_name (str): ``HuggingFace`` model card name or name of the model if custom, used for deciding directory name while saving ``ONNX/qpc`` files.
-
+            :full_batch_size (int): Pass this if you want to execute model with continuous batching.
             Example usage:
 
         .. code-block:: python
@@ -163,9 +169,16 @@ class QEFFAutoModelForCausalLM(QEFFTransformersBase):
         if self.is_transformed:
             return
 
-        if kwargs.get("full_batch_size", None):
-            self._pytorch_transforms.remove(KVCacheTransform)
-            self._pytorch_transforms.append(CBTransform)
+        if self.full_batch_size is not None:
+            if KVCacheTransform in self._pytorch_transforms:
+                self._pytorch_transforms[self._pytorch_transforms.index(KVCacheTransform)] = CBTransform
+            if CBTransform not in self._pytorch_transforms:
+                raise RuntimeError("please don't update _pytorch_transforms variable")
+        else:
+            if CBTransform in self._pytorch_transforms:
+                self._pytorch_transforms[self._pytorch_transforms.index(CBTransform)] = KVCacheTransform
+            if KVCacheTransform not in self._pytorch_transforms:
+                raise RuntimeError("Please don't update _pytorch_transforms variable")
 
         # Update list of pytorch transforms if the model falls in AWQ/GPTQ category
         if hasattr(self.model.config, "quantization_config"):
@@ -182,7 +195,7 @@ class QEFFAutoModelForCausalLM(QEFFTransformersBase):
     def execute(self, *args, **kwargs):  # type: ignore
         raise NotImplementedError("Reached too far!!")
 
-    def export(self, model_card_name: Optional[str] = None) -> str:
+    def export(self) -> str:
         """
         Exports the model to ``ONNX`` format using ``torch.onnx.export``.
         The model should already be transformed i.e. ``self.is_transformed`` should be ``True``.
@@ -190,7 +203,7 @@ class QEFFAutoModelForCausalLM(QEFFTransformersBase):
         We currently don't support exporting non-transformed models. Please refer to the ``convert_to_cloud_bertstyle`` function in the **Low-Level API** for a legacy function that supports this."
 
         ``Optional`` Args:
-            :model_card_name (Optional[str]): Name of the model card. Mandatory when model is initialized with path for ``pretrained_model_name_or_path`` argument during initialization. ``Defaults to None.``
+            does not any arguments.
 
         Raises:
             :AttributeError: If ``pretrained_model_name_or_path`` is a path, this function needs model card name of the model so that it can distinguish between directories while saving the ``ONNX`` files generated. So, user needs to pass ``model_card_name`` as a valid ``string`` in that case, Otherwise this will raise the error.
@@ -199,15 +212,13 @@ class QEFFAutoModelForCausalLM(QEFFTransformersBase):
             :str: Path of the generated ``ONNX`` graph.
         """
         assert self.is_transformed, "Please first run transform on the QEFFAutoModelForCausalLM object"
-
-        # Make sure model_card_name is available for export
-        if self.model_card_name is None and model_card_name is None:
-            raise AttributeError("Please pass model_card_name as valid string input")
-        elif model_card_name is not None:
-            self.model_card_name = model_card_name
-
         # Export
-        _, onnx_model_path = QEfficient.export(model_name=self.model_card_name, model_kv=self, tokenizer=self.tokenizer)
+        _, onnx_model_path = QEfficient.export(
+            model_name=self.model_card_name,
+            model_kv=self,
+            tokenizer=self.tokenizer,
+            full_batch_size=self.full_batch_size,
+        )
         self.onnx_path = onnx_model_path
 
         return self.onnx_path
@@ -215,8 +226,7 @@ class QEFFAutoModelForCausalLM(QEFFTransformersBase):
     def compile(
         self,
         num_cores: int,
-        device_group: List[int],
-        model_card_name: Optional[str] = None,
+        device_group: List[int] = None,
         batch_size: int = 1,
         prompt_len: int = 32,
         ctx_len: int = 128,
@@ -232,7 +242,7 @@ class QEFFAutoModelForCausalLM(QEFFTransformersBase):
 
         ``Mandatory`` Args:
             :num_cores (int): Number of cores used to compile the model.
-            :device_group (List[int]): If this is a list of more that one integers, tensor-slicing is invoked.
+            :device_group (List[int]): If this is a list of more that one integers, tensor-slicing is invoked, defaults to None, and automatically chooses suitable device.
         ``Optional`` Args:
             :model_card_name (Optional[str], optional): Name of the model, Mandatory if ``self.pretrained_model_name_or_path`` is a path. ``Defaults to None``.
             :batch_size (int, optional): Batch size. ``Defaults to 1``.
@@ -249,7 +259,7 @@ class QEFFAutoModelForCausalLM(QEFFTransformersBase):
         # Export first if self.ort_runtime_args are not populated
         if self.onnx_path is None:
             logger.info(f"Exporting the {self.model.__class__.__name__} model to ONNX for compilation!")
-            self.export(model_card_name=model_card_name)
+            self.export()
 
         # Prepare qpc dir path
         qpc_dir_path = get_qpc_dir_path(
@@ -262,7 +272,7 @@ class QEFFAutoModelForCausalLM(QEFFTransformersBase):
             mxfp6=mxfp6,
             mxint8=mxint8,
             device_group=device_group,
-            full_batch_size=None,
+            full_batch_size=self.full_batch_size,
         )
 
         # Compile
@@ -278,13 +288,73 @@ class QEFFAutoModelForCausalLM(QEFFTransformersBase):
             ctx_len=ctx_len,
             mxfp6=mxfp6,
             mxint8=mxint8,
+            full_batch_size=self.full_batch_size,
         )
         self.qpc_path = qpc_dir_path
-        self.device_id = device_group
-
         return self.qpc_path
 
-    def generate(self, prompts: List[str], runtime: str = "AI_100", **kwargs):
+    def export_and_compile(
+        self,
+        num_cores: int,
+        device_group: List[int],
+        batch_size: int = 1,
+        prompt_len: int = 32,
+        ctx_len: int = 128,
+        mxfp6: bool = True,
+        mxint8: bool = False,
+        mos: int = -1,
+        aic_enable_depth_first: bool = False,
+        qpc_dir_suffix: Optional[str] = None,
+        full_batch_size: Optional[int] = None,
+    ) -> str:
+        """
+        This API is specific to Internal VLLM use-case and is not recommended to be used in your application unless your are using VLLM.
+        """
+        _, transformed = CBTransform.apply(self.model)
+        if not transformed:
+            raise RuntimeError("Could not apply Continuous batch transform on the model")
+        if full_batch_size is not None:
+            self.full_batch_size = full_batch_size
+
+        self.export()
+
+        qpc_base_dir_name = get_qpc_dir_path(
+            model_card_name=self.model_card_name,
+            num_cores=num_cores,
+            mos=mos,
+            batch_size=batch_size,
+            prompt_len=prompt_len,
+            ctx_len=ctx_len,
+            mxfp6=mxfp6,
+            mxint8=mxint8,
+            device_group=device_group,
+            full_batch_size=self.full_batch_size,
+        )
+        qpc_base_dir_name = (
+            os.path.dirname(qpc_base_dir_name) + "_" + qpc_dir_suffix if qpc_dir_suffix else qpc_base_dir_name
+        )
+        model_card_dir = os.path.join(QEFF_MODELS_DIR, str(self.model_card_name))
+        os.makedirs(model_card_dir, exist_ok=True)
+        qpc_dir_path = os.path.join(model_card_dir, qpc_base_dir_name)
+
+        # Compile
+        self.qpc_path = QEfficient.compile(
+            onnx_path=self.onnx_path,
+            qpc_path=qpc_dir_path,
+            num_cores=num_cores,
+            device_group=device_group,
+            aic_enable_depth_first=aic_enable_depth_first,
+            mos=mos,
+            batch_size=batch_size,
+            prompt_len=prompt_len,
+            ctx_len=ctx_len,
+            mxfp6=mxfp6,
+            mxint8=mxint8,
+            full_batch_size=full_batch_size,
+        )
+        return self.qpc_path
+
+    def generate(self, prompts: List[str], device_id: List[int] = None, runtime: str = "AI_100", **kwargs):
         """
         This method generates output until ``eos`` or ``generation_len`` by executing the compiled ``qpc`` on ``Cloud AI 100`` Hardware cards.
         This is a sequential execution based on the ``batch_size`` of the compiled model and the number of prompts passed.
@@ -292,20 +362,23 @@ class QEFFAutoModelForCausalLM(QEFFTransformersBase):
 
         ``Mandatory`` Args:
             :prompts (List[str]): List of prompts to run the execution.
+            :device_id (List[int]): Ids of devices for running the qpc pass as [0] in case of normal model / [0, 1, 2, 3] in case of tensor slicing model
         ``optional`` Args:
             :runtime (str, optional): Only ``AI_100`` runtime is supported as of now; ``ONNXRT`` and ``PyTorch`` coming soon. Defaults to "AI_100".
         """
         assert Runtime(runtime) == Runtime.AI_100, "Only AI_100 runtime is supported right now via generate API"
-        self.run_cloud_ai_100(prompts=prompts, **kwargs)
+        self.run_cloud_ai_100(prompts=prompts, device_id=device_id, **kwargs)
 
-    def run_cloud_ai_100(self, prompts: List[str], **kwargs):
+    def run_cloud_ai_100(self, prompts: List[str], device_id: List[int] = None, **kwargs):
         assert isinstance(self.qpc_path, str), "Please run compile API first!"
-        assert (
-            self.device_id is not None
-        ), "please pass valid device_id as input argument"  # FIXME: replace with isinstance
         generation_len = kwargs.pop("generation_len", None)
         return QEfficient.cloud_ai_100_exec_kv(
-            self.tokenizer, self.qpc_path, prompt=prompts, device_id=self.device_id, generation_len=generation_len
+            self.tokenizer,
+            self.qpc_path,
+            prompt=prompts,
+            device_id=device_id,
+            generation_len=generation_len,
+            full_batch_size=self.full_batch_size,
         )
 
 
--- QEfficient/transformers/quantizers/gptq.py
+++ QEfficient/transformers/quantizers/gptq.py
@@ -1,3 +1,10 @@
+# -----------------------------------------------------------------------------
+#
+# Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# ----------------------------------------------------------------------------
+
 import math
 
 import torch
