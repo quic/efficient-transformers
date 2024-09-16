74307b7c0d977ed5fbdc4767cc77f5f8036d6b88
Ilango Rajagopal <quic_irajagop@quicinc.com>
2024-09-16 13:46:47 +0000

    Move compile & export to base model
    - Remove abstract methods
    - Remove unused variables
    
    Signed-off-by: Ilango Rajagopal <quic_irajagop@quicinc.com>
    Signed-off-by: Onkar Chougule <quic_ochougul@quicinc.com>

--- QEfficient/base/modeling_qeff.py
+++ QEfficient/base/modeling_qeff.py
@@ -5,50 +5,25 @@
 #
 # ----------------------------------------------------------------------------
 
-"""
-** This file for holds the classes that handle main functions
-1.load i.e. from_pretrained
-2.execute
-3.transform
-4.export
-5.compile
-For different varities of Transformer Models
-
-Representation of class inheritence followed keeping in line with transformers/diffusers repos ->
-
-                                                                                            QEFFBaseModel
-                                                 ________________________________________________|________________________________________________________________
-                                                |                                                                                                                 |
-                                            QEFFTransformersBase                                                                                           QEFFDiffusersBase
-                                                |                                                                                                                 |
-                                    ____________|________________________________________________________ ________________                       _________________|______________
-                   _____           |                              |                                      |                |                     |                                |
-                  |          QEFFAutoModel             QEFFAutoModelForCausalLM              QEFFAWQModelForCausalLM     ...                   ...                              ...
-QEFFCommonLoader -|       [Provides way to          [Provides way to do 1-5 on                 [Supports 1-5 for
-[Provides         |        do steps 1-5 on           transformers.AutoModelForCausalLM]         AWQ Models]
-interface to      |_____   transformers.AutoModel]
-Load any of
-These models
-by automatically
-detecting the type
-of the model]
-
-** QEFFBASEModel is abstract base class that defines the basic structure of these classes.
-** QEFFPipeline classes will stay at the same level as QEFFAutoModel in this hierarchy in future.
-"""
-
-from abc import ABC, abstractmethod
+import hashlib
+import json
+import logging
+import shutil
+import subprocess
+from abc import ABC, abstractmethod, abstractproperty
 from enum import Enum
-from typing import Any, Dict, List, Optional
+from pathlib import Path
+from typing import Dict, List, Optional
 
-import numpy as np
+import onnx
 import torch
-from onnxruntime import InferenceSession as ORTInferenceSession
 
 from QEfficient.generation.cloud_infer import QAICInferenceSession
+from QEfficient.utils.cache import QEFF_HOME, to_hashable
+
+logger = logging.getLogger(__name__)
 
 
-# Defining placeholder ENUM for execute function
 class Runtime(Enum):
     CPU_ORT = "CPU_ORT"
     CPU_PT = "CPU_PT"
@@ -57,44 +32,210 @@ class Runtime(Enum):
 
 class QEFFBaseModel(ABC):
     """
-    This class acts as parent class for all the varieties of model class (i.e. LLMs, SD, quantized etc.).
-    Enforces certain methods to be implemented by child classes.
+    Base class for all the model classes (i.e. LLMs, SD, quantized etc.).
+    Provides certain utility methods to be used by child classes.
 
-    All the child classes must provide way to load, transform(optimize), exoprt to ONNX etc. capabilities.
+    Class variables:
+    :_pytorch_transforms: Pytorch transformations to be applied after initialization.
+    :_onnx_transforms: ONNX transformations to be applied after ONNX export.
     """
+
+    @classmethod
+    def _transform_names(cls) -> List[str]:
+        return [x.__name__ for x in cls._pytorch_transforms + cls._onnx_transforms]
 
     def __init__(self, model: torch.nn.Module) -> None:
         super().__init__()
         self.model = model
         self.onnx_path: Optional[str] = None
-        self.ort_session: Optional[ORTInferenceSession] = None
         self.qpc_path: Optional[str] = None
-        self.device_id: Optional[List[int]] = None
         self.qpc_session: Optional[QAICInferenceSession] = None
 
-    @classmethod
-    def from_pretrained(cls, pretrained_model_name_or_path: str, *args, **kwargs):
-        raise NotImplementedError("Must implement for child classes")
+        # Apply the transformations
+        for transform in self._pytorch_transforms:
+            self.model, transformed = transform.apply(self.model)
+        logger.info("Pytorch transforms applied")
 
-    def run_pytorch(self, inputs):
-        raise NotImplementedError("Reached too far!!")
+    @abstractproperty
+    def model_name(self) -> str: ...
 
-    def run_ort(self, inputs: Dict[str, np.ndarray], /, cache_session: bool = False):
-        raise NotImplementedError("Reached too far!!")
+    @abstractproperty
+    def model_hash(self) -> str: ...
 
-    def run_cloud_ai_100(
+    @abstractmethod
+    def export(self, *args, **kwargs) -> Path: ...
+
+    @abstractmethod
+    def compile(self, *args, **kwargs) -> Path: ...
+
+    def _export(
         self,
-    ):
-        raise NotImplementedError("Reached too far!!")
+        example_inputs: Dict[str, torch.Tensor],
+        input_names: List[str],
+        output_names: List[str],
+        dynamic_axes: Dict[str, Dict[int, str]],
+        export_kwargs: Dict[str, any] = {},
+        onnx_transform_kwargs: Dict[str, any] = {},
+        export_dir: Optional[str] = None,
+    ) -> str:
+        """
+        Export the Pytorch model to ONNX.
 
-    @abstractmethod
-    def transform(self, *args, **kwargs) -> Any:
-        pass
+        Args:
+            :example_inputs (dict): Sample inputs to trace the model.
+            :input_names (list): names to assign to the input nodes of the graph, in order.
+            :output_names (list): names to assign to the output nodes of the graph, in order.
+            :dynamic_axes (dict): Same as dynamic_axes parameter to be passed to `torch.onnx.export`.
+            :export_kwargs (dict): Additional arguments to be passed to `torch.onnx.export`.
+            :onnx_transform_kwargs (dict): Additional arguments to be passed to `Transform.apply` for this class.
+            :export_dir (str): Specify the export directory. The export_dir will be suffixed with a hash corresponding to current model.
+        """
+        # Base class
+        export_dir = Path(export_dir or (QEFF_HOME / self.model_name))
+        export_dir = export_dir.with_name(export_dir.name + "-" + self.model_hash)
+        onnx_path = export_dir / f"{self.model_name}.onnx"
+        if onnx_path.is_file():
+            self.onnx_path = onnx_path
+            return onnx_path
 
-    @abstractmethod
-    def export(self, *args, **kwargs) -> Any:
-        pass
+        tmp_onnx_dir = export_dir / "onnx_tmp"
+        tmp_onnx_path = tmp_onnx_dir / f"{self.model_name}.onnx"
+        tmp_onnx_dir.mkdir(parents=True, exist_ok=True)
 
-    @abstractmethod
-    def compile(self, *args, **kwargs) -> Any:
-        pass
+        try:
+            torch.onnx.export(
+                self.model,
+                (example_inputs,),
+                str(tmp_onnx_path),
+                input_names=input_names,
+                output_names=output_names,
+                dynamic_axes=dynamic_axes,
+                opset_version=13,
+                **export_kwargs,
+            )
+            logger.info("Pytorch export successful")
+
+            model = onnx.load(tmp_onnx_path, load_external_data=False)
+            onnx_transform_kwargs = {
+                "onnx_base_dir": str(tmp_onnx_dir),
+                "model_name": self.model_name,
+                **onnx_transform_kwargs,
+            }
+            for transform in self._onnx_transforms:
+                model, transformed = transform.apply(model, **onnx_transform_kwargs)
+            model.metadata_props.append(
+                onnx.StringStringEntryProto(key="qeff_transforms", value=",".join(self._transform_names()))
+            )
+            logger.info("ONNX transforms applied")
+
+            onnx.save(model, onnx_path)
+            logger.info("Transformed onnx saved")
+
+        except Exception as e:
+            logger.error(f"ONNX export failed: {e}")
+            raise e
+
+        finally:
+            shutil.rmtree(tmp_onnx_dir, ignore_errors=True)
+
+        self.onnx_path = onnx_path
+        return onnx_path
+
+    def _compile(
+        self,
+        onnx_path: Optional[str] = None,
+        compile_dir: Optional[str] = None,
+        *,
+        specializations: Optional[List[Dict[str, int]]] = None,
+        custom_io: Optional[Dict[str, str]] = None,
+        mdp_ts_num_devices: int = 1,
+        **compiler_options,
+    ) -> str:
+        """
+        Interface for qaic-exec compiler
+
+        Args:
+            :onnx_path (str): Onnx file to compile
+            :compile_dir (str): Directory path to compile the qpc. A suffix is added to the directory path to avoid reusing same qpc for different parameters.
+            :specializations (list): List of specializations to compile for
+            :custom_io (dict): Custom IO to specify the input and outputs in different formats than default
+            :mdp_ts_num_devices (int): Number of devices to paratition to use Multi-Device Partitioning with tensor-slicing.
+            :compiler_options: Pass any compiler option as input. Any flag that is supported by `qaic-exec` can be passed. Params are converted to flags as below:
+                - aic_num_cores=16 -> -aic-num-cores=16
+                - convert_to_fp16=True -> -convert-to-fp16
+        """
+        # Base class
+        onnx_path = Path(onnx_path or self.onnx_path)
+        compile_dir = Path(compile_dir or onnx_path.parent)
+        qpc_path = compile_dir / "qpc"
+        if not onnx_path.is_file():
+            raise FileNotFoundError(f"ONNX file not found at: {onnx_path}")
+
+        command = ["/opt/qti-aic/exec/qaic-exec", f"-m={onnx_path}", "-aic-hw", "-aic-hw-version=2.0"]
+        for key, value in compiler_options.items():
+            option = "-" + key.replace("_", "-")
+            if isinstance(value, bool):
+                if value:
+                    command.append(option)
+                continue
+            command.append(f"{option}={value}")
+        compile_hash = hashlib.sha256(to_hashable(command))
+
+        # Write specializations.json file
+        if specializations is not None:
+            specializations_json = compile_dir / "specializations.json"
+            with open(specializations_json, "w") as fp:
+                json.dump(
+                    {"specializations": [{k: str(v) for k, v in spec.items()} for spec in specializations]},
+                    fp,
+                    indent=4,
+                )
+            command.append(f"-network-specialization-config={specializations_json}")
+            compile_hash.update(to_hashable(specializations))
+
+        # Write custom_io.yaml file
+        if custom_io:
+            custom_io_yaml = compile_dir / "custom_io.yaml"
+            with open(custom_io_yaml, "w") as fp:
+                for io_name, dtype in custom_io.items():
+                    fp.write(f" - IOName: {io_name}\n   Precision: {dtype}\n\n")
+            command.append(f"-custom-IO-list-file={custom_io_yaml}")
+            compile_hash.update(to_hashable(custom_io))
+
+        # Write mdp_config.json file
+        if mdp_ts_num_devices > 1:
+            num_cores = compiler_options.get("aic_num_cores", 16)
+            mdp_ts_json = compile_dir / f"mdp_ts_{mdp_ts_num_devices}.json"
+            with open(mdp_ts_json, "w") as fp:
+                json.dump(
+                    {
+                        "connections": [{"devices": list(range(mdp_ts_num_devices)), "type": "p2p"}],
+                        "partitions": [
+                            {
+                                "name": "Partition0",
+                                "devices": [{"deviceId": d, "numCores": num_cores} for d in range(mdp_ts_num_devices)],
+                            }
+                        ],
+                    },
+                    fp,
+                    indent=4,
+                )
+            command.append(f"-mdp-load-partition-config={mdp_ts_json}")
+            compile_hash.update(to_hashable({"mdp_ts_num_devices": mdp_ts_num_devices}))
+
+        # Check if already compiled
+        compile_hash = compile_hash.hexdigest()[:16]
+        qpc_path = qpc_path.with_name(qpc_path.name + "-" + compile_hash)
+        if qpc_path.is_dir():
+            if (qpc_path / "programqpc.bin").is_file():
+                self.qpc_path = qpc_path
+                return qpc_path
+            # Probably compilation failure last time, delete directory to start over
+            shutil.rmtree(qpc_path)
+
+        command.append(f"-aic-binary-dir={qpc_path}")
+        logger.info(f"Running compiler: {' '.join(command)}")
+        subprocess.run(command).check_returncode()
+
+        self.qpc_path = qpc_path
+        return qpc_path
