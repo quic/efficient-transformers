70319416ea68df0ddbeb15f59b839273a333e07b
Ilango Rajagopal <quic_irajagop@quicinc.com>
2024-09-17 09:52:38 +0000

    Refactor AutoClass to QEFFAutoClass
    Signed-off-by: Ilango Rajagopal <quic_irajagop@quicinc.com>

--- tests/transformers/test_causal_lm.py
+++ tests/transformers/test_causal_lm.py
@@ -60,40 +60,46 @@ def config_id(config):
     return config.model_type
 
 
-@pytest.mark.parametrize("AutoClass", [QEFFAutoModelForCausalLM, QEFFAutoModelForCausalLMwithCB], ids=["nocb", "cb"])
+@pytest.mark.parametrize(
+    "QEFFAutoClass", [QEFFAutoModelForCausalLM, QEFFAutoModelForCausalLMwithCB], ids=["nocb", "cb"]
+)
 @pytest.mark.parametrize("config", configs, ids=config_id)
-def test_causal_lm_init(AutoClass, config):
+def test_causal_lm_init(QEFFAutoClass, config):
     model = AutoModelForCausalLM.from_config(config, **model_kwargs)
-    qeff_model = AutoClass(model)
+    qeff_model = QEFFAutoClass(model)
     with pytest.raises(TypeError):
-        AutoClass(AutoModel.from_config(config, **model_kwargs))
+        QEFFAutoClass(AutoModel.from_config(config, **model_kwargs))
     assert qeff_model.model.__class__.__name__.startswith("QEff")
 
 
-@pytest.mark.parametrize("AutoClass", [QEFFAutoModelForCausalLM, QEFFAutoModelForCausalLMwithCB], ids=["nocb", "cb"])
+@pytest.mark.parametrize(
+    "QEFFAutoClass", [QEFFAutoModelForCausalLM, QEFFAutoModelForCausalLMwithCB], ids=["nocb", "cb"]
+)
 @pytest.mark.parametrize("config", configs, ids=config_id)
-def test_causal_lm_pretrained(AutoClass, config, tmp_path):
+def test_causal_lm_pretrained(QEFFAutoClass, config, tmp_path):
     model = AutoModelForCausalLM.from_config(config, **model_kwargs)
     model.save_pretrained(tmp_path)
 
-    qeff_model = AutoClass.from_pretrained(tmp_path)
+    qeff_model = QEFFAutoClass.from_pretrained(tmp_path)
     assert qeff_model.model.__class__.__name__.startswith("QEff")
 
 
-@pytest.mark.parametrize("AutoClass", [QEFFAutoModelForCausalLM, QEFFAutoModelForCausalLMwithCB], ids=["nocb", "cb"])
+@pytest.mark.parametrize(
+    "QEFFAutoClass", [QEFFAutoModelForCausalLM, QEFFAutoModelForCausalLMwithCB], ids=["nocb", "cb"]
+)
 @pytest.mark.parametrize("config", configs, ids=config_id)
-def test_causal_lm_hash(AutoClass, config):
-    hash_0_0 = AutoClass(AutoModelForCausalLM.from_config(config, **model_kwargs)).model_hash
-    hash_0_1 = AutoClass(AutoModelForCausalLM.from_config(config, **model_kwargs)).model_hash
+def test_causal_lm_hash(QEFFAutoClass, config):
+    hash_0_0 = QEFFAutoClass(AutoModelForCausalLM.from_config(config, **model_kwargs)).model_hash
+    hash_0_1 = QEFFAutoClass(AutoModelForCausalLM.from_config(config, **model_kwargs)).model_hash
 
     assert hash_0_0 == hash_0_1
 
     cfg1 = copy.deepcopy(config)
     cfg1.num_hidden_layers -= 1
-    hash_1_0 = AutoClass(AutoModelForCausalLM.from_config(cfg1, **model_kwargs)).model_hash
+    hash_1_0 = QEFFAutoClass(AutoModelForCausalLM.from_config(cfg1, **model_kwargs)).model_hash
     cfg2 = copy.deepcopy(config)
     cfg2.num_hidden_layers -= 1
-    hash_1_1 = AutoClass(AutoModelForCausalLM.from_config(cfg2, **model_kwargs)).model_hash
+    hash_1_1 = QEFFAutoClass(AutoModelForCausalLM.from_config(cfg2, **model_kwargs)).model_hash
     assert hash_1_0 == hash_1_1
 
     assert hash_0_0 != hash_1_0
