68cdf3bbe4772f78589e923de000f9df09d66ca1
Jou-An Chen <quic_jouachen@quicinc.com>
2024-10-17 13:36:45 -0700

    Remove set delete adapter, add init assertion, update LinearMultiLoRA
    Signed-off-by: Jou-An Chen <quic_jouachen@quicinc.com>

--- tests/lora/test_lora_model.py
+++ tests/lora/test_lora_model.py
@@ -53,13 +53,6 @@ def create_lora_base_model(base_config):
     return lora_base_model
 
 
-def load_adapter_with_random_weights(lora_base_model, adapter_config, adapter_name):
-    lora_base_model.adapter_configs[adapter_name] = adapter_config
-    lora_base_model.adapter_weights[adapter_name] = {"weights": np.ones((3, 3))}
-
-    return lora_base_model
-
-
 # test model initialization using __init__ approach
 @pytest.mark.parametrize("base_model_name,adapter_id_0,adapter_id_1", model_samples)
 def test_auto_lora_model_for_causal_lm_init(base_model_name, adapter_id_0, adapter_id_1):
@@ -87,73 +80,98 @@ def test_auto_lora_model_for_causal_lm_from_pretrained(base_model_name, adapter_
     assert len(qeff_model.active_adapter_to_id) == 0
 
 
+# test the init assertion for models that are not supported
+@pytest.mark.parametrize("base_model_name", ["distilbert/distilgpt2"])
+def test_auto_lora_model_for_causal_lm_init_from_unsupported_model(base_model_name):
+    model_hf = AutoModelForCausalLM.from_pretrained(base_model_name, num_hidden_layers=1)
+    with pytest.raises(AssertionError):
+        QEffAutoLoraModelForCausalLM(model_hf, pretrained_model_name_or_path=base_model_name)
+
+    with pytest.raises(AssertionError):
+        QEffAutoLoraModelForCausalLM.from_pretrained(base_model_name, num_hidden_layers=1)
+
+
 # test model hash
 def test_auto_lora_model_for_causal_lm_hash():
     base_config_0, adapter_config_0 = configs[0].values
     base_config_1, adapter_config_1 = configs[1].values
 
     qeff_model_0 = create_lora_base_model(base_config_0)
-    qeff_model_0 = load_adapter_with_random_weights(qeff_model_0, adapter_config_0, "adapter_0")
-    qeff_model_0 = load_adapter_with_random_weights(qeff_model_0, adapter_config_1, "adapter_1")
+    qeff_model_0.load_adapter(
+        "dummy_id", "adapter_0", adapter_config=adapter_config_0, adapter_weight={"weights": np.ones((3, 3))}
+    )
+    qeff_model_0.load_adapter(
+        "dummy_id", "adapter_1", adapter_config=adapter_config_1, adapter_weight={"weights": np.ones((3, 3))}
+    )
+    model_hash_0_0 = qeff_model_0.model_hash
 
     qeff_model_1 = create_lora_base_model(base_config_1)
-    qeff_model_1 = load_adapter_with_random_weights(qeff_model_1, adapter_config_0, "adapter_0")
-    qeff_model_1 = load_adapter_with_random_weights(qeff_model_1, adapter_config_1, "adapter_1")
+    qeff_model_1.load_adapter(
+        "dummy_id", "adapter_0", adapter_config=adapter_config_0, adapter_weight={"weights": np.ones((3, 3))}
+    )
+    qeff_model_1.load_adapter(
+        "dummy_id", "adapter_1", adapter_config=adapter_config_1, adapter_weight={"weights": np.ones((3, 3))}
+    )
+    model_hash_1_0 = qeff_model_1.model_hash
 
     qeff_model_0_1 = create_lora_base_model(base_config_0)
-    qeff_model_0_1 = load_adapter_with_random_weights(qeff_model_0_1, adapter_config_0, "adapter_0")
-    qeff_model_0_1 = load_adapter_with_random_weights(qeff_model_0_1, adapter_config_1, "adapter_1")
-    qeff_model_0_1.set_adapter("adapter_0")
+    qeff_model_0_1.load_adapter(
+        "dummy_id", "adapter_0", adapter_config=adapter_config_0, adapter_weight={"weights": np.ones((3, 3))}
+    )
+    qeff_model_0_1.load_adapter(
+        "dummy_id", "adapter_1", adapter_config=adapter_config_1, adapter_weight={"weights": np.ones((3, 3))}
+    )
     model_hash_0_1_0 = qeff_model_0_1.model_hash
-    qeff_model_0_1.set_adapter("adapter_1")
-    model_hash_0_1_1 = qeff_model_0_1.model_hash
 
-    # check num of adapter config matters
-    qeff_model_0.set_adapter("adapter_0")
-    model_hash_0_0 = qeff_model_0.model_hash
-    qeff_model_0.set_adapter("adapter_1")
-    model_hash_0_1 = qeff_model_0.model_hash
-    assert model_hash_0_0 != model_hash_0_1
-
-    # check if same model, same adapter result in same hash
+    # check if same model, same adapter config, same adapter weight, result in same hash
     assert model_hash_0_1_0 == model_hash_0_0
-    assert model_hash_0_1_1 == model_hash_0_1
 
-    # check base model configs matters
-    qeff_model_1.set_adapter("adapter_0")
-    qeff_model_1.set_adapter("adapter_1")
+    # check if same model, same adapter config, but different weight, result in different hash
+    qeff_model_0_1.unload_adapter("adapter_1")
+    qeff_model_0_1.unload_adapter("adapter_0")
+    qeff_model_0_1.load_adapter(
+        "dummy_id", "adapter_0", adapter_config=adapter_config_0, adapter_weight={"weights": np.random.randn(3, 3)}
+    )
+    qeff_model_0_1.load_adapter(
+        "dummy_id", "adapter_1", adapter_config=adapter_config_1, adapter_weight={"weights": np.random.randn(3, 3)}
+    )
+    model_hash_0_1_1 = qeff_model_0_1.model_hash
+    assert model_hash_0_1_1 != model_hash_0_0
+
+    # check base model configs difference result in different hash
+    assert model_hash_0_0 != model_hash_1_0
+
+    # check different adapter orders, result in different hash
+    qeff_model_1.unload_adapter("adapter_0")
+    qeff_model_1.unload_adapter("adapter_1")
+    qeff_model_1.load_adapter(
+        "dummy_id", "adapter_1", adapter_config=adapter_config_1, adapter_weight={"weights": np.ones((3, 3))}
+    )
+    qeff_model_1.load_adapter(
+        "dummy_id", "adapter_0", adapter_config=adapter_config_0, adapter_weight={"weights": np.ones((3, 3))}
+    )
     model_hash_1_1 = qeff_model_1.model_hash
-    assert model_hash_1_1 != model_hash_0_1
+    assert model_hash_1_1 != model_hash_1_0
 
-    # check adapter orders matters
-    qeff_model_1.delete_adapter("adapter_0")
-    qeff_model_1.delete_adapter("adapter_1")
-    qeff_model_1.set_adapter("adapter_1")
-    qeff_model_1.set_adapter("adapter_0")
-    model_hash_1_2 = qeff_model_1.model_hash
-    assert model_hash_1_2 != model_hash_1_1
-
-    # check if different adapter config, but same adapter name matters
-    qeff_model_0 = load_adapter_with_random_weights(qeff_model_0, adapter_config_0, "adapter_1")
-    model_hash_0_2 = qeff_model_0.model_hash
-    assert model_hash_0_2 != model_hash_0_1
+    # check if same adapter name, but different config, result in different hash
+    qeff_model_0.load_adapter(
+        "dummy_id", "adapter_1", adapter_config=adapter_config_0, adapter_weight={"weights": np.ones((3, 3))}
+    )
+    model_hash_0_1 = qeff_model_0.model_hash
+    assert model_hash_0_1 != model_hash_0_0
 
 
-# test load_adapter() and set_adapter() and get_adapter_id()
+# test load_adapter() and get_adapter_id()
 @pytest.mark.parametrize("base_model_name,adapter_id_0,adapter_id_1", model_samples[:1])
-def test_auto_lora_model_for_causal_lm_load_set_adapter_id_check(base_model_name, adapter_id_0, adapter_id_1):
+def test_auto_lora_model_for_causal_lm_load_get_adapter_id_check(base_model_name, adapter_id_0, adapter_id_1):
     qeff_model = QEffAutoLoraModelForCausalLM.from_pretrained(base_model_name, num_hidden_layers=1)
 
-    qeff_model.load_adapter(adapter_id_0, "adapter_0")
-    qeff_model.load_adapter(adapter_id_1, "adapter_1")
-    set_id_0 = qeff_model.set_adapter("adapter_0")
-    set_id_1 = qeff_model.set_adapter("adapter_1")
-
+    set_id_0 = qeff_model.load_adapter(adapter_id_0, "adapter_0")
+    set_id_1 = qeff_model.load_adapter(adapter_id_1, "adapter_1")
     assert set_id_1 == set_id_0 + 1
 
     qeff_model.load_adapter(adapter_id_1, "adapter_2")
-    qeff_model.set_adapter("adapter_2")
-    qeff_model.delete_adapter("adapter_1")
+    qeff_model.unload_adapter("adapter_1")
 
     update_id_0 = qeff_model.get_adapter_id("adapter_0")
     update_id_2 = qeff_model.get_adapter_id("adapter_2")
@@ -164,21 +182,18 @@ def test_auto_lora_model_for_causal_lm_load_set_adapter_id_check(base_model_name
         qeff_model.get_adapter_id("adapter_1")
 
 
-# test unload_adapter() and delete_adapter()
+# test download_adapter(), load_adapter() and unload_adapter()
 @pytest.mark.parametrize("base_model_name,adapter_id_0,adapter_id_1", model_samples[1:])
-def test_auto_lora_model_for_causal_lm_unload_delete_adapter(base_model_name, adapter_id_0, adapter_id_1):
+def test_auto_lora_model_for_causal_lm_load_unload_adapter(base_model_name, adapter_id_0, adapter_id_1):
     qeff_model = QEffAutoLoraModelForCausalLM.from_pretrained(base_model_name, num_hidden_layers=1)
 
+    qeff_model.download_adapter(adapter_id_0, "adapter_0")
+    qeff_model.download_adapter(adapter_id_1, "adapter_1")
+
     qeff_model.load_adapter(adapter_id_0, "adapter_0")
-    qeff_model.load_adapter(adapter_id_1, "adapter_1")
-    qeff_model.set_adapter("adapter_0")
 
-    assert not qeff_model.unload_adapter("adapter_0")  # active adapter
-    assert qeff_model.unload_adapter("adapter_1")  # valid unload
-    assert not qeff_model.unload_adapter("adapter_2")  # not loaded adapter
-
-    assert qeff_model.delete_adapter("adapter_0")  # active adapter
-    assert not qeff_model.delete_adapter("adapter_1")  # not active adapter
+    assert not qeff_model.unload_adapter("adapter_1")  # not active adapter
+    assert qeff_model.unload_adapter("adapter_0")  # valid unload
 
 
 # test the export, export caching, compile, generate workflow
@@ -186,10 +201,8 @@ def test_auto_lora_model_for_causal_lm_unload_delete_adapter(base_model_name, ad
 def test_auto_lora_model_for_causal_lm_export_compile_generate(base_model_name, adapter_id_0, adapter_id_1, tmp_path):
     qeff_model = QEffAutoLoraModelForCausalLM.from_pretrained(base_model_name, num_hidden_layers=1)
 
-    qeff_model.load_adapter(adapter_id_0, "adapter_0")
-    qeff_model.load_adapter(adapter_id_1, "adapter_1")
-    qeff_model.set_adapter("adapter_0")
-    qeff_model.set_adapter("adapter_1")
+    id_0 = qeff_model.load_adapter(adapter_id_0, "adapter_0")
+    id_1 = qeff_model.load_adapter(adapter_id_1, "adapter_1")
 
     # export
     start = perf_counter()
@@ -205,7 +218,7 @@ def test_auto_lora_model_for_causal_lm_export_compile_generate(base_model_name, 
     qeff_model.export(export_dir=tmp_path, full_batch_size=1)
     end = perf_counter()
     export_time_1 = end - start
-    assert export_time_1 < 0.01 * export_time_0
+    assert export_time_1 < export_time_0
 
     # test compile
     qeff_model.compile(num_cores=16, device_group=[0])
@@ -213,4 +226,4 @@ def test_auto_lora_model_for_causal_lm_export_compile_generate(base_model_name, 
 
     # test generate
     prompts = ["hello!", "hi", "hello, my name is", "hey"]
-    qeff_model.generate(prompts, [0], prompt_to_lora_id_mapping=[0, 1, 0, INTMAX])
+    qeff_model.generate(prompts, [0], prompt_to_lora_id_mapping=[id_0, id_1, id_0, INTMAX])
