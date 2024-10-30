eae356dba36049f47d14fc759418095cb5f41048
Mahesh Balasubramanian <quic_mahebala@quicinc.com>
2024-10-30 12:09:54 -0700

    Changes made according to the compliance
    Signed-off-by: Mahesh Balasubramanian <quic_mahebala@quicinc.com>

--- tests/vllm/test_qaic_output_consistency.py
+++ tests/vllm/test_qaic_output_consistency.py
@@ -5,87 +5,26 @@
 #
 # -----------------------------------------------------------------------------
 
+import gc
 import random
 
 import pytest
 from vllm import LLM, SamplingParams
 
+test_models = [
+    "TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+]
 
-@pytest.fixture(scope="session")
-def model_name(pytestconfig):
-    return pytestconfig.getoption("model_name")
-
-
-@pytest.fixture(scope="session")
-def seq_len(pytestconfig):
-    return pytestconfig.getoption("seq_len")
-
-
-@pytest.fixture(scope="session")
-def ctx_len(pytestconfig):
-    return pytestconfig.getoption("ctx_len")
+seq_len = 128
+ctx_len = 256
+decode_bsz = 4
+dtype = "mxfp6"
+kv_dtype = "mxint8"
+device_group = [0]
 
 
-@pytest.fixture(scope="session")
-def decode_bsz(pytestconfig):
-    return pytestconfig.getoption("decode_bsz")
-
-
-@pytest.fixture(scope="session")
-def dtype(pytestconfig):
-    return pytestconfig.getoption("dtype")
-
-
-@pytest.fixture(scope="session")
-def kv_dtype(pytestconfig):
-    return pytestconfig.getoption("kv_dtype")
-
-
-@pytest.fixture(scope="session")
-def dataset(pytestconfig):
-    return pytestconfig.getoption("dataset")
-
-
-model_name = None
-seq_len = None
-ctx_len = None
-decode_bsz = None
-dtype = None
-kv_dtype = None
-dataset = None
-device_group = None
-sampling_params = None
-qllm = None
-
-
-@pytest.fixture(autouse=True, scope="session")
-def init(pytestconfig):
-    global model_name
-    global seq_len
-    global ctx_len
-    global decode_bsz
-    global dtype
-    global kv_dtype
-    global dataset
-    global device_group
-    global sampling_params
-    global qllm
-
-    model_name = pytestconfig.getoption("model_name")
-    seq_len = pytestconfig.getoption("seq_len")
-    ctx_len = pytestconfig.getoption("ctx_len")
-    decode_bsz = pytestconfig.getoption("decode_bsz")
-    dtype = pytestconfig.getoption("dtype")
-    kv_dtype = pytestconfig.getoption("kv_dtype")
-    dataset = pytestconfig.getoption("dataset")
-    device_group_num = pytestconfig.getoption("device_group")
-    if device_group_num == 1:
-        device_group = [0]
-    elif device_group_num == 4:
-        device_group = [0, 1, 2, 3]
-    elif device_group_num == 8:
-        device_group = [0, 1, 2, 3, 4, 5, 6, 7]
-
+@pytest.mark.parametrize("model_name", test_models)
+def test_output_consistency(model_name):
     sampling_params = SamplingParams(temperature=0.0, max_tokens=None)
 
     qllm = LLM(
@@ -98,9 +37,6 @@ def init(pytestconfig):
         kv_cache_dtype=kv_dtype,
         device="qaic",
     )
-
-
-def test_output_consistency():
     prompt = ["My name is"]
 
     output = qllm.generate(prompt * 5, sampling_params)
@@ -109,11 +45,26 @@ def test_output_consistency():
     for i, op in enumerate(output):
         check_output.append(op.outputs[0].text)
 
-    # print(check_output)
     assert len(set(check_output)) == 1, "Outputs from different slots for same prompt does not match!!"
 
+    del qllm
+    gc.collect()
 
-def test_generate():
+
+@pytest.mark.parametrize("model_name", test_models)
+def test_generate(model_name):
+    sampling_params = SamplingParams(temperature=0.0, max_tokens=None)
+
+    qllm = LLM(
+        model=model_name,
+        device_group=device_group,
+        max_num_seqs=decode_bsz,
+        max_model_len=ctx_len,
+        max_seq_len_to_capture=seq_len,
+        quantization=dtype,
+        kv_cache_dtype=kv_dtype,
+        device="qaic",
+    )
     outputDict = dict()
     prompt = [
         "My name is",
@@ -145,8 +96,24 @@ def test_generate():
     for key in outputDict.keys():
         assert len(set(outputDict[key])) == 1, "Outputs from different slots for same prompt does not match!!"
 
+    del qllm
+    gc.collect()
 
-def test_generated_tokens():
+
+@pytest.mark.parametrize("model_name", test_models)
+def test_generated_tokens(model_name):
+    sampling_params = SamplingParams(temperature=0.0, max_tokens=None)
+
+    qllm = LLM(
+        model=model_name,
+        device_group=device_group,
+        max_num_seqs=decode_bsz,
+        max_model_len=ctx_len,
+        max_seq_len_to_capture=seq_len,
+        quantization=dtype,
+        kv_cache_dtype=kv_dtype,
+        device="qaic",
+    )
     prompt = [
         "My name is",
         "How to eat mangosteen?",
@@ -170,3 +137,6 @@ def test_generated_tokens():
         continue
 
     assert len(prompt) == i + 1, "Number of Generated Tokens do not match the number of valid inputs!!"
+
+    del qllm
+    gc.collect()
