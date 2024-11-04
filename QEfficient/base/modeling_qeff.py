f24bb76b43018b743e2ada35198f05e6b3cab59d
Onkar Chougule <quic_ochougul@quicinc.com>
2024-11-05 02:12:12 +0530

    fixed test failures
    Signed-off-by: Onkar Chougule <quic_ochougul@quicinc.com>

--- QEfficient/base/modeling_qeff.py
+++ QEfficient/base/modeling_qeff.py
@@ -11,6 +11,7 @@ import json
 import logging
 import shutil
 import subprocess
+import warnings
 from abc import ABC, abstractmethod
 from pathlib import Path
 from typing import Dict, List, Optional
@@ -58,7 +59,7 @@ class QEFFBaseModel(ABC):
             any_transformed = any_transformed or transformed
 
         if not any_transformed:
-            logger.warning(f"No transforms applied to model: {self.model_name}. It may be an unsupported model!")
+            warnings.warn(f"No transforms applied to model: {self.model_name}. It may be an unsupported model!")
         else:
             logger.info(f"Pytorch transforms applied to model: {self.model_name}")
 
