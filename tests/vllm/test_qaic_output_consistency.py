43d77f46261b70addd010891dcddff913c965ffb
Mahesh Balasubramanian <quic_mahebala@quicinc.com>
2024-11-05 16:10:21 -0800

    Updated test and Jenkins file according to the comments
    Signed-off-by: Mahesh Balasubramanian <quic_mahebala@quicinc.com>

--- tests/vllm/test_qaic_output_consistency.py
+++ tests/vllm/test_qaic_output_consistency.py
@@ -5,68 +5,65 @@
 #
 # -----------------------------------------------------------------------------
 
-import gc
 import random
 
 import pytest
 from vllm import LLM, SamplingParams
 
+# Model to test
 test_models = [
     "TinyLlama/TinyLlama-1.1B-Chat-v1.0",
 ]
 
-seq_len = 128
-ctx_len = 256
-decode_bsz = 4
-dtype = "mxfp6"
-kv_dtype = "mxint8"
-device_group = [0]
+# Constants for configuration
+SEQ_LEN = 128
+CTX_LEN = 256
+DECOE_BSZ = 4
+DTYPE = "mxfp6"
+KV_DTYPE = "mxint8"
+DEVICE_GROUP = [0]
 
 
 @pytest.mark.parametrize("model_name", test_models)
 def test_output_consistency(model_name):
+    """This pytest function is used to check the consistency of vLLM.
+       1) Single prompt test to check if the output generated in 5 different
+          runs yields the same results
+       2) Multiple prompt check to test if multiple prompts yield same results
+          if run in different slots.
+
+    Parameters
+    ----------
+    model_name : string
+        Huggingface model card name.
+    """
     sampling_params = SamplingParams(temperature=0.0, max_tokens=None)
 
+    # Creating LLM Object
     qllm = LLM(
         model=model_name,
-        device_group=device_group,
-        max_num_seqs=decode_bsz,
-        max_model_len=ctx_len,
-        max_seq_len_to_capture=seq_len,
-        quantization=dtype,
-        kv_cache_dtype=kv_dtype,
+        device_group=DEVICE_GROUP,
+        max_num_seqs=DECOE_BSZ,
+        max_model_len=CTX_LEN,
+        max_seq_len_to_capture=SEQ_LEN,
+        quantization=DTYPE,
+        kv_cache_dtype=KV_DTYPE,
         device="qaic",
     )
-    prompt = ["My name is"]
 
-    output = qllm.generate(prompt * 5, sampling_params)
+    # Single prompt test
+    prompt1 = ["My name is"]
 
-    check_output = []
-    for i, op in enumerate(output):
-        check_output.append(op.outputs[0].text)
+    output1 = qllm.generate(prompt1 * 5, sampling_params)
 
-    assert len(set(check_output)) == 1, "Outputs from different slots for same prompt does not match!!"
-
-    del qllm
-    gc.collect()
+    check_output1 = []
+    for i, op in enumerate(output1):
+        check_output1.append(op.outputs[0].text)
 
 
-@pytest.mark.parametrize("model_name", test_models)
-def test_generate(model_name):
-    sampling_params = SamplingParams(temperature=0.0, max_tokens=None)
-
-    qllm = LLM(
-        model=model_name,
-        device_group=device_group,
-        max_num_seqs=decode_bsz,
-        max_model_len=ctx_len,
-        max_seq_len_to_capture=seq_len,
-        quantization=dtype,
-        kv_cache_dtype=kv_dtype,
-        device="qaic",
-    )
+    # Multiple prompt test
     outputDict = dict()
-    prompt = [
+    prompt2 = [
         "My name is",
         "How to eat mangosteen?",
         "How many people died in World War II",
@@ -83,60 +80,23 @@ def test_generate(model_name):
         "Where is Statue of Liberty located?",
     ]
 
-    for p in prompt:
+    for p in prompt2:
         outputDict[p] = []
 
     for _ in range(5):
-        random.shuffle(prompt)
-        output = qllm.generate(prompt, sampling_params)
-        for i, op in enumerate(output):
+        random.shuffle(prompt2)
+        output2 = qllm.generate(prompt2, sampling_params)
+        for i, op in enumerate(output2):
             generated_text = op.outputs[0].text
-            outputDict[prompt[i]].append(str(prompt[i] + generated_text))
+            outputDict[prompt2[i]].append(str(prompt2[i] + generated_text))
 
+    
+    # Assertion to check the consistency of single prompt.
+    assert len(set(check_output1)) == 1, "Outputs from different slots for same prompt does not match!!"
+
+    # Assertion to check multiple prompts.
     for key in outputDict.keys():
         assert len(set(outputDict[key])) == 1, "Outputs from different slots for same prompt does not match!!"
 
-    del qllm
-    gc.collect()
-
-
-@pytest.mark.parametrize("model_name", test_models)
-def test_generated_tokens(model_name):
-    sampling_params = SamplingParams(temperature=0.0, max_tokens=None)
-
-    qllm = LLM(
-        model=model_name,
-        device_group=device_group,
-        max_num_seqs=decode_bsz,
-        max_model_len=ctx_len,
-        max_seq_len_to_capture=seq_len,
-        quantization=dtype,
-        kv_cache_dtype=kv_dtype,
-        device="qaic",
-    )
-    prompt = [
-        "My name is",
-        "How to eat mangosteen?",
-        "How many people died in World War II",
-        "Hello ",
-        "Who is the president of United States",
-        "Who is the president of India",
-        "When it snowfalls in San Diego",
-        "In which country yamana river flows",
-        "How many people died in World War II",
-        "Thy youth is proud livery, so gazed on now",
-        "Will be a tattered weed, of small worth held:" "Then being asked where all thy beauty lies",
-        "Where all the treasure of thy lusty days",
-        "To say, within thine own deep-sunken eyes",
-        "Where is Statue of Liberty located?",
-    ]
-
-    output = qllm.generate(prompt, sampling_params)
-
-    for i, op in enumerate(output):
-        continue
-
-    assert len(prompt) == i + 1, "Number of Generated Tokens do not match the number of valid inputs!!"
-
-    del qllm
-    gc.collect()
+    # Assertion to check if any prompts are missed.
+    assert len(prompt2) == len(output2), "Number of Generated Tokens do not match the number of valid inputs!!"
