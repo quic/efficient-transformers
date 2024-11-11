74fc543f0afb8dc44860fcf1fe36f621c375985a
Meet Doshi <quic_meetdosh@quicinc.com>
2024-11-11 03:06:28 -0800

    Revert "Fixed intermediate shape from projection (#166)"
    This reverts commit b6778dfd9fc3c4dcc0ee7aa9332d4f8976ac6562.
    
    Signed-off-by: Meet Doshi <quic_meetdosh@quicinc.com>

--- QEfficient/transformers/models/gemma/modeling_gemma.py
+++ QEfficient/transformers/models/gemma/modeling_gemma.py
@@ -185,7 +185,7 @@ class QEffGemmaAttention(GemmaAttention):
 
         attn_output = attn_output.transpose(1, 2).contiguous()
 
-        attn_output = attn_output.reshape(bsz, q_len, -1)
+        attn_output = attn_output.reshape(bsz, q_len, self.hidden_size)
         attn_output = self.o_proj(attn_output)
 
         if not output_attentions:
