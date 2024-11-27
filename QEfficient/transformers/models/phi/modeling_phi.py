0565286d63e8f33bc6b6cfd3a55fffec82d10160
Rishin Raj <quic_rishinr@quicinc.com>
2024-11-27 06:09:47 +0000

    Format
    Signed-off-by: Rishin Raj <quic_rishinr@quicinc.com>
    Signed-off-by: Amit Raj <quic_amitraj@quicinc.com>

--- QEfficient/transformers/models/phi/modeling_phi.py
+++ QEfficient/transformers/models/phi/modeling_phi.py
@@ -161,7 +161,7 @@ class QEffPhiModel(PhiModel):
         output_hidden_states: Optional[bool] = None,
         return_dict: Optional[bool] = None,
         cache_position: Optional[torch.LongTensor] = None,
-    ) -> Union[Tuple, BaseModelOutputWithPast]:   
+    ) -> Union[Tuple, BaseModelOutputWithPast]:
         output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
         output_hidden_states = (
             output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
