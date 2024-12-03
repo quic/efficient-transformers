946358e63c1dbf35ab3e4635cee23b3b5eaecdc8
Ilango Rajagopal <quic_irajagop@quicinc.com>
2024-12-03 09:23:21 +0000

    GSM-8K dataset
    Signed-off-by: vbaddi <quic_vbaddi@quicinc.com>

--- QEfficient/finetune/dataset/dataset_config.py
+++ QEfficient/finetune/dataset/dataset_config.py
@@ -1,16 +1,17 @@
 from functools import partial
 
-from QEfficient.finetune.dataset.alpaca_dataset import (
+from dataset.alpaca_dataset import (
     InstructionDataset as get_alpaca_dataset,
 )
-from QEfficient.finetune.dataset.custom_dataset import (
+from dataset.custom_dataset import (
     get_custom_dataset,
     get_data_collator,
 )
-from QEfficient.finetune.dataset.grammer_dataset import (
+from dataset.grammer_dataset import (
     get_dataset as get_grammar_dataset,
 )
-from QEfficient.finetune.dataset.samsum_dataset import (
+from dataset.gsm8k_dataset import get_gsm8k_dataset
+from dataset.samsum_dataset import (
     get_preprocessed_samsum as get_samsum_dataset,
 )
 
@@ -18,6 +19,7 @@ DATASET_PREPROC = {
     "alpaca_dataset": partial(get_alpaca_dataset),
     "grammar_dataset": get_grammar_dataset,
     "samsum_dataset": get_samsum_dataset,
+    "gsm8k_dataset": get_gsm8k_dataset,
     "custom_dataset": get_custom_dataset,
 }
 DATALOADER_COLLATE_FUNC = {"custom_dataset": get_data_collator}
