c540cbe088fccfaba60ef0142e0b0d27adb52021
Vinayak Baddi <quic_vbaddi@quicinc.com>
2024-12-12 09:47:02 +0000

    nit: revert the qaicrt changes
    Signed-off-by: vbaddi <quic_vbaddi@quicinc.com>

--- QEfficient/__init__.py
+++ QEfficient/__init__.py
@@ -5,34 +5,24 @@
 #
 # -----------------------------------------------------------------------------
 
-try:
-    import torch_qaic  # noqa: F401
+from QEfficient.base import QEffAutoModel, QEFFAutoModelForCausalLM, QEFFCommonLoader
+from QEfficient.compile.compile_helper import compile
+from QEfficient.exporter.export_hf_to_cloud_ai_100 import qualcomm_efficient_converter
+from QEfficient.generation.text_generation_inference import cloud_ai_100_exec_kv
+from QEfficient.peft import QEffAutoPeftModelForCausalLM
+from QEfficient.transformers.transform import transform
 
-    torch_qaic_installed = True
-except ImportError:
-    torch_qaic_installed = False
+# Users can use QEfficient.export for exporting models to ONNX
+export = qualcomm_efficient_converter
+__version__ = "0.0.1.dev0"
 
-if not torch_qaic_installed:
-    from QEfficient.base import QEffAutoModel, QEFFAutoModelForCausalLM, QEFFCommonLoader
-    from QEfficient.compile.compile_helper import compile
-    from QEfficient.exporter.export_hf_to_cloud_ai_100 import qualcomm_efficient_converter
-    from QEfficient.generation.text_generation_inference import cloud_ai_100_exec_kv
-    from QEfficient.peft import QEffAutoPeftModelForCausalLM
-    from QEfficient.transformers.transform import transform
-
-    # Users can use QEfficient.export for exporting models to ONNX
-    export = qualcomm_efficient_converter
-    __version__ = "0.0.1.dev0"
-
-    __all__ = [
-        "transform",
-        "export",
-        "compile",
-        "cloud_ai_100_exec_kv",
-        "QEffAutoModel",
-        "QEFFAutoModelForCausalLM",
-        "QEffAutoPeftModelForCausalLM",
-        "QEFFCommonLoader",
-    ]
-else:
-    print("torch_qaic is installed, skipping QEfficient imports.")
+__all__ = [
+    "transform",
+    "export",
+    "compile",
+    "cloud_ai_100_exec_kv",
+    "QEffAutoModel",
+    "QEFFAutoModelForCausalLM",
+    "QEffAutoPeftModelForCausalLM",
+    "QEFFCommonLoader",
+]
