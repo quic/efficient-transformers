157142a274c821f34f5a53e1a6f60987ac4c5d1b
amitraj <quic_amitraj@quicinc.com>
2024-12-18 20:24:04 +0530

    Update ONNX_EXPORT_OPSET to 13
    Signed-off-by: amitraj <quic_amitraj@quicinc.com>

--- QEfficient/utils/constants.py
+++ QEfficient/utils/constants.py
@@ -47,7 +47,7 @@ ONNX_EXPORT_EXAMPLE_BATCH_SIZE = 1
 ONNX_EXPORT_EXAMPLE_SEQ_LEN = 32
 ONNX_EXPORT_EXAMPLE_FBS = 4
 ONNX_EXPORT_EXAMPLE_NLK = 2  # Number of Logits to Keep
-ONNX_EXPORT_OPSET = 14
+ONNX_EXPORT_OPSET = 13
 
 COMPILER = ["/opt/qti-aic/exec/qaic-exec", "-aic-hw", "-aic-hw-version=2.0"]
 
