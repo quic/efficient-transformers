a08d4eb85e13b3683e6da39b7729c9bde6ae7844
Mamta Singh <quic_mamtsing@quicinc.com>
2024-12-27 12:28:40 +0000

    Bypass qeff import if aic sdk not installed
    Signed-off-by: Mamta Singh <quic_mamtsing@quicinc.com>

--- QEfficient/__init__.py
+++ QEfficient/__init__.py
@@ -5,24 +5,34 @@
 #
 # -----------------------------------------------------------------------------
 
-from QEfficient.base import QEFFAutoModel, QEFFAutoModelForCausalLM, QEFFCommonLoader
-from QEfficient.compile.compile_helper import compile
-from QEfficient.exporter.export_hf_to_cloud_ai_100 import qualcomm_efficient_converter
-from QEfficient.generation.text_generation_inference import cloud_ai_100_exec_kv
-from QEfficient.peft import QEffAutoPeftModelForCausalLM
-from QEfficient.transformers.transform import transform
+try:
+    import qaicrt  # noqa: F401
 
-# Users can use QEfficient.export for exporting models to ONNX
-export = qualcomm_efficient_converter
-__version__ = "0.0.1.dev0"
+    qaic_sdk_installed = True
+except ModuleNotFoundError:
+    qaic_sdk_installed = False
 
-__all__ = [
-    "transform",
-    "export",
-    "compile",
-    "cloud_ai_100_exec_kv",
-    "QEFFAutoModel",
-    "QEFFAutoModelForCausalLM",
-    "QEffAutoPeftModelForCausalLM",
-    "QEFFCommonLoader",
-]
+if qaic_sdk_installed:
+    from QEfficient.base import QEffAutoModel, QEFFAutoModelForCausalLM, QEFFCommonLoader
+    from QEfficient.compile.compile_helper import compile
+    from QEfficient.exporter.export_hf_to_cloud_ai_100 import qualcomm_efficient_converter
+    from QEfficient.generation.text_generation_inference import cloud_ai_100_exec_kv
+    from QEfficient.peft import QEffAutoPeftModelForCausalLM
+    from QEfficient.transformers.transform import transform
+
+    # Users can use QEfficient.export for exporting models to ONNX
+    export = qualcomm_efficient_converter
+    __version__ = "0.0.1.dev0"
+
+    __all__ = [
+        "transform",
+        "export",
+        "compile",
+        "cloud_ai_100_exec_kv",
+        "QEffAutoModel",
+        "QEFFAutoModelForCausalLM",
+        "QEffAutoPeftModelForCausalLM",
+        "QEFFCommonLoader",
+    ]
+else:
+    print("QAIC SDK is not found, skipping QEfficient imports.")
