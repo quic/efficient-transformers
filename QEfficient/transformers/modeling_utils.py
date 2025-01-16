74689ca8826a93938d76744d5c4cd542f5f441bd
Kushal Dulla <quic_kdulla@quicinc.com>
2025-01-16 04:58:14 +0000

    Initial changes for onboarding Whisper
    Signed-off-by: Kushal Dulla <quic_kdulla@quicinc.com>

--- QEfficient/transformers/modeling_utils.py
+++ QEfficient/transformers/modeling_utils.py
@@ -75,6 +75,13 @@ from transformers.models.starcoder2.modeling_starcoder2 import (
     Starcoder2ForCausalLM,
     Starcoder2Model,
 )
+from transformers.models.whisper.modeling_whisper import (
+    WhisperAttention,
+    WhisperDecoderLayer,
+    WhisperEncoder,
+    WhisperDecoder,
+    WhisperForConditionalGeneration,
+)
 
 from QEfficient.customop import CustomRMSNormAIC
 
@@ -133,6 +140,12 @@ from .models.starcoder2.modeling_starcoder2 import (
     QEffStarcoder2ForCausalLM,
     QEffStarcoder2Model,
 )
+from .models.whisper.modeling_whisper import (
+    QEffWhisperAttention,
+    QEffWhisperDecoderLayer,
+    QEffWhisperEncoder,
+    QEffWhisperDecoder,
+)
 
 # Define a named tuple for ModelArchitectures
 # Required for the Automation tool
@@ -157,6 +170,7 @@ qeff_supported_architectures = ModelArchitectures(
         Starcoder2ForCausalLM.__name__,
         GPTBigCodeForCausalLM.__name__,
         MllamaForCausalLM.__name__,
+        WhisperForConditionalGeneration.__name__,
     ]
 )
 
@@ -241,4 +255,10 @@ TransformersToQEffModulesDict: Dict[Type[nn.Module], Type[nn.Module]] = {
     GPTBigCodeAttention: QEffGPTBigCodeAttention,
     GPTBigCodeBlock: QEffGPTBigCodeBlock,
     GPTBigCodeModel: QEffGPTBigCodeModel,
+    # Whisper encoder and decoder layers
+    WhisperAttention: QEffWhisperAttention,
+    WhisperDecoderLayer: QEffWhisperDecoderLayer,
+    WhisperEncoder: QEffWhisperEncoder,
+    WhisperDecoder: QEffWhisperDecoder,
 }
+
