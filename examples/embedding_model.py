81ed2a47b384adab3d60133c7d77dd271791d884
Amit Raj <quic_amitraj@quicinc.com>
2025-01-23 16:26:48 +0530

    Ruff check and format
    Signed-off-by: Amit Raj <quic_amitraj@quicinc.com>

--- examples/embedding_model.py
+++ examples/embedding_model.py
@@ -6,32 +6,35 @@
 # -----------------------------------------------------------------------------
 
 # This is the work example of the Embedding model with the AI 100
-from transformers import AutoTokenizer, AutoModel
 import torch
 import torch.nn.functional as F
+from transformers import AutoTokenizer
+
 from QEfficient import QEFFAutoModel as AutoModel
 
+
 def mean_pooling(model_output, attention_mask):
-    token_embeddings = model_output#First element of model_output contains all token embeddings
+    token_embeddings = model_output  # First element of model_output contains all token embeddings
     input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
     return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
 
+
 # Sentences we want sentence embeddings for
-sentences = 'This is an example sentence'
+sentences = "This is an example sentence"
 
 # Load model from HuggingFace Hub
-tokenizer = AutoTokenizer.from_pretrained('sentence-transformers/all-MiniLM-L6-v2')
+tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
 
 
-qeff_model = AutoModel.from_pretrained('sentence-transformers/all-MiniLM-L6-v2')
+qeff_model = AutoModel.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
 qeff_model.compile(num_cores=14)
 
 # Tokenize sentences
-encoded_input = tokenizer(sentences, return_tensors='pt')
-qeff_output= torch.tensor(qeff_model.generate(encoded_input))
+encoded_input = tokenizer(sentences, return_tensors="pt")
+qeff_output = torch.tensor(qeff_model.generate(encoded_input))
 
 # Perform pooling
-sentence_embeddings = mean_pooling(qeff_output, encoded_input['attention_mask'])
+sentence_embeddings = mean_pooling(qeff_output, encoded_input["attention_mask"])
 
 # Normalize embeddings
 sentence_embeddings = F.normalize(sentence_embeddings, p=2, dim=1)
