826cd5587c858183a2e8d954b85d7be88f555c14
Onkar Chougule <quic_ochougul@quicinc.com>
2025-02-10 17:26:59 +0530

    format
    Signed-off-by: Onkar Chougule <quic_ochougul@quicinc.com>

--- tests/transformers/models/test_image_text_to_text_intern.py
+++ tests/transformers/models/test_image_text_to_text_intern.py
@@ -14,8 +14,8 @@ from PIL import Image
 from torchvision.transforms.functional import InterpolationMode
 from transformers import AutoConfig, AutoTokenizer, TextStreamer
 
-from conversation import get_conv_template
 from QEfficient import QEFFAutoModelForCausalLM
+from tests.transformers.models.conversation import get_conv_template
 
 IMAGENET_MEAN = (0.485, 0.456, 0.406)
 IMAGENET_STD = (0.229, 0.224, 0.225)
