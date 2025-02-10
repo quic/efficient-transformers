d42b111e58bf559a56972ae19e0b615bf164e292
Onkar Chougule <quic_ochougul@quicinc.com>
2025-02-10 17:16:37 +0530

    added copyright
    Signed-off-by: Onkar Chougule <quic_ochougul@quicinc.com>

--- tests/transformers/models/test_image_text_to_text_intern.py
+++ tests/transformers/models/test_image_text_to_text_intern.py
@@ -1,3 +1,10 @@
+# -----------------------------------------------------------------------------
+#
+# Copyright (c) 2025 Qualcomm Innovation Center, Inc. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# -----------------------------------------------------------------------------
+
 import copy
 
 import numpy as np
