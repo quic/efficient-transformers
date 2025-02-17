86146b84f8e75d09832d493176585c02f5b8b39c
Ann Kuruvilla <quic_akuruvil@quicinc.com>
2025-02-17 06:05:17 +0000

    Format
    Signed-off-by: Ann <quic_akuruvil@quicinc.com>

--- QEfficient/transformers/models/pytorch_transforms.py
+++ QEfficient/transformers/models/pytorch_transforms.py
@@ -8,8 +8,8 @@
 from types import MethodType
 from typing import Tuple
 
-import transformers
 from torch import nn
+import transformers
 from transformers.models.codegen.modeling_codegen import (
     CodeGenAttention,
     CodeGenBlock,
