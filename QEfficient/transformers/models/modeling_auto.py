546c434819765ad9537b2a1096d6bbe4bddabd74
shubhagr-quic <quic_shubhagr@quicinc.com>
2025-02-21 16:49:44 +0530

    Support for Prefix caching Feature in QNN Compilation Path. (#262)
    Signed-off-by: Shubham Agrawal <quic_shubhagr@quicinc.com>
    Signed-off-by: Hem Agnihotri <quic_hemagnih@quicinc.com>

--- QEfficient/transformers/models/modeling_auto.py
+++ QEfficient/transformers/models/modeling_auto.py
@@ -1503,6 +1503,7 @@ class QEFFAutoModelForCausalLM(QEFFBaseModel):
                 mxfp6_matmul=mxfp6_matmul,
                 mxint8_kv_cache=mxint8_kv_cache,
                 qnn_config=qnn_config,
+                kv_cache_batch_size=kv_cache_batch_size,
             )
         else:
             # Custom IO
