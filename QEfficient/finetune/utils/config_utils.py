48cb313316ba2c801d94e43829e018a4afbb4590
Meet Patel <quic_meetkuma@quicnic.com>
2025-02-28 13:43:43 +0530

    Added finetuning support for BERT based models on IMDB dataset.
    Signed-off-by: meetkuma <meetkuma@qti.qualcomm.com>

--- QEfficient/finetune/utils/config_utils.py
+++ QEfficient/finetune/utils/config_utils.py
@@ -16,7 +16,7 @@ from peft import (
     PrefixTuningConfig,
 )
 from transformers import default_data_collator
-from transformers.data import DataCollatorForSeq2Seq
+from transformers.data import DataCollatorForSeq2Seq, DefaultDataCollator
 
 import QEfficient.finetune.configs.dataset_config as datasets
 from QEfficient.finetune.configs.peft_config import lora_config, prefix_config
@@ -88,7 +88,10 @@ def get_dataloader_kwargs(train_config, dataset, dataset_processer, mode):
                     num_replicas=dist.get_world_size(),
                     shuffle=False,
                 )
-                kwargs["collate_fn"] = DataCollatorForSeq2Seq(dataset_processer)
+                if train_config.task_type == "seq_classification":
+                    kwargs["collate_fn"] = DefaultDataCollator(dataset_processer)
+                else:
+                    kwargs["collate_fn"] = DataCollatorForSeq2Seq(dataset_processer)
         else:
             kwargs["sampler"] = data_utils.DistributedSampler(
                 dataset, num_replicas=dist.get_world_size(), rank=dist.get_rank(), shuffle=True
