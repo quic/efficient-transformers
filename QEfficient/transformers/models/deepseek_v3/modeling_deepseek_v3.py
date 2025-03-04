db232c738d72cf0e345493c5091888f53075aa32
Vinayak Baddi <quic_vbaddi@quicinc.com>
2025-03-04 06:23:19 +0000

    Add sample example for deepseek
    Signed-off-by: Ann Kuruvilla <quic_akuruvil@quicinc.com>

--- QEfficient/transformers/models/deepseek_v3/modeling_deepseek_v3.py
+++ QEfficient/transformers/models/deepseek_v3/modeling_deepseek_v3.py
@@ -1,3 +1,10 @@
+# -----------------------------------------------------------------------------
+#
+# Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# -----------------------------------------------------------------------------
+
 from typing import List, Optional, Tuple, Union
 
 import torch
