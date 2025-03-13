04bfb9d3ea6ab706e0e7282bb4fd6446a5fb41ea
Hem Agnihotri <quic_hemagnih@quicinc.com>
2025-03-13 01:55:34 +0000

    Added Infra in QEfficient for execution of models whose modelling file is not present at hugging face and checkpoint like swiftkv
    Signed-off-by: Hem Agnihotri <quic_hemagnih@quicinc.com>

---
+++ QEfficient/transformers/models/llama_swiftkv/__init__.py
@@ -0,0 +1,6 @@
+# -----------------------------------------------------------------------------
+#
+# Copyright (c) 2025 Qualcomm Innovation Center, Inc. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# -----------------------------------------------------------------------------
