1120ef68b4736bec08674c6d1b4655c037ea6358
Hem Agnihotri <quic_hemagnih@quicinc.com>
2025-03-13 01:55:34 +0000

    Added Infra in QEfficient for execution of models whose modelling file is not present at hugging face and checkpoint like swiftkv
    Signed-off-by: Hem Agnihotri <quic_hemagnih@quicinc.com>

--- QEfficient/__init__.py
+++ QEfficient/__init__.py
@@ -1,6 +1,6 @@
 # -----------------------------------------------------------------------------
 #
-# Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+# Copyright (c) 2025 Qualcomm Innovation Center, Inc. All rights reserved.
 # SPDX-License-Identifier: BSD-3-Clause
 #
 # -----------------------------------------------------------------------------
@@ -12,7 +12,25 @@ import os
 # hf_transfer is imported (will happen on line 15 via leading imports)
 os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
 
+from transformers import AutoConfig
+
+from QEfficient.transformers.modeling_utils import (
+    MODEL_TYPE_TO_CONFIG_CLS_AND_ARCH_CLS,
+    get_auto_model_class,
+    get_model_class_type_from_model_type,
+)
 from QEfficient.utils.logging_utils import logger
+
+# loop over all the model types which are not present in transformers and register them
+for model_type, model_cls in MODEL_TYPE_TO_CONFIG_CLS_AND_ARCH_CLS.items():
+    # Register the model config class based on the model type. This will be first element in the tuple
+    AutoConfig.register(model_type, model_cls[0])
+
+    model_class_type = get_model_class_type_from_model_type(model_type)
+    AutoModelClassName = get_auto_model_class(model_class_type, model_cls[1])
+
+    # Register the non transformer library Class and config class using AutoModelClass
+    AutoModelClassName.register(model_cls[0], model_cls[1])
 
 
 def check_qaic_sdk():
