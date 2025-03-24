003fe3bad8ca4e887fd79909de7647925c88d885
Amit Raj <quic_amitraj@quicinc.com>
2025-03-24 10:02:07 +0000

    Enabled gemma2, codegen, falcon
    Signed-off-by: Amit Raj <quic_amitraj@quicinc.com>

--- QEfficient/transformers/models/mpt/modeling_mpt.py
+++ QEfficient/transformers/models/mpt/modeling_mpt.py
@@ -12,14 +12,13 @@ from typing import Optional, Tuple, Union
 import torch
 import torch.utils.checkpoint
 from torch import nn
-from torch.nn import CrossEntropyLoss
 from transformers.cache_utils import DynamicCache
 from transformers.modeling_attn_mask_utils import _prepare_4d_causal_attention_mask
 from transformers.modeling_outputs import (
     BaseModelOutputWithPastAndCrossAttentions,
     CausalLMOutputWithCrossAttentions,
 )
-from transformers.models.mpt.modeling_mpt import MptAttention, MptBlock, MptForCausalLM, MptModel, logger
+from transformers.models.mpt.modeling_mpt import MptAttention, MptBlock, MptForCausalLM, MptModel
 
 from QEfficient.transformers.modeling_attn_mask_utils import _create_causal_mask
 
