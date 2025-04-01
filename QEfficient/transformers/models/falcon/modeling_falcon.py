5ee5591cc61d4ee2d28ea5e444d2a5aeddcd4047
Amit Raj <quic_amitraj@quicinc.com>
2025-04-01 04:58:01 +0000

    falcon, gemma, gemma2, mistral, qwen clean up
    Signed-off-by: Amit Raj <quic_amitraj@quicinc.com>

--- QEfficient/transformers/models/falcon/modeling_falcon.py
+++ QEfficient/transformers/models/falcon/modeling_falcon.py
@@ -69,37 +69,6 @@ class QEffFalconRotaryEmbedding(FalconRotaryEmbedding):
         )
 
 
-def qeff_apply_rotary_pos_emb(q, k, cos, sin, position_ids, unsqueeze_dim=1):
-    """Applies Rotary Position Embedding to the query and key tensors.
-
-    Args:
-        q (`torch.Tensor`): The query tensor.
-        k (`torch.Tensor`): The key tensor.
-        cos (`torch.Tensor`): The cosine part of the rotary embedding.
-        sin (`torch.Tensor`): The sine part of the rotary embedding.
-        position_ids (`torch.Tensor`):
-            The position indices of the tokens corresponding to the query and key tensors. For example, this can be
-            used to pass offsetted position ids when working with a KV-cache.
-        unsqueeze_dim (`int`, *optional*, defaults to 1):
-            The 'unsqueeze_dim' argument specifies the dimension along which to unsqueeze cos[position_ids] and
-            sin[position_ids] so that they can be properly broadcasted to the dimensions of q and k. For example, note
-            that cos[position_ids] and sin[position_ids] have the shape [batch_size, seq_len, head_dim]. Then, if q and
-            k have the shape [batch_size, heads, seq_len, head_dim], then setting unsqueeze_dim=1 makes
-            cos[position_ids] and sin[position_ids] broadcastable to the shapes of q and k. Similarly, if q and k have
-            the shape [batch_size, seq_len, heads, head_dim], then set unsqueeze_dim=2.
-    Returns:
-        `tuple(torch.Tensor)` comprising of the query and key tensors rotated using the Rotary Position Embedding.
-    """
-    cos = cos.unsqueeze(unsqueeze_dim)
-    sin = sin.unsqueeze(unsqueeze_dim)
-
-    # Apply rotation
-    q_embed = (q * cos) + (rotate_half(q) * sin)
-    k_embed = (k * cos) + (rotate_half(k) * sin)
-    # Cast back to original dtype
-    return q_embed.to(q.dtype), k_embed.to(k.dtype)
-
-
 class QEffFalconAttention(FalconAttention):
     """
     Copied from FalconAttention: https://github.com/huggingface/transformers/blob/main/src/transformers/models/falcon/modeling_falcon.py
@@ -289,6 +258,7 @@ class QEffFalconModel(FalconModel):
         batch_size, seq_length, _ = inputs_embeds.shape
 
         if cache_position is None:
+            past_seen_tokens = past_key_values.get_seq_length() if past_key_values is not None else 0
             cache_position = torch.arange(
                 past_key_values_length, past_key_values_length + seq_length, device=inputs_embeds.device
             )
@@ -296,16 +266,7 @@ class QEffFalconModel(FalconModel):
         if position_ids is None:
             position_ids = cache_position.unsqueeze(0)
         alibi = None
-        causal_mask = self._update_causal_mask(
-            attention_mask,
-            inputs_embeds,
-            cache_position,
-            position_ids,
-            past_key_values,
-            output_attentions,
-            head_mask,
-            alibi,
-        )
+        causal_mask = _create_causal_mask(position_ids=position_ids, target_length=past_seen_tokens)
 
         # Prepare head mask if needed
         # 1.0 in head_mask indicate we keep the head
@@ -358,28 +319,6 @@ class QEffFalconModel(FalconModel):
             hidden_states=all_hidden_states,
             attentions=all_self_attentions,
         )
-
-    def _update_causal_mask(
-        self,
-        attention_mask: torch.Tensor,
-        input_tensor: torch.Tensor,
-        cache_position: torch.Tensor,
-        position_ids: torch.Tensor,
-        past_key_values: Cache,
-        output_attentions: bool,
-        head_mask: torch.Tensor,
-        alibi: torch.Tensor,
-    ):
-        past_seen_tokens = past_key_values.get_seq_length() if past_key_values is not None else 0
-        batch_size, sequence_length, _ = input_tensor.shape
-        target_length = (
-            attention_mask.shape[-1] if isinstance(attention_mask, torch.Tensor) else past_seen_tokens + sequence_length
-        )
-        # In case the provided `attention` mask is 2D, we generate a causal mask here (4D).
-        causal_mask = _create_causal_mask(position_ids=position_ids, target_length=target_length)
-
-        return causal_mask
-
 
 class QEffFalconForCausalLM(FalconForCausalLM):
     """
