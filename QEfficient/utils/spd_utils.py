f251ecfe2d971402af572e6d4a301d7fa5157ffa
eplatero <quic_eplatero@quicinc.com>
2025-04-10 13:03:51 -0500

    added explicit errors rather than asserts
    Signed-off-by: eplatero <quic_eplatero@quicinc.com>

--- QEfficient/utils/spd_utils.py
+++ QEfficient/utils/spd_utils.py
@@ -14,7 +14,10 @@ from QEfficient.utils.helper_utils import filter_kwargs
 
 
 def _get_speculative_config(speculative_model, **kwargs) -> dict:
-    assert isinstance(speculative_model, (str, Path))
+    if not isinstance(speculative_model, (str, Path)):
+        raise ValueError(
+            f"`speculative_config` must be a string or Path object but is of type {type(speculative_model)}"
+        )
     try:
         speculative_config, _ = PretrainedConfig.get_config_dict(
             speculative_model, _configuration_file="speculator_config.json", **kwargs
@@ -29,9 +32,11 @@ def get_speculative_weights(speculative_model, **kwargs) -> str:
     turbo_weights_file = "speculator.safetensors"
     hf_hub_kwargs = filter_kwargs(hf_hub_download, kwargs)
     if (local_path := Path(speculative_model)).exists():
-        assert local_path.is_dir()
+        if not local_path.is_dir():
+            raise ValueError(f"local model path {local_path} must point to an existing dictionary")
         weights_path = local_path / turbo_weights_file
-        assert weights_path.exists()
+        if not weights_path.exists():
+            raise FileNotFoundError(f"weights path {weights_path} does not exist.")
     else:
         weights_path = hf_hub_download(speculative_model, filename=turbo_weights_file, **hf_hub_kwargs)
     return str(weights_path)
