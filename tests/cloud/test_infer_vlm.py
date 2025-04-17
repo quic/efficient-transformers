9cde30bca0682e3efe84efe379d8bedcf27ef6b2
Asmita Goswami <quic_asmigosw@quicinc.com>
2025-04-17 09:17:35 +0000

    Added Copyrights
    Signed-off-by: Asmita Goswami <quic_asmigosw@quicinc.com>

--- tests/cloud/test_infer_vlm.py
+++ tests/cloud/test_infer_vlm.py
@@ -1,3 +1,10 @@
+# -----------------------------------------------------------------------------
+#
+# Copyright (c) 2025 Qualcomm Innovation Center, Inc. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# -----------------------------------------------------------------------------
+
 import pytest
 
 from QEfficient.cloud.infer import main as infer
