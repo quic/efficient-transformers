9a27775c17048201584cda67bb47804b648929ea
Mohit Soni <quic_mohisoni@quicinc.com>
2025-05-09 14:07:53 +0530

    Updating Chunking method (#398)
    Signed-off-by: Mohit Soni <quic_mohisoni@quicinc.com>
    Signed-off-by: Mohit Soni <mohisoni@qti.qualcomm.com>
    Signed-off-by: Dipankar Sarkar <dipankar@qti.qualcomm.com>

--- QEfficient/transformers/models/modeling_auto.py
+++ QEfficient/transformers/models/modeling_auto.py
@@ -803,8 +803,8 @@ class _QEffAutoModelForImageTextToTextDualQPC:
         input_len = inputs["attention_mask"].sum(1, keepdims=True)
         input_ids_length = inputs["input_ids"].shape[1]
         num_chunks = -(input_ids_length // -prefill_seq_len)  # ceil divide without float
-        padded_len = num_chunks * prefill_seq_len  # Convert to a multiple of prompt_len
-
+        # padded_len = num_chunks * prefill_seq_len  # Convert to a multiple of prompt_len
+        padded_len = vision_session.bindings[vision_session.binding_index_map["input_ids"]].dims[1]
         if generation_len is None:
             generation_len = ctx_len - input_len.max()
         assert generation_len > 0, "generation length should be greater than zero"
@@ -841,6 +841,7 @@ class _QEffAutoModelForImageTextToTextDualQPC:
         vision_end = perf_counter()
 
         lang_inputs = {k: v for k, v in inputs.items() if k not in vision_inputs}
+        lang_inputs["input_ids"] = inputs["input_ids"]
         lang_inputs["position_ids"] = np.where(
             lang_inputs.pop("attention_mask"), np.arange(padded_len), -1
         )  # Need to use -1 as position_ids for invalid tokens
@@ -862,6 +863,9 @@ class _QEffAutoModelForImageTextToTextDualQPC:
         for i in range(num_chunks):
             chunk_inputs["input_ids"] = lang_inputs["input_ids"][:, i * prefill_seq_len : (i + 1) * prefill_seq_len]
             chunk_inputs["position_ids"] = lang_inputs["position_ids"][
+                :, i * prefill_seq_len : (i + 1) * prefill_seq_len
+            ]
+            chunk_inputs["vision_embeds"] = lang_inputs["vision_embeds"][
                 :, i * prefill_seq_len : (i + 1) * prefill_seq_len
             ]
             outputs = lang_session.run(chunk_inputs)
@@ -1890,7 +1894,7 @@ class QEFFAutoModelForCausalLM(QEFFBaseModel):
         elif num_speculative_tokens is None:
             raise TypeError("missing required argument `num_speculative_tokens` as `is_tlm` instance variable is True.")
 
-        if not isinstance(num_speculative_tokens, int) and num_speculative_tokens < 2:
+        if not isinstance(num_speculative_tokens, int) and num_speculative_tokens cloud_ai_100_generate 2:
             ValueError(
                 f"`num_speculative_tokens` arg should be an integer greater than 1, got {num_speculative_tokens}"
             )
