4ef9b280032e7ccbb6c0b424d3d02512654d61f8
Mohit Soni <quic_mohisoni@quicinc.com>
2025-05-19 15:37:51 +0530

    Updating index method in Wrappers (#410)
    Signed-off-by: Mohit Soni <quic_mohisoni@quicinc.com>
    Signed-off-by: Amit Raj <quic_amitraj@quicinc.com>

--- QEfficient/transformers/models/modeling_auto.py
+++ QEfficient/transformers/models/modeling_auto.py
@@ -803,8 +803,8 @@ class _QEffAutoModelForImageTextToTextDualQPC:
         input_len = inputs["attention_mask"].sum(1, keepdims=True)
         input_ids_length = inputs["input_ids"].shape[1]
         num_chunks = -(input_ids_length // -prefill_seq_len)  # ceil divide without float
-        # padded_len = num_chunks * prefill_seq_len  # Convert to a multiple of prompt_len
-        padded_len = vision_session.bindings[0].dims[1]
+        padded_len = num_chunks * prefill_seq_len  # Convert to a multiple of prompt_len
+
         if generation_len is None:
             generation_len = ctx_len - input_len.max()
         assert generation_len > 0, "generation length should be greater than zero"
@@ -841,7 +841,6 @@ class _QEffAutoModelForImageTextToTextDualQPC:
         vision_end = perf_counter()
 
         lang_inputs = {k: v for k, v in inputs.items() if k not in vision_inputs}
-        lang_inputs["input_ids"] = inputs["input_ids"]
         lang_inputs["position_ids"] = np.where(
             lang_inputs.pop("attention_mask"), np.arange(padded_len), -1
         )  # Need to use -1 as position_ids for invalid tokens
@@ -854,9 +853,10 @@ class _QEffAutoModelForImageTextToTextDualQPC:
             vision_session.deactivate()
         lang_session.activate()
 
-        # lang_session.set_buffers(vision_outputs)
-        lang_inputs["vision_embeds"] = vision_outputs["vision_embeds"]
+        lang_session.set_buffers(vision_outputs)
+
         # Prepare inputs for prefill
+        chunk_inputs = lang_inputs.copy()
         prefill_start = perf_counter()
 
         # Prepare inputs for prefill
@@ -871,15 +871,11 @@ class _QEffAutoModelForImageTextToTextDualQPC:
             chunk_inputs["position_ids"] = lang_inputs["position_ids"][
                 :, i * prefill_seq_len : (i + 1) * prefill_seq_len
             ]
-            chunk_inputs["vision_embeds"] = lang_inputs["vision_embeds"][
-                :, i * prefill_seq_len : (i + 1) * prefill_seq_len
-            ]
             outputs = lang_session.run(chunk_inputs)
             chunk_inputs["image_idx"] = outputs["image_idx_output"]
 
         prefill_time = perf_counter() - prefill_start + vision_end - vision_start
         # Skip inputs/outputs again
-        lang_inputs["vision_embeds"] = lang_inputs["vision_embeds"][:, :prefill_seq_len]
         lang_session.skip_buffers(
             [
                 x
