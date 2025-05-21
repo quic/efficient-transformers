d9d79ade8c20ecae1eec9bab21b82380d86587a9
Asmita Goswami <asmigosw@qti.qualcomm.com>
2025-05-21 10:03:26 +0000

    Update new model_registery file to register non-transformer models
    Signed-off-by: Asmita Goswami <quic_asmigosw@quicinc.com>

--- QEfficient/__init__.py
+++ QEfficient/__init__.py
@@ -12,27 +12,9 @@ import os
 # hf_transfer is imported (will happen on line 15 via leading imports)
 os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
 
-from transformers import AutoConfig, AutoModelForCausalLM
-
-# Placeholder for all non-transformer models
-from QEfficient.transformers.models.llama_swiftkv.modeling_llama_swiftkv import (
-    QEffLlamaSwiftKVConfig,
-    QEffLlamaSwiftKVForCausalLM,
-)
+# Placeholder for all non-transformer models registered in QEfficient
+import QEfficient.utils.model_registery  # noqa: F401
 from QEfficient.utils.logging_utils import logger
-
-# Map of model type to config class, Modelling class and transformer model architecture class
-MODEL_TYPE_TO_CONFIG_CLS_AND_ARCH_CLS = {
-    "llama_swiftkv": [QEffLlamaSwiftKVConfig, QEffLlamaSwiftKVForCausalLM, AutoModelForCausalLM],
-}
-
-# loop over all the model types which are not present in transformers and register them
-for model_type, model_cls in MODEL_TYPE_TO_CONFIG_CLS_AND_ARCH_CLS.items():
-    # Register the model config class based on the model type. This will be first element in the tuple
-    AutoConfig.register(model_type, model_cls[0])
-
-    # Register the non transformer library Class and config class using AutoModelClass
-    model_cls[2].register(model_cls[0], model_cls[1])
 
 
 def check_qaic_sdk():
