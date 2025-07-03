2a4f02c4032681c553ec496b8a09fca6efb80721
Rishin Raj <rishinr@qti.qualcomm.com>
2025-07-03 13:50:50 +0530

    MDP hash support (#479)
    Signed-off-by: Rishin Raj <rishinr@qti.qualcomm.com>
    Signed-off-by: Dhiraj Kumar Sah <dhirajku@qti.qualcomm.com>

--- QEfficient/utils/__init__.py
+++ QEfficient/utils/__init__.py
@@ -11,8 +11,10 @@ from QEfficient.transformers.quantizers.auto import (  # noqa: F401
 )
 from QEfficient.utils._utils import (  # noqa: F401
     check_and_assign_cache_dir,
+    create_json,
     custom_format_warning,
     dump_qconfig,
+    generate_mdp_partition_config,
     get_num_layers_from_config,
     get_num_layers_vlm,
     get_onnx_dir_name,
@@ -24,6 +26,7 @@ from QEfficient.utils._utils import (  # noqa: F401
     hf_download,
     load_hf_processor,
     load_hf_tokenizer,
+    load_json,
     login_and_download_hf_lm,
     make_serializable,
     onnx_exists,
