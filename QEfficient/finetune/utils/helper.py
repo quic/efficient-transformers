458675e681a0bc9a1dd237bda07225b184599fb7
Swati Allabadi <quic_sallabad@quicinc.com>
2025-07-09 15:40:14 +0530

    [QEff Finetune] Fix for padding dataset  (#504)
    1. Padding the dataset with dummy samples (they won't contribute in
    total_loss) to make the #samples a multiple of degree of ddp*batch_size)
    to avoid dropping of any samples in case of
    a) Fine tuning through DDP
    b) train_batch_size > 1 or val_batch_size > 1
    2. Repositioned loss = loss /gradient_accumulation_steps
    3. Corrected the calculation of average loss in case when
    --max_train_steps or --max_val_steps flags are set.
    
    ---------
    
    Signed-off-by: Swati Allabadi <sallabad@qti.qualcomm.com>
    Co-authored-by: Swati Allabadi <sallabad@qti.qualcomm.com>

--- QEfficient/finetune/utils/helper.py
+++ QEfficient/finetune/utils/helper.py
@@ -4,8 +4,13 @@
 # SPDX-License-Identifier: BSD-3-Clause
 #
 # -----------------------------------------------------------------------------
+import os
 
 TASK_TYPE = ["generation", "seq_classification"]
 PEFT_METHOD = ["lora"]
 DEVICE = ["qaic", "cpu", "cuda"]
 BATCHING_STRATEGY = ["padding", "packing"]
+
+
+def get_num_ddp_devices():
+    return int(os.getenv("WORLD_SIZE", 1))
