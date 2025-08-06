753f110ed502f0a679fa5d6e15c7cbd6b520311f
Vinayak Baddi <quic_vbaddi@quicinc.com>
2025-08-06 19:05:18 +0000

    nit: add header to __init__
    Signed-off-by: vbaddi <quic_vbaddi@quicinc.com>
    Signed-off-by: Onkar Chougule <ochougul@qti.qualcomm.com>

--- QEfficient/transformers/models/gpt_oss/__init__.py
+++ QEfficient/transformers/models/gpt_oss/__init__.py
@@ -1,1 +1,6 @@
+# -----------------------------------------------------------------------------
 #
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# ----------------------------------------------------------------------------
