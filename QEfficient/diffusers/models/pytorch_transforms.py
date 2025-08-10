fcb6b11989867f6e8e3b9340b2493a14a79e0163
Amit Raj <amitraj@qti.qualcomm.com>
2025-08-10 06:05:06 +0000

    Fix-2
    Signed-off-by: Amit Raj <amitraj@qti.qualcomm.com>
    Signed-off-by: Amit Raj <amitraj@qti.qualcommm.com>

--- QEfficient/diffusers/models/pytorch_transforms.py
+++ QEfficient/diffusers/models/pytorch_transforms.py
@@ -6,11 +6,13 @@
 # -----------------------------------------------------------------------------
 from typing import Tuple
 
-from diffusers.models.attention import JointTransformerBlock
-from diffusers.models.attention_processor import Attention, JointAttnProcessor2_0
 from torch import nn
 
+from diffusers.models.attention import JointTransformerBlock
+from diffusers.models.attention_processor import Attention, JointAttnProcessor2_0
+from diffusers.models.normalization import RMSNorm
 from QEfficient.base.pytorch_transforms import ModuleMappingTransform
+from QEfficient.customop.rms_norm import CustomRMSNormAIC
 from QEfficient.diffusers.models.attention import QEffJointTransformerBlock
 from QEfficient.diffusers.models.attention_processor import (
     QEffAttention,
@@ -19,7 +21,7 @@ from QEfficient.diffusers.models.attention_processor import (
 
 
 class CustomOpsTransform(ModuleMappingTransform):
-    _module_mapping = {}
+    _module_mapping = {RMSNorm: CustomRMSNormAIC}
 
 
 class AttentionTransform(ModuleMappingTransform):
