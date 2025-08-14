e579c88ad3e932d08b98590487fb1056b23aeb60
Amit Raj <amitraj@qti.qualcomm.com>
2025-08-14 13:46:39 +0000

    Code cleanup
    Signed-off-by: Amit Raj <amitraj@qti.qualcomm.com>

--- QEfficient/utils/constants.py
+++ QEfficient/utils/constants.py
@@ -103,6 +103,35 @@ LLAMA4_MAX_POSITION_EMBEDDINGS = 65536
 GEMMA3_MAX_POSITION_EMBEDDINGS = 32768
 
 
+# wo_sfs: weight output scaling factors (used to normalize T5 encoder output weights before export)
+WO_SFS = [
+    61,
+    203,
+    398,
+    615,
+    845,
+    1190,
+    1402,
+    2242,
+    1875,
+    2393,
+    3845,
+    3213,
+    3922,
+    4429,
+    5020,
+    5623,
+    6439,
+    6206,
+    5165,
+    4593,
+    2802,
+    2618,
+    1891,
+    1419,
+]
+
+
 class Constants:
     # Export Constants.
     SEQ_LEN = 32
