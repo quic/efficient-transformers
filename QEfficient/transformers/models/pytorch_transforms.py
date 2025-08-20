697630eb709277b9ecb6ac2b80c4971f5c880b17
Vinayak Baddi <quic_vbaddi@quicinc.com>
2025-08-20 08:50:05 +0000

    nit: seperate gate, up projections for MoE
    Signed-off-by: vbaddi <quic_vbaddi@quicinc.com>
    Signed-off-by: Onkar Chougule <ochougul@qti.qualcomm.com>

--- QEfficient/transformers/models/pytorch_transforms.py
+++ QEfficient/transformers/models/pytorch_transforms.py
@@ -54,6 +54,7 @@ from transformers.models.gpt_bigcode.modeling_gpt_bigcode import (
 from transformers.models.gpt_oss.modeling_gpt_oss import (
     GptOssAttention,
     GptOssDecoderLayer,
+    GptOssExperts,
     GptOssForCausalLM,
     GptOssMLP,
     GptOssModel,
@@ -254,6 +255,7 @@ from QEfficient.transformers.models.gpt_bigcode.modeling_gpt_bigcode import (
 from QEfficient.transformers.models.gpt_oss.modeling_gpt_oss import (
     QEffGptOssAttention,
     QEffGptOssDecoderLayer,
+    QEffGptOssExperts,
     QEffGptOssForCausalLM,
     QEffGptOssMLP,
     QEffGptOssModel,
@@ -523,7 +525,7 @@ class KVCacheTransform(ModuleMappingTransform):
         GptOssModel: QEffGptOssModel,
         GptOssForCausalLM: QEffGptOssForCausalLM,
         GptOssMLP: QEffGptOssMLP,
-        # GptOssExperts: QEffGptOssExperts,
+        GptOssExperts: QEffGptOssExperts,
         # Granite
         GraniteModel: QEffGraniteModel,
         GraniteForCausalLM: QEffGraniteForCausalLM,
