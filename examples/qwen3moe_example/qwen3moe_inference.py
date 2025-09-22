e1ead5f479748d35a6da5a5448fc9891f8fc643a
Dipankar Sarkar <quic_dipankar@quicinc.com>
2025-09-22 06:18:57 +0000

    Cleaning done 1
    Signed-off-by: Dipankar Sarkar <dipankar@qti.qualcomm.com>

--- examples/qwen3moe_example/qwen3moe_inference.py
+++ examples/qwen3moe_example/qwen3moe_inference.py
@@ -8,15 +8,14 @@
 from transformers import AutoTokenizer
 
 from QEfficient import QEFFAutoModelForCausalLM
+from QEfficient.utils.constants import Constants
 
 model_name = "Qwen/Qwen3-30B-A3B-Instruct-2507"
 """
-# For CB inference, set continuous_batching to True and add full_batch_size argument in compile function
+# For CB inference, set continuous_batching to True and add full_batch_size,mxfp6,mint8 argument in compile function
 # We will use prompt_len=1 for compilation for both cb and non-cb inference
 """
-model = QEFFAutoModelForCausalLM.from_pretrained(model_name, continuous_batching=False, num_hidden_layers=48)
+model = QEFFAutoModelForCausalLM.from_pretrained(model_name, continuous_batching=False)
 model.compile(prefill_seq_len=1, ctx_len=256, num_cores=16, num_devices=4, mxfp6_matmul=False, mxint8_kv_cache=False)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-exec_info = model.generate(prompts=["My name is"], tokenizer=tokenizer)
-gen_length = 50
-cloud_ai_100_tokens = exec_info.generated_ids[0][:, :gen_length]
+exec_info = model.generate(prompts=Constants.INPUT_STR, tokenizer=tokenizer)
