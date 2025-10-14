0fc8eb896dfaa62c72883e79252864d4ccd23b02
Dipankar Sarkar <dipankar@qti.qualcomm.com>
2025-10-14 20:01:13 +0000

    CI Bug fix
    Signed-off-by: Dipankar Sarkar <quic_dipankar@quicinc.com>

--- tests/transformers/models/test_causal_lm_models.py
+++ tests/transformers/models/test_causal_lm_models.py
@@ -80,7 +80,7 @@ def get_custom_n_layers(model_name):
         return 2
     elif model_name in ModelConfig.SWIFTKV_MODELS:
         return None
-    return 1
+    return 16
 
 
 def load_causal_lm_model(model_name, n_layer=1, config=None):
@@ -185,10 +185,6 @@ def check_causal_lm_pytorch_vs_kv_vs_ort_vs_ai100(
         assert (pytorch_hf_tokens == pytorch_kv_tokens).all(), (
             "Tokens don't match for HF PyTorch model output and KV PyTorch model output"
         )
-    assert (pytorch_hf_tokens == pytorch_kv_tokens).all(), (
-        "Tokens don't match for HF PyTorch model output and KV PyTorch model output"
-    )
-
     onnx_model_path = qeff_model.export()
     ort_tokens = api_runner.run_kv_model_on_ort(onnx_model_path, is_tlm=is_tlm)
     gen_len = ort_tokens.shape[-1]
