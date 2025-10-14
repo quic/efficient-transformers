2c41123805b2112c504e2c5b6995996e810c2e94
Mohit Soni <mohisoni@qti.qualcomm.com>
2025-10-14 19:39:22 +0000

    Fixing License
    Signed-off-by: Mohit Soni <mohisoni@qti.qualcomm.com>

--- QEfficient/transformers/models/qwen2_5_vl/modeling_qwen2_5_vl.py
+++ QEfficient/transformers/models/qwen2_5_vl/modeling_qwen2_5_vl.py
@@ -1,3 +1,10 @@
+# -----------------------------------------------------------------------------
+#
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# -----------------------------------------------------------------------------
+
 import math
 from typing import Callable, List, Optional, Tuple, Union
 
