1cd87efb2e9cc76b46c8654a7125c97b32024a24
Abukhoyer Shaik <abukhoye@qti.qualcomm.com>
2025-10-22 05:56:19 +0000

    Models test config in single Config file
    Signed-off-by: Abukhoyer Shaik <abukhoye@qti.qualcomm.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,46 +5,11 @@
 #
 # -----------------------------------------------------------------------------
 
-import json
 import os
 import shutil
 
-import pytest
-from transformers import AutoConfig
-
 from QEfficient.utils.constants import QEFF_MODELS_DIR
 from QEfficient.utils.logging_utils import logger
-from QEfficient.utils.test_utils import ModelConfig
-
-
-def get_custom_model_config_dict(configs):
-    """
-    Converts a list of custom model configuration dictionaries into a dictionary
-    mapping model names to their corresponding AutoConfig objects.
-
-    Args:
-        configs (List[Dict]): A list of dictionaries, each containing model configuration parameters.
-
-    Returns:
-        Dict[str, AutoConfig]: A dictionary where keys are model names and values are AutoConfig objects.
-    """
-    config_dict = {}
-    for config in configs:
-        model_name = config["model_name"]
-        config_dict[model_name] = AutoConfig.from_pretrained(
-            model_name,
-            trust_remote_code=config["model_name"] in ModelConfig.EXTERNAL_MODELS,
-            **config.get("additional_params", {}),
-        )
-    return config_dict
-
-
-# Pytest fixture to load custom model configs from a JSON file
-@pytest.fixture(scope="session")
-def custom_causal_model_config_dict():
-    with open("tests/transformers/models/custom_tiny_model_configs.json", "r") as f:
-        custom_model_configs_data = json.load(f)
-    return get_custom_model_config_dict(custom_model_configs_data)
 
 
 def qeff_models_clean_up():
