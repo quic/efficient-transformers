24262215932396fc641d923c0cff496bbc511aae
Mohit Soni <quic_mohisoni@quicinc.com>
2025-11-04 18:04:41 +0530

    Modeling fix (#605)
    Signed-off-by: Mohit Soni <mohisoni@qti.qualcom.com>
    Co-authored-by: Mohit Soni <mohisoni@qti.qualcom.com>
    Signed-off-by: abhishek-singh591 <sabhis@qti.qualcomm.com>

--- QEfficient/transformers/models/modeling_auto.py
+++ QEfficient/transformers/models/modeling_auto.py
@@ -1412,6 +1412,8 @@ class _QEffAutoModelForImageTextToTextDualQPC:
                 if x.startswith("past_") or x.endswith("_RetainedState")
             ]
         )
+        if not_mllama:
+            lang_session.skip_buffers(vision_outputs.keys())
 
         # Get first token
         lang_inputs["input_ids"] = outputs["logits"].argmax(2)
