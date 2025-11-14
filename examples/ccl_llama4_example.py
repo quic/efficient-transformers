d93d62fdf6c07766a810b83adea97b3a468d1140
vjanfaza <vjanfaza@qti.qualcomm.com>
2025-11-13 19:17:04 -0800

    Adding Compute-Context-Length (CCL) (#576)
    Compute-Context-Length (CCL) technique optimizes the throughput of large
    language models (LLMs) on Qualcomm devices when handling very large
    context lengths. The current Ahead Of Time (AOT) compilation on Qualcomm
    devices doesn't predict the number of tokens needed, leading to
    significant throughput drops during the prefilling and the decoding
    phases. This happens because the system performs attention calculations
    based on large context length. To address this issue, we introduce
    Compute Context Length (CCL), an additional ONNX variable that allows
    for dynamic context-length specialization. By generating tokens using
    smaller, more manageable context lengths (CCL), we optimize memory reads
    and attention calculations, thereby improving throughput.
    
    ---------
    
    Signed-off-by: Vahid Janfaza <vjanfaza@qti.qualcomm.com>
    Signed-off-by: abhishek-singh591 <sabhis@qti.qualcomm.com>

---
+++ examples/ccl_llama4_example.py
@@ -0,0 +1,128 @@
+# -----------------------------------------------------------------------------
+#
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# -----------------------------------------------------------------------------
+
+import torch
+import transformers
+from transformers import AutoConfig, AutoProcessor, TextStreamer
+
+from QEfficient import QEFFAutoModelForImageTextToText
+
+model_id = "meta-llama/Llama-4-Scout-17B-16E-Instruct"
+config = AutoConfig.from_pretrained(model_id)
+# For Testing Purpose Only
+config.text_config.num_hidden_layers = 4
+config.vision_config.num_hidden_layers = 2
+
+ctx_len = 8192
+# Set the list of ccl during prefilling process
+comp_ctx_lengths_prefill = [3072]
+# Set the list of ccl during decoding process
+comp_ctx_lengths_decode = [4096, ctx_len]
+
+qeff_model = QEFFAutoModelForImageTextToText.from_pretrained(
+    model_id,
+    attn_implementation="eager",
+    kv_offload=True,
+    qaic_config={
+        "comp_ctx_lengths_prefill": comp_ctx_lengths_prefill,
+        "comp_ctx_lengths_decode": comp_ctx_lengths_decode,
+        "ctx_len": ctx_len,
+    },
+    config=config,
+)
+tokenizer = transformers.AutoTokenizer.from_pretrained(model_id)
+processor = AutoProcessor.from_pretrained(model_id)
+
+### use skip_vision=Ture, if want to run only text, ow false ###
+skip_vision = False
+
+if skip_vision:
+    ## Only Text ##
+    qeff_model.compile(
+        prefill_seq_len=128,
+        ctx_len=ctx_len,
+        img_size=336,
+        num_cores=16,
+        num_devices=4,
+        max_num_tiles=17,
+        mxfp6_matmul=True,
+        mxint8_kv_cache=True,
+        aic_enable_depth_first=True,
+        skip_vision=True,
+        mos=1,
+    )
+
+    messages = [
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": "Can you describe the image in detail.",
+                },
+            ],
+        },
+    ]
+
+    inputs = processor.apply_chat_template(
+        messages,
+        add_generation_prompt=True,
+        tokenize=True,
+        return_dict=True,
+        return_tensors="pt",
+    )
+
+    streamer = TextStreamer(tokenizer)
+    output = qeff_model.generate(inputs=inputs, device_ids=[0, 1, 2, 3], generation_len=100)
+    print(output.generated_ids)
+    print(tokenizer.batch_decode(output.generated_ids))
+    print(output)
+
+else:
+    ## Vision + Text ##
+    qeff_model.compile(
+        prefill_seq_len=128,
+        ctx_len=ctx_len,
+        img_size=336,
+        num_cores=16,
+        num_devices=4,
+        max_num_tiles=17,
+        mxfp6_matmul=True,
+        mxint8_kv_cache=True,
+        aic_enable_depth_first=True,
+        mos=1,
+    )
+
+    ### IMAGE + TEXT ###
+    image_url = (
+        "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/datasets/cat_style_layout.png"
+    )
+
+    messages = [
+        {
+            "role": "user",
+            "content": [
+                {"type": "image", "url": image_url},
+                {"type": "text", "text": "Can you describe the image in detail."},
+            ],
+        },
+    ]
+
+    inputs = processor.apply_chat_template(
+        messages,
+        add_generation_prompt=True,
+        tokenize=True,
+        return_dict=True,
+        return_tensors="pt",
+    )
+    inputs["pixel_values"] = inputs["pixel_values"].to(torch.float32)
+    streamer = TextStreamer(tokenizer)
+    output = qeff_model.generate(inputs=inputs, device_ids=[0, 1, 2, 3], generation_len=100)
+    print(output.generated_ids)
+    print(tokenizer.batch_decode(output.generated_ids))
+    print(output)
+    print()
---
+++ examples/ccl_qwen2_5_vl_CB.py
@@ -0,0 +1,81 @@
+# -----------------------------------------------------------------------------
+#
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# -----------------------------------------------------------------------------
+
+# If we want to enable QBlocking Run below command:, default is without blocking
+# ATTENTION_BLOCKING_MODE=q num_q_blocks=2 python -W ignore qwen2_5_vl_example.py
+
+import transformers
+from transformers import AutoConfig, AutoProcessor, TextStreamer
+
+from QEfficient import QEFFAutoModelForImageTextToText
+
+## For AWQ model update pytorch version to 2.8.*
+model_id = "Qwen/Qwen2.5-VL-32B-Instruct"
+config = AutoConfig.from_pretrained(model_id)
+# config.text_config.num_hidden_layers = 2
+
+ctx_len = 8192
+comp_ctx_lengths_prefill = [4096]
+comp_ctx_lengths_decode = [6144, ctx_len]
+
+qeff_model = QEFFAutoModelForImageTextToText.from_pretrained(
+    model_id,
+    attn_implementation="eager",
+    kv_offload=True,
+    config=config,
+    continuous_batching=True,
+    qaic_config={
+        "comp_ctx_lengths_prefill": comp_ctx_lengths_prefill,
+        "comp_ctx_lengths_decode": comp_ctx_lengths_decode,
+        "ctx_len": ctx_len,
+    },
+)
+tokenizer = transformers.AutoTokenizer.from_pretrained(model_id)
+processor = AutoProcessor.from_pretrained(model_id)
+
+batch_size = 1
+## Vision + Text ##
+qeff_model.compile(
+    batch_size=batch_size,
+    full_batch_size=4,
+    prefill_seq_len=128,
+    ctx_len=8192,
+    num_cores=16,
+    num_devices=4,
+    height=354,
+    width=536,
+    mxfp6_matmul=True,
+    mxint8_kv_cache=True,
+    aic_enable_depth_first=True,
+    mos=1,
+)
+
+image_urls = [
+    "https://picsum.photos/id/237/536/354",
+    "https://picsum.photos/id/237/536/354",
+    "https://picsum.photos/id/237/536/354",
+    "https://picsum.photos/id/237/536/354",
+]
+
+prompts = [
+    "Can you describe the image in detail?",
+    "What are the objects in the image?",
+    "What is the main subject of the image?",
+    "What colors are predominant in the image?",
+]
+
+streamer = TextStreamer(tokenizer)
+output = qeff_model.generate(
+    tokenizer=tokenizer,
+    prompts=prompts,
+    processor=processor,
+    images=image_urls,
+    generation_len=100,
+)
+print(output.generated_ids)
+print(tokenizer.batch_decode(output.generated_ids))
+print(output)
