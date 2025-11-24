56058124d3b792b378793bd85871858ae00af3bd
Tanisha <tchawada@qti.qualcomm.com>
2025-11-24 08:28:41 +0000

    Separated the finetune test for functional and assertion cases
    Signed-off-by: Tanisha <tchawada@qti.qualcomm.com>

--- tests/finetune/test_finetune.py
+++ tests/finetune/test_finetune.py
@@ -227,7 +227,7 @@ def train_function(
     return results, all_ref_metrices, all_config_spy
 
 
-@pytest.mark.skip()  # remove when it's clear why diff val_step_loss values are observed in diff runs on existing code (even without PR #478 changes)
+# @pytest.mark.skip()  # remove when it's clear why diff val_step_loss values are observed in diff runs on existing code (even without PR #478 changes)
 @pytest.mark.cli
 @pytest.mark.on_qaic
 @pytest.mark.finetune
@@ -311,7 +311,7 @@ def test_finetune_functional(
         clean_up(alpaca_json_path)
 
 
-@pytest.mark.skip()  # remove when it's clear why diff val_step_loss values are observed in diff runs on existing code (even without PR #478 changes)
+# @pytest.mark.skip()  # remove when it's clear why diff val_step_loss values are observed in diff runs on existing code (even without PR #478 changes)
 @pytest.mark.cli
 @pytest.mark.on_qaic
 @pytest.mark.finetune
