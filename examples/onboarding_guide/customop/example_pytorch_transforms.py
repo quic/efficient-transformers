0ae3d0693057e2c52806f119892ec06f475c9781
Rishin Raj <rishinr@qti.qualcomm.com>
2025-11-25 16:04:07 +0000

    Lint and format
    Signed-off-by: Rishin Raj <rishinr@qti.qualcomm.com>

--- examples/onboarding_guide/customop/example_pytorch_transforms.py
+++ examples/onboarding_guide/customop/example_pytorch_transforms.py
@@ -15,49 +15,38 @@ For the actual production transforms, see:
 - QEfficient/transformers/models/pytorch_transforms.py
 """
 
-from torch import nn
 from transformers.activations import (
-    GELUActivation,
     NewGELUActivation,
-    FastGELUActivation,
-    QuickGELUActivation,
 )
 
 from QEfficient.base.pytorch_transforms import ModuleMappingTransform
-from QEfficient.customop import CustomRMSNormAIC, CustomGELUAIC
+from QEfficient.customop import CustomGELUAIC
 
 
 class CustomOpsTransform(ModuleMappingTransform):
     """
     Maps standard PyTorch operations to custom Cloud AI 100 implementations.
-    
+
     How it works:
     1. When a model is loaded, this transform scans all modules
     2. For each module type in _module_mapping, it replaces the module
        with the corresponding custom implementation
     3. The replacement happens automatically before ONNX export
     """
-    
+
     _module_mapping = {
         # ACTIVATION FUNCTIONS
-        
-        # GELU 
+        # GELU
         NewGELUActivation: CustomGELUAIC,
-        
-        
         # TODO: Add other activation functions
         # nn.SiLU: CustomSiLUAIC,
         # nn.Mish: CustomMishAIC,
-        
         # NORMALIZATION LAYERS
         # RMSNorm - Used by Llama, Mistral, Mixtral, etc.
         # from transformers.models.llama.modeling_llama import LlamaRMSNorm
         # LlamaRMSNorm: CustomRMSNormAIC,
-        
         # TODO: Add your model's normalization layers
         # YourModelRMSNorm: CustomRMSNormAIC,
-        
-
         # OTHER OPERATIONS
         # TODO: Add other custom operations
         # nn.Linear: CustomLinearAIC,  # If you have a custom linear layer
