472528acdf568125053b4b3e9e67e05a956f8b42
Tanisha Chawada <tchawada@qti.qualcomm.com>
2025-12-05 15:07:40 +0530

    [QEff. Finetune]: Adding optimizer registry and its test cases (#649)
    Adding a Script for Registering and Retrieving Optimizer Classes
    The script includes:
    
    
    get_optimizer()
    Returns the optimizer class and kwargs.
    Additionally, there is a test_optimizer.py script that validates the
    functionality of the optimizer registration and retrieval process.
    
    ---------
    
    Signed-off-by: Tanisha Chawada <tchawada@qti.qualcomm.com>

--- QEfficient/finetune/experimental/core/optimizer.py
+++ QEfficient/finetune/experimental/core/optimizer.py
@@ -4,3 +4,28 @@
 # SPDX-License-Identifier: BSD-3-Clause
 #
 # -----------------------------------------------------------------------------
+
+"""
+Optimizer components for the training system.
+"""
+
+import torch.optim as optim
+
+from QEfficient.finetune.experimental.core.component_registry import registry
+
+registry.optimizer("Adam")(optim.Adam)
+registry.optimizer("AdamW")(optim.AdamW)
+registry.optimizer("SGD")(optim.SGD)
+
+
+def prepare_optimizer(opt_config):
+    """
+    Create optimizer from config.
+    Args: opt_config: Dictionary containing optimizer configuration.
+    Returns: Tuple of optimizer class and its arguments.
+    """
+    opt_name = opt_config.pop("optimizer_name")
+    opt_cls = registry.get_optimizer(opt_name)
+    opt_config["lr"] = float(opt_config["lr"])
+    optimizer_cls_and_kwargs = (opt_cls, opt_config)
+    return optimizer_cls_and_kwargs
