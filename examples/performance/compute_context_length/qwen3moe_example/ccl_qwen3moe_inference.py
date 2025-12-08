06f9f0830850300a4cdf1b5ba5e6ca1ebd2de49d
vjanfaza <vjanfaza@qti.qualcomm.com>
2025-12-08 09:32:17 -0800

    Adding ccl_enabled flag during model loading and passing CCL lists during compilation process (#623)
    In these changes, instead of passing CCL lists during model loading, I
    passed a flag called ccl_enabled to specify whether CCL feature is
    enabled or not and moved passing CCL lists to compilation process.
    
    ---------
    
    Signed-off-by: Vahid Janfaza <vjanfaza@qti.qualcomm.com>
    Co-authored-by: Hem Agnihotri <hemagnih@qti.qualcomm.com>

--- examples/performance/compute_context_length/qwen3moe_example/ccl_qwen3moe_inference.py
+++ examples/performance/compute_context_length/qwen3moe_example/ccl_qwen3moe_inference.py
@@ -16,21 +16,25 @@ model_name = "Qwen/Qwen3-30B-A3B-Instruct-2507"
 # We will use prompt_len=1 for compilation for both cb and non-cb inference
 """
 
+## Activate Compute-Context-Length (CCL) feature by setting ccl_enabled=True when loading the model with from_pretrained().
+## Use the optional comp_ctx_lengths argument to provide two lists of context lengths for the prefilling and decoding processes. If comp_ctx_lengths=None, the model will run with its default context length.
+##   - The first list, comp_ctx_lengths_prefill, defines the compute-context-length values for the prefilling process.
+##           -- The process starts with the first value in the list and gradually increases the context length based on the position_id of the current prompt chunk.
+##   - The second list, comp_ctx_lengths_decode, defines the compute-context-length values for the decoding process.
+##           -- During decoding, the model selects an appropriate context length from the list based on the input prompt length and cache index.
+##           -- It starts from the correct value in the list and increases the context length dynamically when the cache index exceeds the current threshold.
+
 ctx_len = 1024
 prefill_seq_len = 1
-# In moe models when compiling with prefill_seq_len=1 and non-continuous-batching mode, prefill and decode will share the same specializations.
-comp_ctx_lengths_prefill = [256, 512, ctx_len]
-comp_ctx_lengths_decode = [256, 512, ctx_len]
+# In moe models when compiling with prefill_seq_len=1 and non-continuous-batching mode, prefill and decode will share the same ccl specializations.
+comp_ctx_lengths_prefill = [256, 512, ctx_len]  # None #
+comp_ctx_lengths_decode = [256, 512, ctx_len]  # None #
 
 model = QEFFAutoModelForCausalLM.from_pretrained(
     model_name,
     continuous_batching=False,
-    qaic_config={
-        "comp_ctx_lengths_prefill": comp_ctx_lengths_prefill,
-        "comp_ctx_lengths_decode": comp_ctx_lengths_decode,
-        "ctx_len": ctx_len,
-        "prefill_seq_len": prefill_seq_len,
-    },
+    ccl_enabled=True,
+    num_hidden_layers=4,
 )
 
 model.compile(
@@ -42,6 +46,8 @@ model.compile(
     mxfp6_matmul=True,
     mxint8_kv_cache=True,
     mos=1,
+    comp_ctx_lengths_prefill=comp_ctx_lengths_prefill,
+    comp_ctx_lengths_decode=comp_ctx_lengths_decode,
 )
 # mos=1,
 tokenizer = AutoTokenizer.from_pretrained(model_name)
