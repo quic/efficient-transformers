57cb01ad3bb8aafb41ecd97a3f92887a9362a90a
Dhiraj Kumar Sah <dhirajku@qti.qualcomm.com>
2025-12-08 09:46:03 +0000

    [QEff.finetune] WIP - Adding TrainerClass and tests for init checks.
    Signed-off-by: Dhiraj Kumar Sah <dhirajku@qti.qualcomm.com>

--- QEfficient/finetune/experimental/core/trainer/base_trainer.py
+++ QEfficient/finetune/experimental/core/trainer/base_trainer.py
@@ -4,3 +4,11 @@
 # SPDX-License-Identifier: BSD-3-Clause
 #
 # -----------------------------------------------------------------------------
+from transformers import Trainer, TrainingArguments
+
+from QEfficient.finetune.experimental.core.component_registry import registry
+
+
+@registry.trainer_module(name="base", args_cls=TrainingArguments, required_kwargs={})
+class BaseTrainer(Trainer):
+    pass  # Just using the standard Trainer
--- QEfficient/finetune/experimental/core/trainer/sft_trainer.py
+++ QEfficient/finetune/experimental/core/trainer/sft_trainer.py
@@ -4,3 +4,17 @@
 # SPDX-License-Identifier: BSD-3-Clause
 #
 # -----------------------------------------------------------------------------
+from trl import SFTConfig, SFTTrainer
+
+from QEfficient.finetune.experimental.core.component_registry import registry
+from QEfficient.finetune.experimental.core.config_manager import PeftConfig
+
+# def get_default_peft_config():
+#     """Get default peft config from config_manager and convert to peft library config."""
+#     peft_config_params = PeftConfig()
+#     return peft_config_params.to_peft_config()
+
+
+@registry.trainer_module(name="sft", args_cls=SFTConfig, required_kwargs={"peft_config": PeftConfig})
+class SFTTrainerModule(SFTTrainer):
+    pass  # Just using the standard SFTTrainer
