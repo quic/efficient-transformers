c6b17184ea234a3e971df1e3e5ecaf79ec637de6
Dipankar Sarkar <dipankar@qti.qualcomm.com>
2025-12-11 11:14:44 +0000

    Fixing QwenProcessor issue in diffuser pipeline
    Signed-off-by: Dipankar Sarkar <quic_dipankar@quicinc.com>
    Signed-off-by: Dipankar Sarkar <dipankar@qti.qualcomm.com>

--- QEfficient/diffusers/models/pytorch_transforms.py
+++ QEfficient/diffusers/models/pytorch_transforms.py
@@ -5,6 +5,7 @@
 #
 # -----------------------------------------------------------------------------
 
+from diffusers.models.attention_processor import Attention
 from diffusers.models.autoencoders.autoencoder_kl_wan import (
     WanDecoder3d,
     WanEncoder3d,
@@ -21,6 +22,7 @@ from diffusers.models.transformers.transformer_flux import (
 )
 from diffusers.models.transformers.transformer_wan import WanAttention, WanAttnProcessor, WanTransformer3DModel
 from diffusers.models.transformers.transformer_qwenimage import (
+    QwenDoubleStreamAttnProcessor2_0,
     QwenImageTransformer2DModel,
 )
 from torch import nn
@@ -51,6 +53,8 @@ from QEfficient.diffusers.models.transformers.transformer_wan import (
     QEffWanTransformer3DModel,
 )
 from QEfficient.diffusers.models.transformers.transformer_qwenimage import (
+    QEffQwenDoubleStreamAttnProcessor2_0,
+    QEffQwenImageAttention,
     QEffQwenImageTransformer2DModel,
 )
 
@@ -73,6 +77,8 @@ class AttentionTransform(ModuleMappingTransform):
         WanAttention: QEffWanAttention,
         WanTransformer3DModel: QEffWanTransformer3DModel,
         QwenImageTransformer2DModel: QEffQwenImageTransformer2DModel,
+        QwenDoubleStreamAttnProcessor2_0: QEffQwenDoubleStreamAttnProcessor2_0,
+        Attention: QEffQwenImageAttention,
         WanDecoder3d: QEffWanDecoder3d,
         WanEncoder3d: QEffWanEncoder3d,
         WanResidualBlock: QEffWanResidualBlock,
