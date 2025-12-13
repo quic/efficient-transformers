b769fc0ceb9d14fb257fcc85d23eab7dc1a855b3
Vahid Janfaza <vjanfaza@qti.qualcomm.com>
2025-12-12 16:56:09 -0800

    Add automatic CCL list generation for prefill and decode when user does not provide lists
    Signed-off-by: Vahid Janfaza <vjanfaza@qti.qualcomm.com>

--- QEfficient/transformers/models/modeling_auto.py
+++ QEfficient/transformers/models/modeling_auto.py
@@ -1127,7 +1127,7 @@ class _QEffAutoModelForImageTextToTextDualQPC:
         # if ccl_enabled is True read Compute-Context-Length lists
         if self.ccl_enabled:
             if comp_ctx_lengths_prefill is None and comp_ctx_lengths_decode is None:
-                print("Auto-generating CCL-prefill and CCL-decode lists based on Context Length (CL).")
+                logger.info("Auto-generating CCL-prefill and CCL-decode lists based on Context Length (CL).")
             self.comp_ctx_lengths_prefill, self.comp_ctx_lengths_decode, ctx_len = process_ccl_specializations(
                 comp_ctx_lengths_prefill, comp_ctx_lengths_decode, ctx_len, prefill_seq_len
             )
@@ -1772,7 +1772,7 @@ class _QEFFAutoModelForImageTextToTextSingleQPC(QEFFTransformersBase, Multimodal
         # if ccl_enabled is True read Compute-Context-Length lists
         if self.ccl_enabled:
             if comp_ctx_lengths_prefill is None and comp_ctx_lengths_decode is None:
-                print("Auto-generating CCL-prefill and CCL-decode lists based on Context Length (CL).")
+                logger.info("Auto-generating CCL-prefill and CCL-decode lists based on Context Length (CL).")
             self.comp_ctx_lengths_prefill, self.comp_ctx_lengths_decode, ctx_len = process_ccl_specializations(
                 comp_ctx_lengths_prefill, comp_ctx_lengths_decode, ctx_len, prefill_seq_len
             )
@@ -2868,7 +2868,7 @@ class QEFFAutoModelForCausalLM(QEFFBaseModel):
         # if ccl_enabled is True read Compute-Context-Length lists
         if self.ccl_enabled:
             if comp_ctx_lengths_prefill is None and comp_ctx_lengths_decode is None:
-                print("Auto-generating CCL-prefill and CCL-decode lists based on Context Length (CL).")
+                logger.info("Auto-generating CCL-prefill and CCL-decode lists based on Context Length (CL).")
             self.comp_ctx_lengths_prefill, self.comp_ctx_lengths_decode, ctx_len = process_ccl_specializations(
                 comp_ctx_lengths_prefill, comp_ctx_lengths_decode, ctx_len, prefill_seq_len
             )
