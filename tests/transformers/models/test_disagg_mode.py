28d3f78414302987a4947fa8089a966491d8ec9d
Dipankar Sarkar <dipankar@qti.qualcomm.com>
2025-12-30 17:27:26 +0000

    Adding qwen3moe to test
    Signed-off-by: Dipankar Sarkar <dipankar@qti.qualcomm.com>

--- tests/transformers/models/test_disagg_mode.py
+++ tests/transformers/models/test_disagg_mode.py
@@ -16,7 +16,10 @@ from QEfficient import QEFFAutoModelForCausalLM
 from QEfficient.generation.cloud_infer import QAICInferenceSession
 from QEfficient.transformers.quantizers import replace_transformers_quantizers, undo_transformers_quantizers
 
-model_id = "openai/gpt-oss-120b"  # weights are not required to convert to fp32
+model_id = [
+    # "Qwen/Qwen3-30B-A3B-Instruct-2507",
+    "openai/gpt-oss-20b",
+]  # weights are not required to convert to fp32
 
 prompt2 = """
 Once upon a time, in a small town, there lived a young boy named Alex. Alex was a curious and adventurous child, always eager to explore the world around him. One day, while playing in the park, Alex stumbled upon a mysterious old book hidden beneath a pile of leaves. The book was filled with stories of distant lands, magical creatures, and extraordinary adventures.
@@ -30,8 +33,9 @@ prompt1 = "Once upon a time"
 prompts = [prompt1, prompt2]
 
 
+@pytest.mark.skip(reason="only works for gpt-oss right now")
 @pytest.mark.on_qaic
-@pytest.mark.parametrize("model_id", [model_id])
+@pytest.mark.parametrize("model_id", model_id)
 @pytest.mark.parametrize("prompt", prompts)
 def test_disagg_mode_prefill(model_id, prompt):
     # Run prefill
@@ -66,7 +70,7 @@ def test_disagg_mode_prefill(model_id, prompt):
     past_key_values = []
     for i in range(config.num_hidden_layers):
         cache_len = 128 if i % 2 == 0 else PREFILL_SEQ_LEN
-        pad_shape = (1, 8, cache_len, 64)
+        pad_shape = (1, config.num_key_value_heads, cache_len, config.head_dim)
         past_key = torch.zeros((pad_shape), dtype=torch.float32)
         past_value = torch.zeros((pad_shape), dtype=torch.float32)
         pkv = (past_key, past_value)
@@ -74,7 +78,6 @@ def test_disagg_mode_prefill(model_id, prompt):
     inputs["past_key_values"] = past_key_values
 
     qeff_out = qeff_model.model(**inputs)
-
     # Check our pytorch implementation
     assert (qeff_out.logits - out.logits[:, -1, :]).abs().max() < 1e-4
 
@@ -92,7 +95,7 @@ def test_disagg_mode_prefill(model_id, prompt):
     )
 
     prefill_session = QAICInferenceSession(prefill_qpc_path)
-    logits_out_placeholder = np.zeros((1, 1, 201088), dtype=np.float32)
+    logits_out_placeholder = np.zeros((1, 1, config.vocab_size), dtype=np.float32)
     prefill_session.set_buffers({"logits": logits_out_placeholder})
     inputs.pop("past_key_values")
     inputs = {k: v.detach().numpy() for k, v in inputs.items()}
@@ -104,9 +107,9 @@ def test_disagg_mode_prefill(model_id, prompt):
     assert (torch.from_numpy(qpc_out["logits"]) - qeff_out.logits).abs().max() < 5e-2
 
 
-@pytest.mark.skip(reason="no way of currently testing this without the assert sdk")
+# @pytest.mark.skip(reason="no way of currently testing this without the assert sdk")
 @pytest.mark.on_qaic
-@pytest.mark.parametrize("model_id", [model_id])
+@pytest.mark.parametrize("model_id", model_id)
 @pytest.mark.parametrize("prompt", prompts)
 def test_disagg_mode_prefill_chunked(model_id, prompt):
     # Run prefill
@@ -141,7 +144,7 @@ def test_disagg_mode_prefill_chunked(model_id, prompt):
     past_key_values = []
     for i in range(config.num_hidden_layers):
         cache_len = CTX_LEN
-        pad_shape = (1, 8, cache_len, 64)
+        pad_shape = (1, config.num_key_value_heads, cache_len, config.head_dim)
         past_key = torch.zeros((pad_shape), dtype=torch.float32)
         past_value = torch.zeros((pad_shape), dtype=torch.float32)
         pkv = (past_key, past_value)
@@ -157,6 +160,7 @@ def test_disagg_mode_prefill_chunked(model_id, prompt):
         inputs["past_key_values"] = qeff_out["past_key_values"]
 
     # Check our pytorch implementation
+
     assert (qeff_out.logits - out.logits[:, -1, :]).abs().max() < 1e-4
 
     prefill_qpc_path = qeff_model.compile(
@@ -176,7 +180,7 @@ def test_disagg_mode_prefill_chunked(model_id, prompt):
     prefill_session.skip_buffers(
         [x for x in prefill_session.input_names + prefill_session.output_names if x.startswith("past_")]
     )
-    logits_out_placeholder = np.zeros((1, 1, 201088), dtype=np.float32)
+    logits_out_placeholder = np.zeros((1, 1, config.vocab_size), dtype=np.float32)
     prefill_session.set_buffers({"logits": logits_out_placeholder})
     inputs.pop("past_key_values")
     inputs = {k: v.detach().numpy() for k, v in inputs.items()}
