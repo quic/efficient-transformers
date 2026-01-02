7b591ffa09d57062fb993425b280417ad8bc247d
Dhiraj Kumar Sah <dhirajku@qti.qualcomm.com>
2026-01-02 19:29:15 +0530

    Added Trainer classes and tests for FT (#697)
    This PR contains all the changes of PR #660 along with all the comments
    being addressed. The new PR was created due a rebase issue.
    
    Signed-off-by: Dhiraj Kumar Sah <dhirajku@qti.qualcomm.com>
    Signed-off-by: Sharvari Medhe <smedhe@qti.qualcomm.com>

--- QEfficient/finetune/experimental/core/trainer/sft_trainer.py
+++ QEfficient/finetune/experimental/core/trainer/sft_trainer.py
@@ -4,3 +4,12 @@
 # SPDX-License-Identifier: BSD-3-Clause
 #
 # -----------------------------------------------------------------------------
+from trl import SFTConfig, SFTTrainer
+
+from QEfficient.finetune.experimental.core.component_registry import registry
+from QEfficient.finetune.experimental.core.config_manager import PeftConfig
+
+
+@registry.trainer_module(name="sft", args_cls=SFTConfig, required_kwargs={"peft_config": PeftConfig})
+class SFTTrainerModule(SFTTrainer):
+    pass  # Just using the standard SFTTrainer
