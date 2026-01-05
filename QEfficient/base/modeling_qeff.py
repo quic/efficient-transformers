d30fb1838988261a126ea40801ed3e37abddde31
Abhishek Kumar Singh <sabhis@qti.qualcomm.com>
2026-01-05 06:07:54 +0000

    Resolved lint error
    Signed-off-by: Abhishek Kumar Singh <sabhis@qti.qualcomm.com>

--- QEfficient/base/modeling_qeff.py
+++ QEfficient/base/modeling_qeff.py
@@ -518,6 +518,7 @@ class QEFFBaseModel(ABC):
 
         command.append(f"-aic-binary-dir={qpc_path}")
         logger.info(f"Running compiler: {' '.join(command)}")
+
         try:
             subprocess.run(command, capture_output=True, check=True)
         except subprocess.CalledProcessError as e:
