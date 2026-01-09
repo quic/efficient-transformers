198e3bb0d5763054ff4a027e0002bbfa8e35ba86
vjanfaza <vjanfaza@qti.qualcomm.com>
2026-01-09 08:55:52 -0800

    Evaluating the values of CCL lists for different scenarios (#710)
    Signed-off-by: Vahid Janfaza <vjanfaza@qti.qualcomm.com>

--- QEfficient/utils/constants.py
+++ QEfficient/utils/constants.py
@@ -191,9 +191,6 @@ CCL_MIN_CTX_LEN = 1024
 # used for gpt-oss prefill-only model Q-blocking
 GPT_OSS_PREFILL_Q_BLOCK_SIZE = 256
 
-# used for gpt-oss prefill-only model Q-blocking
-GPT_OSS_PREFILL_Q_BLOCK_SIZE = 256
-
 
 class Constants:
     # Export Constants.
