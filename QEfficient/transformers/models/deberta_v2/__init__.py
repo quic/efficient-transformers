0e0629df4ffdc4c2739c57df244508eef665df82
Amit Raj <amitraj@qti.qualcomm.com>
2026-01-16 12:23:33 +0000

    Minor fix-3
    Signed-off-by: Amit Raj <amitraj@qti.qualcomm.com>

--- QEfficient/transformers/models/deberta_v2/__init__.py
+++ QEfficient/transformers/models/deberta_v2/__init__.py
@@ -4,13 +4,3 @@
 # SPDX-License-Identifier: BSD-3-Clause
 #
 # -----------------------------------------------------------------------------
-
-from QEfficient.transformers.models.deberta_v2.modeling_deberta_v2 import (
-    QEffDebertaV2ForSequenceClassification,
-    QEffDisentangledSelfAttention,
-)
-
-__all__ = [
-    "QEffDebertaV2ForSequenceClassification",
-    "QEffDisentangledSelfAttention",
-]
