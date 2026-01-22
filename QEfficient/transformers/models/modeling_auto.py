91044aedcf137606f25741d571ea3398497acd8d
abhishek-singh591 <sabhis@qti.qualcomm.com>
2026-01-22 07:40:00 +0000

    Made minor changes
    Signed-off-by: abhishek-singh591 <sabhis@qti.qualcomm.com>

--- QEfficient/transformers/models/modeling_auto.py
+++ QEfficient/transformers/models/modeling_auto.py
@@ -344,8 +344,8 @@ class QEFFAutoModel(QEFFTransformersBase):
 
         return self._export(
             example_inputs,
-            output_names,
-            dynamic_axes,
+            output_names=output_names,
+            dynamic_axes=dynamic_axes,
             export_dir=export_dir,
             use_onnx_subfunctions=kwargs.get("use_onnx_subfunctions", False),
         )
@@ -623,8 +623,8 @@ class QEffVisionEncoderForTextImageToTextModel(QEFFBaseModel):
         """
         return self._export(
             inputs,
-            output_names,
-            dynamic_axes,
+            output_names=output_names,
+            dynamic_axes=dynamic_axes,
             export_dir=export_dir,
             offload_pt_weights=offload_pt_weights,
             use_onnx_subfunctions=kwargs.get("use_onnx_subfunctions", False),
@@ -768,8 +768,8 @@ class QEffCausalLMForTextImageToTextModel(QEFFBaseModel):
         """
         return self._export(
             inputs,
-            output_names,
-            dynamic_axes,
+            output_names=output_names,
+            dynamic_axes=dynamic_axes,
             export_dir=export_dir,
             offload_pt_weights=offload_pt_weights,
             use_onnx_subfunctions=kwargs.get("use_onnx_subfunctions", False),
@@ -1708,8 +1708,8 @@ class _QEFFAutoModelForImageTextToTextSingleQPC(QEFFTransformersBase, Multimodal
         output_names = self.model.get_output_names()
         return self._export(
             inputs,
-            output_names,
-            dynamic_axes,
+            output_names=output_names,
+            dynamic_axes=dynamic_axes,
             export_dir=export_dir,
             use_onnx_subfunctions=use_onnx_subfunctions,
         )
@@ -2706,8 +2706,8 @@ class QEFFAutoModelForCausalLM(QEFFBaseModel):
             )
         return self._export(
             example_inputs,
-            output_names,
-            dynamic_axes,
+            output_names=output_names,
+            dynamic_axes=dynamic_axes,
             export_dir=export_dir,
             use_onnx_subfunctions=kwargs.get("use_onnx_subfunctions", False),
             offload_pt_weights=kwargs.get("offload_pt_weights", True),
@@ -3300,8 +3300,8 @@ class QEFFAutoModelForSpeechSeq2Seq(QEFFTransformersBase, MultimodalUtilityMixin
         output_names = self.model.get_output_names()
         return self._export(
             inputs,
-            output_names,
-            dynamic_axes,
+            output_names=output_names,
+            dynamic_axes=dynamic_axes,
             export_dir=export_dir,
             use_onnx_subfunctions=kwargs.get("use_onnx_subfunctions", False),
         )
@@ -3676,8 +3676,8 @@ class QEFFAutoModelForCTC(QEFFTransformersBase):
 
         return self._export(
             example_inputs,
-            output_names,
-            dynamic_axes,
+            output_names=output_names,
+            dynamic_axes=dynamic_axes,
             export_dir=export_dir,
             use_onnx_subfunctions=kwargs.get("use_onnx_subfunctions", False),
         )
