8079f473f4d87f474f225cb250c377b879250d55
vtirumal <vtirumal@qti.qualcomm.com>
2026-02-05 10:11:06 +0000

    Rebased and updating Wan I2V
    Signed-off-by: vtirumal <vtirumal@qti.qualcomm.com>

---
+++ examples/diffusers/wan/wan_lightning_i2v.py
@@ -0,0 +1,94 @@
+# -----------------------------------------------------------------------------
+#
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# -----------------------------------------------------------------------------
+import numpy as np
+import safetensors.torch
+import torch
+from diffusers.loaders.lora_conversion_utils import _convert_non_diffusers_wan_lora_to_diffusers
+from diffusers.utils import export_to_video, load_image
+from huggingface_hub import hf_hub_download
+
+from QEfficient import QEffWanImageToVideoPipeline
+
+# Load the pipeline
+pipeline = QEffWanImageToVideoPipeline.from_pretrained("Wan-AI/Wan2.2-I2V-A14B-Diffusers")
+
+# Download the LoRAs
+high_noise_lora_path = hf_hub_download(
+    repo_id="lightx2v/Wan2.2-Lightning",
+    filename="Wan2.2-I2V-A14B-4steps-lora-rank64-Seko-V1/high_noise_model.safetensors",
+)
+low_noise_lora_path = hf_hub_download(
+    repo_id="lightx2v/Wan2.2-Lightning",
+    filename="Wan2.2-I2V-A14B-4steps-lora-rank64-Seko-V1/low_noise_model.safetensors",
+)
+
+
+# LoRA conversion
+def load_wan_lora(path: str):
+    return _convert_non_diffusers_wan_lora_to_diffusers(safetensors.torch.load_file(path))
+
+
+# Load into the transformers
+pipeline.transformer.model.transformer_high.load_lora_adapter(
+    load_wan_lora(high_noise_lora_path), adapter_name="high_noise"
+)
+pipeline.transformer.model.transformer_high.set_adapters(["high_noise"], weights=[1.0])
+pipeline.transformer.model.transformer_low.load_lora_adapter(
+    load_wan_lora(low_noise_lora_path), adapter_name="low_noise"
+)
+pipeline.transformer.model.transformer_low.set_adapters(["low_noise"], weights=[1.0])
+
+
+# # Configure for 2-layer model (faster inference)
+# pipeline.transformer.model.transformer_high.config['num_layers'] = 1
+# pipeline.transformer.model.transformer_low.config['num_layers']= 1
+
+# # Reduce high noise transformer blocks
+# original_blocks = pipeline.transformer.model.transformer_high.blocks
+# pipeline.transformer.model.transformer_high.blocks = torch.nn.ModuleList(
+#     [original_blocks[i] for i in range(0, pipeline.transformer.model.transformer_high.config['num_layers'])]
+# )
+
+# # Reduce low noise transformer blocks
+# org_blocks = pipeline.transformer.model.transformer_low.blocks
+# pipeline.transformer.model.transformer_low.blocks = torch.nn.ModuleList(
+#     [org_blocks[i] for i in range(0, pipeline.transformer.model.transformer_low.config['num_layers'])]
+# )
+
+
+image = load_image(
+    "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/astronaut.jpg"
+)
+# max_area = 190 * 320
+max_area = 480 * 832
+aspect_ratio = image.height / image.width
+mod_value = pipeline.model.vae.config.scale_factor_spatial * pipeline.model.transformer.config.patch_size[1]
+height = round(np.sqrt(max_area * aspect_ratio)) // mod_value * mod_value
+width = round(np.sqrt(max_area / aspect_ratio)) // mod_value * mod_value
+
+image = image.resize((width, height))
+prompt = (
+    "An astronaut hatching from an egg, on the surface of the moon, the darkness and depth of space realised in "
+    "the background. High quality, ultrarealistic detail and breath-taking movie-like camera shot."
+)
+
+output = pipeline(
+    image=image,
+    prompt=prompt,
+    num_inference_steps=4,
+    height=height,
+    width=width,
+    num_frames=81,
+    guidance_scale=1.0,
+    generator=torch.manual_seed(0),
+    # custom_config_path="examples/diffusers/wan/wan_i2v_config.json",
+    use_onnx_subfunctions=True,
+    parallel_compile=False,
+)
+frames = output.images[0]
+export_to_video(frames, "output_i2v.mp4", fps=16)
+print(output)
