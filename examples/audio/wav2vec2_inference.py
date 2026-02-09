33917598149b61fa1ec892e15323046b0f965f0f
Tanisha Chawada <tchawada@qti.qualcomm.com>
2026-02-09 05:54:14 +0000

    Updating wav2vec2_inference for batch_input
    Signed-off-by: Tanisha Chawada <tchawada@qti.qualcomm.com>

--- examples/audio/wav2vec2_inference.py
+++ examples/audio/wav2vec2_inference.py
@@ -16,13 +16,16 @@ from QEfficient import QEFFAutoModelForCTC
 def main():
     parser = argparse.ArgumentParser(description="CTC speech recognition inference with Wav2Vec2")
     parser.add_argument(
-        "--model-name",
+        "--model_name",
         type=str,
         default="facebook/wav2vec2-base-960h",
         help="HuggingFace CTC model ID (e.g., Wav2Vec2)",
     )
 
-    parser.add_argument("--num-cores", type=int, default=16, help="Number of cores")
+    parser.add_argument("--num_cores", type=int, default=16, help="Number of cores")
+    parser.add_argument("--batch_size", type=int, default=1, help="Batch size")
+    parser.add_argument("--seq_len", type=int, default=480000, help="Context length for generation")
+    parser.add_argument("--num-devices", type=int, default=1, help="Number of devices")
     args = parser.parse_args()
 
     print(f"Loading CTC model: {args.model_name}")
@@ -31,10 +34,7 @@ def main():
     # Using a standard english dataset
     print("Loading audio sample from dataset...")
     ds = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
-    data = ds[0]["audio"]["array"]
-
-    # Reshape so shape corresponds to data with batch size 1
-    data = data.reshape(-1)
+    data=[ds[i]["audio"]["array"] for i in range(args.batch_size)]
 
     # Load processor
     processor = AutoProcessor.from_pretrained(args.model_name)
@@ -43,7 +43,10 @@ def main():
     model = QEFFAutoModelForCTC.from_pretrained(args.model_name)
 
     ## STEP 3 -- Compile the model
-    model.compile(num_cores=args.num_cores)
+    model.compile(batch_size=args.batch_size,
+                  num_devices=args.num_devices,
+                  seq_len=args.seq_len,
+                  num_cores=args.num_cores)
 
     ## STEP 4 -- Run the model and generate the output
     model_output = model.generate(processor, inputs=data)
