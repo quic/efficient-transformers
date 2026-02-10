c6e048989c2fd326711e8cb705aa96582502a6e5
Kushal Dulla <quic_kdulla@quicinc.com>
2026-02-10 07:58:30 +0000

    Initial changes for moving transforms out of pretrained
    Signed-off-by: Kushal Dulla <quic_kdulla@quicinc.com>

--- QEfficient/transformers/attention_blocking_policy.py
+++ QEfficient/transformers/attention_blocking_policy.py
@@ -11,13 +11,21 @@ from typing import Optional
 
 from QEfficient.transformers.attention_blocking import AttentionBlockingConfig
 
+from QEfficient.utils import (
+    constants,
+)
+from QEfficient.utils.blocking_configurator import build_transformer_blocking_config
+
+
+
 
 def derive_blocking_config(model_config, device_info: Optional[dict] = None, compile_params: Optional[dict] = None):
     """
     Dummy policy for auto blocking config selection.
     This can be replaced by a real model + device + compile param heuristic.
     """
-    max_seq_len = getattr(model_config, "max_position_embeddings", None)
+    build_transformer_blocking_config(model.config, specializations=decode_spec, blocking_mode="kv", compile_config={"mdp_ts_num_devices": compile_params.get("mdp_ts_num_devices", 1), 
+                                                                                                                    "aic_num_cores": compiler_params.get("aic_num_cores", constants.DEFAULT_AIC_NUM_CORES)})
     num_kv_blocks = 2
     if isinstance(max_seq_len, int) and max_seq_len > 0:
         num_kv_blocks = max(1, min(8, max_seq_len // 128))
