3084a120424a8076072d767efaf11856004cb9d9
Dipankar Sarkar <dipankar@qti.qualcomm.com>
2026-03-03 14:29:46 +0000

    Test Added for Qwen3Moe
    Signed-off-by: Dipankar Sarkar <dipankar@qti.qualcomm.com>

--- tests/transformers/models/test_disagg_mode.py
+++ tests/transformers/models/test_disagg_mode.py
@@ -18,6 +18,7 @@ from QEfficient.transformers.quantizers import replace_transformers_quantizers, 
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 model_id = "openai/gpt-oss-20b"  # weights are not required to convert to fp32
 =======
 model_id = [
@@ -34,6 +35,14 @@ model_names_chunking = [
     "openai/gpt-oss-20b",
 ]
 
+=======
+model_id_blocking = [
+    "openai/gpt-oss-20b",
+]
+model_id_chunking = [
+    "Qwen/Qwen3-30B-A3B-Instruct-2507",
+]
+>>>>>>> 493cc0f (Test Added for Qwen3Moe)
 prompt2 = """
 Once upon a time, in a small town, there lived a young boy named Alex. Alex was a curious and adventurous child, always eager to explore the world around him. One day, while playing in the park, Alex stumbled upon a mysterious old book hidden beneath a pile of leaves. The book was filled with stories of distant lands, magical creatures, and extraordinary adventures.
 
@@ -49,10 +58,14 @@ prompts = [prompt1, prompt2]
 @pytest.mark.on_qaic
 <<<<<<< HEAD
 @pytest.mark.llm_model
+<<<<<<< HEAD
 @pytest.mark.parametrize("model_id", [model_id])
 =======
 @pytest.mark.parametrize("model_id", model_names_blocking)
 >>>>>>> 728df6d (Addressing Changes and Review Comments)
+=======
+@pytest.mark.parametrize("model_id", model_id_blocking)
+>>>>>>> 493cc0f (Test Added for Qwen3Moe)
 @pytest.mark.parametrize("prompt", prompts)
 def test_disagg_mode_prefill(model_id, prompt):
     # Run prefill
@@ -87,7 +100,7 @@ def test_disagg_mode_prefill(model_id, prompt):
     past_key_values = []
     for i in range(config.num_hidden_layers):
         cache_len = 128 if i % 2 == 0 else PREFILL_SEQ_LEN
-        pad_shape = (1, config.num_key_value_heads, cache_len, config.head_dim)
+        pad_shape = (1, 8, cache_len, 64)
         past_key = torch.zeros((pad_shape), dtype=torch.float32)
         past_value = torch.zeros((pad_shape), dtype=torch.float32)
         pkv = (past_key, past_value)
@@ -95,6 +108,7 @@ def test_disagg_mode_prefill(model_id, prompt):
     inputs["past_key_values"] = past_key_values
 
     qeff_out = qeff_model.model(**inputs)
+
     # Check our pytorch implementation
     assert (qeff_out.logits - out.logits[:, -1, :]).abs().max() < 1e-4
 
@@ -127,10 +141,14 @@ def test_disagg_mode_prefill(model_id, prompt):
 @pytest.mark.on_qaic
 <<<<<<< HEAD
 @pytest.mark.llm_model
+<<<<<<< HEAD
 @pytest.mark.parametrize("model_id", [model_id])
 =======
 @pytest.mark.parametrize("model_id", model_names_chunking)
 >>>>>>> 728df6d (Addressing Changes and Review Comments)
+=======
+@pytest.mark.parametrize("model_id", model_id_chunking)
+>>>>>>> 493cc0f (Test Added for Qwen3Moe)
 @pytest.mark.parametrize("prompt", prompts)
 def test_disagg_mode_prefill_chunked(model_id, prompt):
     # Run prefill
@@ -181,7 +199,6 @@ def test_disagg_mode_prefill_chunked(model_id, prompt):
         inputs["past_key_values"] = qeff_out["past_key_values"]
 
     # Check our pytorch implementation
-
     assert (qeff_out.logits - out.logits[:, -1, :]).abs().max() < 1e-4
 
     prefill_qpc_path = qeff_model.compile(
@@ -218,7 +235,7 @@ def test_disagg_mode_prefill_chunked(model_id, prompt):
 
 
 @pytest.mark.on_qaic
-@pytest.mark.parametrize("model_id", [model_id])
+@pytest.mark.parametrize("model_id", model_id_blocking)
 @pytest.mark.parametrize("prompt", [prompt1])
 def test_disagg_mode_prefill_only_and_decode_only(model_id, prompt):
     # Run prefill for original pytorch model
@@ -323,7 +340,7 @@ def test_disagg_mode_prefill_only_and_decode_only(model_id, prompt):
     )
 
     prefill_session = QAICInferenceSession(prefill_qpc_path)
-    logits_out_placeholder = np.zeros((1, 1, 201088), dtype=np.float32)
+    logits_out_placeholder = np.zeros((1, 1, config.vocab_size), dtype=np.float32)
     prefill_session.set_buffers({"logits": logits_out_placeholder})
     inputs.pop("past_key_values")
     inputs = {k: v.detach().numpy() for k, v in inputs.items()}
@@ -389,7 +406,7 @@ def test_disagg_mode_prefill_only_and_decode_only(model_id, prompt):
 
 
 @pytest.mark.on_qaic
-@pytest.mark.parametrize("model_id", [model_id])
+@pytest.mark.parametrize("model_id", model_id_blocking)
 @pytest.mark.parametrize("prompt", [prompt1])
 def test_disagg_mode_prefix_caching(model_id, prompt):
     PREFILL_SEQ_LEN = 128
@@ -468,7 +485,7 @@ def prefix_caching_inference(model_id, prefill_qpc_path, decode_qpc_path, prompt
     inputs["batch_index"] = np.array([[decode_batch_id]], dtype=np.int64)
 
     prefill_session = QAICInferenceSession(prefill_qpc_path)
-    logits_out_placeholder = np.zeros((1, 1, 201088), dtype=np.float32)
+    logits_out_placeholder = np.zeros((1, 1, config.vocab_size), dtype=np.float32)
     prefill_session.set_buffers({"logits": logits_out_placeholder})
     for i in range(num_chunks):
         chunk_inputs = inputs.copy()
