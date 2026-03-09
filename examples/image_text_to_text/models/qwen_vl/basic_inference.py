4f81aa880832978540e8abc7c577b9c41dea12bf
Asmita Goswami <asmigosw@qti.qualcomm.com>
2026-03-09 08:14:16 +0000

    Ruff format
    Signed-off-by: asmigosw <asmigosw@qti.qualcomm.com>
    Signed-off-by: Dhiraj Kumar Sah <dhirajku@qti.qualcomm.com>
    Signed-off-by: Asmita Goswami <asmigosw@qti.qualcomm.com>

--- examples/image_text_to_text/models/qwen_vl/basic_inference.py
+++ examples/image_text_to_text/models/qwen_vl/basic_inference.py
@@ -6,7 +6,6 @@
 # -----------------------------------------------------------------------------
 
 import requests
-import torch
 import transformers
 from PIL import Image
 from qwen_vl_utils import process_vision_info
@@ -19,15 +18,8 @@ model_id = "Qwen/Qwen2.5-VL-32B-Instruct"
 config = AutoConfig.from_pretrained(model_id)
 config.text_config.num_hidden_layers = 2
 
-# To change the model weights to required dtype (bf16/fp16/fp32)
-LOAD_DTYPE = torch.float32
-
 qeff_model = QEFFAutoModelForImageTextToText.from_pretrained(
-    model_id,
-    attn_implementation="eager",
-    kv_offload=True,
-    config=config,
-    torch_dtype=LOAD_DTYPE,
+    model_id, attn_implementation="eager", kv_offload=True, config=config
 )
 tokenizer = transformers.AutoTokenizer.from_pretrained(model_id)
 processor = AutoProcessor.from_pretrained(model_id)
