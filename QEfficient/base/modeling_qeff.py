8680d1af19536664e3313d6c9332e43b6d4aae36
Dhiraj Kumar Sah <dhirajku@qti.qualcomm.com>
2026-03-17 16:54:01 +0000

    Added check to not pass Custom_IO yaml when model weight and pkv are both in bfloat16. Added a patch incloud infer to map bfloat16 or 11 key type to np.float16 for AI200 inference.
    Signed-off-by: Dhiraj Kumar Sah <dhirajku@qti.qualcomm.com>
    Signed-off-by: Asmita Goswami <asmigosw@qti.qualcomm.com>

--- QEfficient/base/modeling_qeff.py
+++ QEfficient/base/modeling_qeff.py
@@ -538,12 +538,19 @@ class QEFFBaseModel(ABC):
             command.append(f"-network-specialization-config={specializations_json}")
 
         # Write custom_io.yaml file
+        model_in_bfloat16 = self.config.torch_dtype == torch.bfloat16
+        pkv_in_bfloat16 = any("past_" in key and "bfloat16" in value for key, value in custom_io.items())
         if custom_io is not None:
             custom_io_yaml = compile_dir / "custom_io.yaml"
             with open(custom_io_yaml, "w") as fp:
                 for io_name, dtype in custom_io.items():
                     fp.write(f" - IOName: {io_name}\n   Precision: {dtype}\n\n")
-            command.append(f"-custom-IO-list-file={custom_io_yaml}")
+            if model_in_bfloat16 and pkv_in_bfloat16:
+                logger.warning(
+                    "Model and Past KV types are both bfloat16. Custom IO list file will be ignored during compile."
+                )
+            else:
+                command.append(f"-custom-IO-list-file={custom_io_yaml}")
 
         command.append(f"-aic-binary-dir={qpc_path}")
         logger.info(f"Running compiler: {' '.join(command)}")
