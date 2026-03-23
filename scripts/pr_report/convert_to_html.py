dcf96dea694ee303cdcd6d0aa39ee68b7d66adec
Rishin Raj <rishinr@qti.qualcomm.com>
2026-03-23 12:03:47 +0530

    Update convert_to_html.py
    Signed-off-by: Rishin Raj <rishinr@qti.qualcomm.com>

--- scripts/pr_report/convert_to_html.py
+++ scripts/pr_report/convert_to_html.py
@@ -1,4 +1,10 @@
-#!/usr/bin/env python3
+# -----------------------------------------------------------------------------
+#
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# -----------------------------------------------------------------------------
+
 """Convert pr_report.md to styled HTML."""
 
 import sys
