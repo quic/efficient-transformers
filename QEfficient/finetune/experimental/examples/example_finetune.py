d654fe6c75d743bf32a65ff223c215dfed7bfe1b
smedhe <smedhe@qti.qualcomm.com>
2026-03-25 10:14:48 +0530

    [QEff. finetune_experimnetal] cherry picking PR-787, 791,813,795 (#872)
    we are only cherry-picking PR-787, 791,813,795, skipping rebasing PR
    785, cherry-picking experimental related branches from PR 692,747
    
    ---------
    
    Signed-off-by: Swati Allabadi <sallabad@qti.qualcomm.com>
    Signed-off-by: Sharvari Medhe <smedhe@qti.qualcomm.com>
    Signed-off-by: Tanisha Chawada <tchawada@qti.qualcomm.com>
    Signed-off-by: Ann Kuruvilla <akuruvil@qti.qualcomm.com>
    Co-authored-by: Ann Kuruvilla <akuruvil@qti.qualcomm.com>
    Signed-off-by: Ann Kuruvilla <akuruvil@qti.qualcomm.com>

--- QEfficient/finetune/experimental/examples/example_finetune.py
+++ QEfficient/finetune/experimental/examples/example_finetune.py
@@ -4,3 +4,12 @@
 # SPDX-License-Identifier: BSD-3-Clause
 #
 # -----------------------------------------------------------------------------
+
+
+from QEfficient.cloud.finetune_experimental import main
+from QEfficient.finetune.experimental.examples.custom_dataset import (
+    SeqCompletionDataset,  # noqa: F401 - registers CustomDataset
+)
+
+if __name__ == "__main__":
+    main()
