25c06e9de90b4094419c93f40360b26761e550a4
Ann Kuruvilla <akuruvil@qti.qualcomm.com>
2026-03-27 10:38:08 +0000

    Fixed repolinter error
    Signed-off-by: Ann Kuruvilla <akuruvil@qti.qualcomm.com>

--- QEfficient/finetune/experimental/core/utils/constants.py
+++ QEfficient/finetune/experimental/core/utils/constants.py
@@ -1,1 +1,8 @@
+# -----------------------------------------------------------------------------
+#
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# -----------------------------------------------------------------------------
+
 DTYPE_MAPPING = {"fp16": "float16", "bf16": "bfloat16"}
