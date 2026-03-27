f9ceea338b9b2e9b26cea0b36efff23e828b16c0
vbaddi <vbaddi@qti.qualcomm.com>
2026-03-27 17:43:22 +0000

    nit: extend to all the VLMs
    Signed-off-by: vbaddi <vbaddi@qti.qualcomm.com>

--- QEfficient/transformers/models/modeling_auto.py
+++ QEfficient/transformers/models/modeling_auto.py
@@ -1582,6 +1582,7 @@ class _QEffAutoModelForImageTextToTextDualQPC:
                 compile_dir=compile_dir,
                 compile_only=True,
                 specializations=specializations["vision"],
+                specialization_module_name="Vision",
                 convert_to_fp16=(CUSTOM_IO_DTYPE_MAP[target_dtype] == "float16"),
                 mxfp6_matmul=constants.VISION_MXFP6_MATMUL,
                 mdp_ts_num_devices=num_devices,
