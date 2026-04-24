e384ec79597e25e9790cf3ea39cac830aedd31b9
Amit Raj <amitraj@qti.qualcomm.com>
2026-04-24 17:11:17 +0530

    Added first block caching for flux
    Signed-off-by: Amit Raj <amitraj@qti.qualcomm.com>

--- QEfficient/diffusers/first_block_cache/__init__.py
+++ QEfficient/diffusers/first_block_cache/__init__.py
@@ -5,9 +5,14 @@
 #
 # ----------------------------------------------------------------------------
 
+from QEfficient.diffusers.first_block_cache.flux import enable_flux_first_block_cache
 from QEfficient.diffusers.first_block_cache.wan import (
     enable_wan_first_block_cache,
     run_wan_non_unified_first_block_cache_denoise,
 )
 
-__all__ = ["enable_wan_first_block_cache", "run_wan_non_unified_first_block_cache_denoise"]
+__all__ = [
+    "enable_flux_first_block_cache",
+    "enable_wan_first_block_cache",
+    "run_wan_non_unified_first_block_cache_denoise",
+]
