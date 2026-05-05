f9bbf40cd2009dae0637519a1fccb65aec10f91d
Mamta Singh <168400541+quic-mamta@users.noreply.github.com>
2026-05-05 14:27:40 +0530

    kimi-k2-minor-fix (#959)
    Signed-off-by: Mamta Singh <168400541+quic-mamta@users.noreply.github.com>

--- QEfficient/base/modeling_qeff.py
+++ QEfficient/base/modeling_qeff.py
@@ -287,7 +287,6 @@ class QEFFBaseModel(ABC):
         """
         # TODO: Hack for retain_full_kv, handle this outside
         export_kwargs.pop("retain_full_kv", None)
-        export_kwargs.pop("mla_absorption", None)
         onnx_path = export_dir / f"{self.model_name}.onnx"
 
         # Return early if ONNX already exists
@@ -392,7 +391,6 @@ class QEFFBaseModel(ABC):
         offload_pt_weights: Optional[bool] = True,
         use_onnx_subfunctions: Optional[bool] = False,
         retain_full_kv: Optional[bool] = False,
-        mla_absorption: Optional[Dict[str, bool]] = None,
         qaic_config: Optional[dict] = None,
         **compiler_options,
     ):
@@ -400,7 +398,6 @@ class QEFFBaseModel(ABC):
             "offload_pt_weights": offload_pt_weights,
             "use_onnx_subfunctions": use_onnx_subfunctions,
             "retain_full_kv": retain_full_kv,
-            "mla_absorption": mla_absorption,
         }
 
         if prefill_only:
@@ -498,7 +495,6 @@ class QEFFBaseModel(ABC):
         offload_pt_weights: Optional[bool] = True,
         enable_chunking: Optional[bool] = False,
         retain_full_kv: Optional[bool] = None,
-        mla_absorption: Optional[Dict[str, bool]] = None,
         qaic_config: Optional[dict] = None,
         specialization_module_name: Optional[str] = None,
         **compiler_options,
@@ -539,7 +535,6 @@ class QEFFBaseModel(ABC):
                 offload_pt_weights,
                 use_onnx_subfunctions,
                 retain_full_kv,
-                mla_absorption,
                 num_devices=mdp_ts_num_devices,
                 qaic_config=qaic_config,
                 **compiler_options,
