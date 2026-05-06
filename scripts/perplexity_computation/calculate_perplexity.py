db60cffab8625070e42105645088efdace1e0d2e
qraniumcitest <rmakar@qti.qualcomm.com>
2026-05-07 03:12:42 +0530

    Update calculate_perplexity.py
    Signed-off-by: qraniumcitest <rmakar@qti.qualcomm.com>

--- scripts/perplexity_computation/calculate_perplexity.py
+++ scripts/perplexity_computation/calculate_perplexity.py
@@ -18,8 +18,6 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 from QEfficient.generation.cloud_infer import QAICInferenceSession
 
 logger = logging.getLogger(__name__)
-
-
 # 1. Data Loading
 class WikiTextDataset(Dataset):
     def __init__(self, dataset_name, tokenizer, ctx_len, stride=1024):
