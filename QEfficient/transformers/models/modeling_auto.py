08902dabc6011bb7e8a69a620fc17816181673c8
Vaibhav Verma <vaibverm@qti.qualcomm.com>
2026-05-08 19:52:36 -0500

    Added block_table and slot_id inputs + minor modelling_auto.py changes
    Signed-off-by: Vaibhav Verma <vaibverm@qti.qualcomm.com>

--- QEfficient/transformers/models/modeling_auto.py
+++ QEfficient/transformers/models/modeling_auto.py
@@ -4057,8 +4057,8 @@ class QEFFAutoModelForCausalLM(QEFFBaseModel):
                     max_blocks = max(max_blocks, num_blocks)
             block_size = -(-seq_len // max_blocks)
             seq_len = block_size * max_blocks
-            num_kv_blocks = self.hash_params["blocking_config"].num_kv_blocks
-            self.supports_paged_attention = "paged" in self.hash_params["blocking_config"].mode
+            num_kv_blocks = self.hash_params["blocking_kwargs"].num_kv_blocks
+            self.supports_paged_attention = "paged" in self.hash_params["blocking_kwargs"].mode
 
         # TODO: Remove this hack ##################
         if dynamo:
@@ -4442,8 +4442,8 @@ class QEFFAutoModelForCausalLM(QEFFBaseModel):
         if full_batch_size:
             spec["full_batch_exec_size"] = exec_batch_size
         if self.hash_params.get("blocking_kwargs", None):
-            if "paged" in self.hash_params["blocking_config"].mode:
-                num_kv_blocks = self.hash_params["blocking_config"].num_kv_blocks
+            if "paged" in self.hash_params["blocking_kwargs"].mode:
+                num_kv_blocks = self.hash_params["blocking_kwargs"].num_kv_blocks
                 spec["num_kv_blocks"] = num_kv_blocks
                 spec["total_num_kv_blocks"] = kv_cache_batch_size * num_kv_blocks
                 spec["kv_block_size"] = (-ctx_len) // (-num_kv_blocks)
@@ -4515,8 +4515,8 @@ class QEFFAutoModelForCausalLM(QEFFBaseModel):
         else:
             spec["batch_size"] = kv_cache_batch_size
         if self.hash_params.get("blocking_kwargs", None):
-            if "paged" in self.hash_params["blocking_config"].mode:
-                num_kv_blocks = self.hash_params["blocking_config"].num_kv_blocks
+            if "paged" in self.hash_params["blocking_kwargs"].mode:
+                num_kv_blocks = self.hash_params["blocking_kwargs"].num_kv_blocks
                 spec["num_kv_blocks"] = num_kv_blocks
                 spec["total_num_kv_blocks"] = kv_cache_batch_size * num_kv_blocks
                 spec["kv_block_size"] = (-ctx_len) // (-num_kv_blocks)
