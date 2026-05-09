d9c331288c3c8f4d0a5b7a560e6b4c6f3bd9539c
Vaibhav Verma <vaibverm@qti.qualcomm.com>
2026-05-09 05:12:33 -0500

    Working version with PagedAttention
    Signed-off-by: Vaibhav Verma <vaibverm@qti.qualcomm.com>

--- QEfficient/blocking/blocked_attention_forwards.py
+++ QEfficient/blocking/blocked_attention_forwards.py
@@ -1117,8 +1117,8 @@ def blocked_kv_paged_attention_forward(
                 if skip_future.item():
                     break
 
-        block_index = block_table[:, i]
-        updated = (position_ids.max(1, keepdim=True).values // kv_block_size) == i
+        block_index = block_table[:, j]
+        updated = (position_ids.max(1, keepdim=True).values // kv_block_size) == j
         k_block, v_block = past_key_value.read_only_pagedAttention(block_index, updated, layer_idx, cache_kwargs)
         k_block_states, v_block_states = _get_kv_states(module, k_block, v_block)
 
@@ -1391,8 +1391,8 @@ def blocked_qkv_paged_attention_forward(
                     if skip_future.item():
                         break
 
-            block_index = block_table[:, i]
-            updated = (position_ids.max(1, keepdim=True).values // kv_block_size) == i
+            block_index = block_table[:, j]
+            updated = (position_ids.max(1, keepdim=True).values // kv_block_size) == j
             k_block, v_block = past_key_value.read_only_pagedAttention(block_index, updated, layer_idx, cache_kwargs)
             k_block_states, v_block_states = _get_kv_states(module, k_block, v_block)
 
@@ -1705,8 +1705,8 @@ def blocked_hqkv_paged_attention_forward(
                         if skip_future.item():
                             break
 
-                block_index = block_table[:, i]
-                updated = (position_ids.max(1, keepdim=True).values // kv_block_size) == i
+                block_index = block_table[:, j]
+                updated = (position_ids.max(1, keepdim=True).values // kv_block_size) == j
                 k_block, v_block = past_key_value.read_only_pagedAttention(block_index, updated, layer_idx, cache_kwargs)
                 k_block_states, v_block_states = _get_kv_states(module, k_block, v_block)
 
@@ -2071,8 +2071,8 @@ def blocked_bhqkv_paged_attention_forward(
                             if skip_future.item():
                                 break
 
-                    block_index = block_table[:, i]
-                    updated = (position_ids.max(1, keepdim=True).values // kv_block_size) == i
+                    block_index = block_table[:, j]
+                    updated = (position_ids.max(1, keepdim=True).values // kv_block_size) == j
                     k_block, v_block = past_key_value.read_only_pagedAttention(block_index, updated, layer_idx, cache_kwargs)
                     k_block_states, v_block_states = _get_kv_states(module, k_block, v_block)
 
