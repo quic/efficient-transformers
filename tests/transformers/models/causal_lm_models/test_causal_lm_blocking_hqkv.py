a52a1a6b53cb1ca465982f6ab25d515011ce5fb5
Kushal Dulla <kdulla@qti.qualcomm.com>
2026-05-14 15:24:10 +0530

    formatting issues
    Signed-off-by: Kushal Dulla <kdulla@qti.qualcomm.com>

--- tests/transformers/models/causal_lm_models/test_causal_lm_blocking_hqkv.py
+++ tests/transformers/models/causal_lm_models/test_causal_lm_blocking_hqkv.py
@@ -293,7 +293,7 @@ def test_few_causal_all_blocking_pytorch_vs_kv_vs_ort_vs_ai100_CB(model_name, ma
     check_causal_lm_pytorch_vs_kv_vs_ort_vs_ai100(
         model_name=model_name, qaic_config=qaic_config, manual_cleanup=manual_cleanup, num_devices=4
     )
-    
+
     # q block only
     qaic_config = dict(enable_blocking=True, num_q_blocks=NUM_Q_BLOCKS)
     check_causal_lm_pytorch_vs_kv_vs_ort_vs_ai100(
@@ -369,7 +369,7 @@ def test_dummy_causal_all_blocking_pytorch_vs_kv_vs_ort_vs_ai100_CB(model_name, 
         manual_cleanup=manual_cleanup,
         continuous_batching=True,
     )
-    
+
     # kv blocking only, head parallel
     qaic_config = dict(enable_blocking=True, num_kv_blocks=NUM_KV_BLOCKS, kv_blocking_headpar_split=0)
     check_causal_lm_pytorch_vs_kv_vs_ort_vs_ai100(
