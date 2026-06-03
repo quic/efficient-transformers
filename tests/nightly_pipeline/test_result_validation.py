ceb47d291b9c3e746a0812cc1b32306cf07a7b5e
Abukhoyer Shaik <abukhoye@qti.qualcomm.com>
2026-06-03 07:37:27 +0000

    nightly prev result unavailability
    Signed-off-by: Abukhoyer Shaik <abukhoye@qti.qualcomm.com>

--- tests/nightly_pipeline/test_result_validation.py
+++ tests/nightly_pipeline/test_result_validation.py
@@ -30,7 +30,7 @@ MODEL_ARTIFACTS = [
 @pytest.mark.parametrize("model_class, artifact_filename, csv_filename", MODEL_ARTIFACTS)
 def test_validate_nightly_results(model_class, artifact_filename, csv_filename, artifacts_dir, get_pipeline_config):
     previous_artifacts_dir = os.environ.get("NIGHTLY_PIPELINE_PREVIOUS_ARTIFACTS_DIR")
-    if previous_artifacts_dir == "":
+    if previous_artifacts_dir == "null" or previous_artifacts_dir == "":
         previous_artifacts_dir = None
     current_artifact_file = artifacts_dir / artifact_filename
     previous_artifact_file = None
