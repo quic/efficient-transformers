253f65402cac35b8ac3cf0bc3091cea921dd422f
Vaibhav Verma <vaibverm@qti.qualcomm.com>
2026-06-04 08:55:00 -0500

    Adding PagedAttention specific test, unit tests and examples + lint/format cleanup
    Signed-off-by: Vaibhav Verma <vaibverm@qti.qualcomm.com>

--- tests/unit_test/models/test_model_quickcheck.py
+++ tests/unit_test/models/test_model_quickcheck.py
@@ -2115,7 +2115,7 @@ class TestGetCompilationDims:
                 ]
             },
         )
-        bs, ctx, fbs = get_compilation_dims(qpc_path)
+        bs, ctx, fbs, num_kv_blocks = get_compilation_dims(qpc_path)
         assert bs == 1
         assert ctx == 4096
         assert fbs is None
@@ -2138,7 +2138,7 @@ class TestGetCompilationDims:
                 ]
             },
         )
-        bs, ctx, fbs = get_compilation_dims(qpc_path)
+        bs, ctx, fbs, num_kv_blocks = get_compilation_dims(qpc_path)
         assert bs == 1
         assert ctx == 4096
         assert fbs == 16
@@ -2155,7 +2155,7 @@ class TestGetCompilationDims:
                 ]
             },
         )
-        bs, ctx, fbs = get_compilation_dims(qpc_path)
+        bs, ctx, fbs, num_kv_blocks = get_compilation_dims(qpc_path)
         assert bs == 1
         assert ctx == 4096
         assert fbs is None
