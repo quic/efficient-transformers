5f2e1ac3cbe685d604b2270f0d739ae8e232b8a1
Tanisha Chawada <tchawada@qti.qualcomm.com>
2026-06-11 22:15:19 +0530

    Adding unit test and ci tests for gemma4
    Signed-off-by: Tanisha Chawada <tchawada@qti.qualcomm.com>

--- tests/transformers/models/image_text_to_text/test_image_text_to_text_models.py
+++ tests/transformers/models/image_text_to_text/test_image_text_to_text_models.py
@@ -78,6 +78,11 @@ def check_image_text_to_text_pytorch_vs_kv_vs_ort_vs_ai100(
         if hasattr(config, "model_type") and config.model_type in ["gemma3"]:
             config.text_config._sliding_window_pattern = 2
             config.text_config.layer_types = ["sliding_attention", "full_attention"]
+        if hasattr(config, "model_type") and config.model_type in ["gemma4"]:
+            config.text_config.num_kv_shared_layers = 0
+            config.text_config.num_hidden_layers = 1
+            config.vision_config.num_hidden_layers = 1
+            config.text_config.layer_types = ["sliding_attention"]
         if hasattr(config, "model_type") and config.model_type in [
             "qwen3_vl",
             "qwen3_vl_moe",
@@ -89,10 +94,7 @@ def check_image_text_to_text_pytorch_vs_kv_vs_ort_vs_ai100(
             config._attn_implementation = "eager"
             model_hf = load_vlm_model(config)
             qeff_model = QEFFAutoModelForCausalLM.from_pretrained(
-                model_name,
-                kv_offload=kv_offload,
-                config=config,
-                torch_dtype=torch_dtype,
+                model_name, kv_offload=kv_offload, config=config, torch_dtype=torch_dtype, ignore_mismatched_sizes=True
             )
         else:
             model_hf = load_vlm_model(config)
@@ -101,6 +103,7 @@ def check_image_text_to_text_pytorch_vs_kv_vs_ort_vs_ai100(
                 kv_offload=kv_offload,
                 config=config,
                 torch_dtype=torch_dtype,
+                ignore_mismatched_sizes=True,
             )
     else:
         model_hf = load_vlm_model_from_config(config)
@@ -109,6 +112,7 @@ def check_image_text_to_text_pytorch_vs_kv_vs_ort_vs_ai100(
             kv_offload=kv_offload,
             config=model_hf.config,
             torch_dtype=torch_dtype,
+            ignore_mismatched_sizes=True,
         )
     compile_kwargs = {
         "num_devices": num_devices,
@@ -118,7 +122,9 @@ def check_image_text_to_text_pytorch_vs_kv_vs_ort_vs_ai100(
         "enable_qnn": enable_qnn,
         "qnn_config": qnn_config,
     }
-
+    if model_name == "google/gemma-4-E2B-it":
+        compile_kwargs["split-model-io"] = True
+        compile_kwargs["node_precision_info"] = True
     if model_name in ModelConfig.INTERNVL_MODELS:
         tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True, use_fast=False)
         processor = InternProcessor(model_hf, tokenizer)
@@ -351,7 +357,11 @@ def test_image_text_to_text_pytorch_vs_kv_vs_ort_vs_ai100_qnn(model_name, kv_off
     ``Mandatory`` Args:
         :model_name (str): Hugging Face Model Card name, Example: ``gpt2``
     """
-    if model_name == "meta-llama/Llama-4-Scout-17B-16E-Instruct" or model_name == "google/gemma-3-4b-it":
+    if (
+        model_name == "meta-llama/Llama-4-Scout-17B-16E-Instruct"
+        or model_name == "google/gemma-3-4b-it"
+        or "google/gemma-4-E2B-it"
+    ):
         pytest.skip("QNN is not supported for these models yet.")
 
     qnn_config_json_path = os.path.join(os.getcwd(), "qnn_config.json")
