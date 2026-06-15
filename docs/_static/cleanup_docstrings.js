1d407af0323813a455369bcd355f753c0e3d7e09
Amit Raj <amitraj@qti.qualcomm.com>
2026-06-15 15:49:37 +0530

    fix: Add license header to cleanup_docstrings.js for repolinter compliance
    Signed-off-by: Amit Raj <amitraj@qti.qualcomm.com>

--- docs/_static/cleanup_docstrings.js
+++ docs/_static/cleanup_docstrings.js
@@ -1,3 +1,10 @@
+// -----------------------------------------------------------------------------
+//
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// -----------------------------------------------------------------------------
+
 // Remove orphaned RST code-block directive text that mkdocstrings doesn't fully process
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelectorAll('.doc p').forEach(function (p) {
