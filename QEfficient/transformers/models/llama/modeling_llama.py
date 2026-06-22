a8f2180b0c1305f11716c3b3f4c83670af75c45e
Sharvari Medhe <smedhe@qti.qualcomm.com>
2026-06-22 07:18:17 +0530

    changing torch.tensor to torch.full_like for MIN_MASKED_ATTENTION_VALUE
    Signed-off-by: Sharvari Medhe <smedhe@qti.qualcomm.com>
    (cherry picked from commit 806f2cfda824fd160f6cb3d0bf1d433b17924a65)
    Signed-off-by: Sharvari Medhe <smedhe@qti.qualcomm.com>
    Signed-off-by: Amit Raj <amitraj@qti.qualcomm.com>

--- QEfficient/transformers/models/llama/modeling_llama.py
+++ QEfficient/transformers/models/llama/modeling_llama.py
@@ -93,7 +93,7 @@ def eager_attention_forward(
 
     attn_weights = torch.matmul(query, key_states.transpose(2, 3)) * scaling
     if attention_mask is not None:
-        masked_fill = torch.full_like(attn_weights, MIN_MASKED_ATTENTION_VALUE, dtype=module.config.dtype)
+        masked_fill = torch.full_like(attn_weights, MIN_MASKED_ATTENTION_VALUE, dtype=attn_weights.dtype)
         attn_weights = torch.where(
             attention_mask, masked_fill, attn_weights
         )
