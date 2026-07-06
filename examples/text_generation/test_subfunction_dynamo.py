7e94c294a2250fbc5c100189d601805ce9fa733e
Sharvari Medhe <smedhe@qti.qualcomm.com>
2026-07-06 10:13:01 +0530

    removing export reports, disabling rewriteunsupportedops
    Signed-off-by: Sharvari Medhe <smedhe@qti.qualcomm.com>
    Signed-off-by: Amit Raj <amitraj@qti.qualcomm.com>

--- examples/text_generation/test_subfunction_dynamo.py
+++ examples/text_generation/test_subfunction_dynamo.py
@@ -12,10 +12,10 @@ from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer
 from QEfficient.transformers.models.modeling_auto import QEFFAutoModelForCausalLM
 from QEfficient.utils.run_utils import ApiRunner
 
-model_name = "google/gemma-7b"
+model_name = "openai/gpt-oss-20b"
 tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 config = AutoConfig.from_pretrained(model_name, trust_remote_code=True)
-config.num_hidden_layers = 4
+# config.num_hidden_layers = 4
 config.torch_dtype = torch.float16
 # print(config)
 runner = ApiRunner(
@@ -28,18 +28,18 @@ runner = ApiRunner(
 )
 
 # PyTorch (KV) output
-hf_model = AutoModelForCausalLM.from_pretrained(model_name, config=config, trust_remote_code=True)
+hf_model = AutoModelForCausalLM.from_pretrained(model_name, config=config)
 hf_tokens = runner.run_hf_model_on_pytorch(hf_model)
 print(hf_tokens)
 
-qeff_model = QEFFAutoModelForCausalLM.from_pretrained(model_name, config=config, trust_remote_code=True)
+qeff_model = QEFFAutoModelForCausalLM.from_pretrained(model_name, config=config)
 pt_tokens = runner.run_kv_model_on_pytorch(qeff_model.model)
 print(pt_tokens)
 
-# onnx_path = qeff_model.export(use_dynamo=True, use_onnx_subfunctions=True)
-# ort_inputs = runner.input_handler.prepare_ort_inputs()
-# ort_tokens = runner.run_kv_model_on_ort(onnx_path)
-# print(ort_tokens)
+onnx_path = qeff_model.export(use_dynamo=True, use_onnx_subfunctions=True)
+ort_inputs = runner.input_handler.prepare_ort_inputs()
+ort_tokens = runner.run_kv_model_on_ort(onnx_path)
+print(ort_tokens)
 
 qeff_model.compile(
     prefill_seq_len=1, ctx_len=2048, use_dynamo=True, use_onnx_subfunctions=True, num_devices=4, mxfp6_matmul=True
