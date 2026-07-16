b13f168dcdb00f3337267f19c221c1ebb7fd1ff7
kdulla <kdulla@qti.qualcomm.com>
2026-07-16 16:25:39 +0530

    Identified bug in batch fold cache layout and updated to correct read (#1186)
    Identified incorrect implementation in read cache, updated cache layout
    and read calls accordingly, still has ongoing compilation bug.
    
    Signed-off-by: Kushal Dulla <kdulla@qti.qualcomm.com>

--- QEfficient/base/onnx_transforms.py
+++ QEfficient/base/onnx_transforms.py
@@ -19,6 +19,8 @@ from onnx import ModelProto, TensorProto, external_data_helper, numpy_helper
 from QEfficient.customop.ctx_scatter_gather import (
     CtxChunkScatterBatch,
     CtxChunkScatterBatchFunc,
+    CtxGatherBlockedKVBatch,
+    CtxGatherFuncBlockedKVBatch,
     CtxGather,
     CtxGather3D,
     CtxGatherBlockedKV,
@@ -115,6 +117,7 @@ class CustomOpTransform(BaseOnnxTransform):
         "CtxScatterFuncCB": (CtxScatterFuncCB, CtxScatterCB),
         "CtxGatherFuncCB": (CtxGatherFuncCB, CtxGatherCB),
         "CtxChunkScatterBatchFunc": (CtxChunkScatterBatchFunc, CtxChunkScatterBatch),
+        "CtxGatherFuncBlockedKVBatch": (CtxGatherFuncBlockedKVBatch, CtxGatherBlockedKVBatch)
         # "CastToUInt4": (CastToUInt4Func, CastToUInt4),
     }
 
