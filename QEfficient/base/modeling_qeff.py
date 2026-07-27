dc2f2b561872e95d464d9bc2febf5740b1e8d5b5
kdulla <kdulla@qti.qualcomm.com>
2026-07-27 17:48:33 +0530

    Added in progress changes for benchmark alignment (#1219)
    Added in progress changes for benchmark alignment, including updated CtxChunkScatter, reshape before custom op and introduction of kv unroll option
    
    Signed-off-by: Kushal Dulla <kdulla@qti.qualcomm.com>
    Signed-off-by: vtirumal <vtirumal@qti.qualcomm.com>

--- QEfficient/base/modeling_qeff.py
+++ QEfficient/base/modeling_qeff.py
@@ -556,6 +556,13 @@ class QEFFBaseModel(ABC):
             dynamic_axes = {rename_map.get(k, k): v for k, v in dynamic_axes.items()}
             input_names = aligned_input_names
 
+        for k, v in example_inputs.items():
+            if type(v) is list:
+                for i, v_i in enumerate(v):
+                    print(f"input {k}_{i} shape is {v_i[0].shape}")
+            else:
+                print(f"input {k} shape is {v.shape}")
+
         try:
             if dynamo:
                 self._export_via_dynamo(
