8b62e8ff1941b4d85338253a2ed21a2c1d2fec68
Vaibhav Verma <vaibverm@qti.qualcomm.com>
2026-07-29 20:12:38 -0500

    Remove block_table and slot_id args from cache update function used in non-paged path
    Signed-off-by: Vaibhav Verma <vaibverm@qti.qualcomm.com>

--- QEfficient/transformers/models/llama/modeling_llama.py
+++ QEfficient/transformers/models/llama/modeling_llama.py
@@ -168,8 +168,6 @@ class QEffLlamaAttention(LlamaAttention):
                 comp_ctx_lengths=comp_ctx_lengths,
                 batch_index=batch_index,
                 position_ids=position_ids,
-                block_table=block_table,
-                slot_id=slot_id,
             )
             attn_output, attn_weights = eager_attention_forward(
                 self,
--- QEfficient/transformers/models/mixtral_moe/modeling_mixtral.py
+++ QEfficient/transformers/models/mixtral_moe/modeling_mixtral.py
@@ -241,8 +241,6 @@ class QEffMixtralAttention(MixtralAttention):
                 comp_ctx_lengths=comp_ctx_lengths,
                 batch_index=batch_index,
                 position_ids=position_ids,
-                block_table=block_table,
-                slot_id=slot_id,
             )
             attn_output, attn_weights = eager_attention_forward(
                 self,
