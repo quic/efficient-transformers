a0df373507ff86c29f4aaf89dd57c8e44287ed04
Vaibhav Verma <vaibverm@qti.qualcomm.com>
2026-07-29 19:45:53 -0500

    Remove block_table and slot_id args from cache update function used in non-paged path
    Signed-off-by: Vaibhav Verma <vaibverm@qti.qualcomm.com>

--- QEfficient/blocking/attention_blocking.py
+++ QEfficient/blocking/attention_blocking.py
@@ -219,8 +219,6 @@ def past_key_value_update(
     comp_ctx_lengths: Optional[torch.LongTensor] = None,
     batch_index: Optional[torch.LongTensor] = None,
     position_ids: Optional[torch.LongTensor] = None,
-    block_table: Optional[torch.LongTensor] = None,
-    slot_id: Optional[torch.LongTensor] = None,
     sliding_window: Optional[int] = None,
 ):
     if past_key_value is not None:
@@ -324,8 +322,6 @@ def generic_blocked_attention_interface(
                     comp_ctx_lengths=comp_ctx_lengths,
                     batch_index=batch_index,
                     position_ids=position_ids,
-                    block_table=block_table,
-                    slot_id=slot_id,
                     sliding_window=sliding_window,
                 )
 
