91f864f30d1f6869f1c506d07690db758a68531c
Amar <amarshar@qti.qualcomm.com>
2026-08-26 12:21:56 +0530

    added gpt tiny model in test
    Signed-off-by: Amar <amarshar@qti.qualcomm.com>

--- tests/weight_free/_helpers.py
+++ tests/weight_free/_helpers.py
@@ -45,7 +45,7 @@ WEIGHT_FREE_CAUSAL_LM_MODEL_IDS = {
     "glm4_moe": "tiny-random/glm-4-moe",
     "gpt2": "hf-internal-testing/tiny-random-GPT2LMHeadModel",
     "gpt_bigcode": "hf-internal-testing/tiny-random-GPTBigCodeForCausalLM",
-    "gpt_oss": "tiny-random/gpt-oss-bf16",
+    "gpt_oss": "tiny-random/gpt-oss-mxfp4",
     "gptj": "hf-internal-testing/tiny-random-GPTJForCausalLM",
     "granite": "hf-internal-testing/tiny-random-GraniteForCausalLM",
     "granitemoe": "ibm-granite/granite-3.1-1b-a400m-instruct",
