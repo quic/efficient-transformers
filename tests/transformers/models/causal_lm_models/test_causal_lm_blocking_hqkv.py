70f047692c201f6cd001c59437b3e6dabb538b53
Aditya Dhananjay Jadhav <your-email@qualcomm.com>
2026-09-10 10:59:59 -0700

    Unit Test:Replace HQKV blocking config by KV blocking
    Signed-off-by: Aditya Dhananjay Jadhav <your-email@qualcomm.com>

--- tests/transformers/models/causal_lm_models/test_causal_lm_blocking_hqkv.py
+++ tests/transformers/models/causal_lm_models/test_causal_lm_blocking_hqkv.py
@@ -57,13 +57,11 @@ def test_full_causal_all_blocking_pytorch_vs_kv_vs_ort_vs_ai100(model_name, bloc
         num_devices=4,
     )
 
-    # head qkv_paged_attention blocking
+    # kv_paged_attention blocking
     qaic_config = dict(
-        enable_blocking=True,
-        blocking_mode="hqkv_paged",
-        head_block_size=HEAD_BLOCK_SIZE,
+        blocking_mode="kv",
+        paged_attention=True,
         num_kv_blocks=NUM_KV_BLOCKS,
-        num_q_blocks=NUM_Q_BLOCKS,
     )
     check_causal_lm_pytorch_vs_kv_vs_ort_vs_ai100(
         model_name=model_name, qaic_config=qaic_config, manual_cleanup=manual_cleanup, num_devices=4
@@ -85,13 +83,11 @@ def test_few_causal_all_blocking_pytorch_vs_kv_vs_ort_vs_ai100(model_name, block
         manual_cleanup=manual_cleanup,
     )
 
-    # head qkv_paged_attention blocking
+    # kv_paged_attention blocking
     qaic_config = dict(
-        enable_blocking=True,
-        blocking_mode="hqkv_paged",
-        head_block_size=HEAD_BLOCK_SIZE,
+        blocking_mode="kv",
+        paged_attention=True,
         num_kv_blocks=NUM_KV_BLOCKS,
-        num_q_blocks=NUM_Q_BLOCKS,
     )
     check_causal_lm_pytorch_vs_kv_vs_ort_vs_ai100(
         model_name=model_name, qaic_config=qaic_config, n_layer=n_layer, manual_cleanup=manual_cleanup
@@ -122,13 +118,11 @@ def test_dummy_causal_all_blocking_pytorch_vs_kv_vs_ort_vs_ai100(model_name, blo
         manual_cleanup=manual_cleanup,
     )
 
-    # head qkv_paged_attention blocking
+    # kv_paged_attention blocking
     qaic_config = dict(
-        enable_blocking=True,
-        blocking_mode="hqkv_paged",
-        head_block_size=HEAD_BLOCK_SIZE,
+        blocking_mode="kv",
+        paged_attention=True,
         num_kv_blocks=NUM_KV_BLOCKS,
-        num_q_blocks=NUM_Q_BLOCKS,
     )
     check_causal_lm_pytorch_vs_kv_vs_ort_vs_ai100(
         model_name=model_name, qaic_config=qaic_config, n_layer=n_layer, config=hf_config, manual_cleanup=manual_cleanup
@@ -150,13 +144,11 @@ def test_full_causal_all_blocking_pytorch_vs_kv_vs_ort_vs_ai100_CB(model_name, b
         num_devices=4,
     )
 
-    # head qkv_paged_attention blocking
+    # kv_paged_attention blocking
     qaic_config = dict(
-        enable_blocking=True,
-        blocking_mode="hqkv_paged",
-        head_block_size=HEAD_BLOCK_SIZE,
+        blocking_mode="kv",
+        paged_attention=True,
         num_kv_blocks=NUM_KV_BLOCKS,
-        num_q_blocks=NUM_Q_BLOCKS,
     )
     check_causal_lm_pytorch_vs_kv_vs_ort_vs_ai100(
         model_name=model_name,
@@ -183,13 +175,11 @@ def test_few_causal_all_blocking_pytorch_vs_kv_vs_ort_vs_ai100_CB(model_name, bl
         continuous_batching=True,
     )
 
-    # head qkv_paged_attention blocking
+    # kv_paged_attention blocking
     qaic_config = dict(
-        enable_blocking=True,
-        blocking_mode="hqkv_paged",
-        head_block_size=HEAD_BLOCK_SIZE,
+        blocking_mode="kv",
+        paged_attention=True,
         num_kv_blocks=NUM_KV_BLOCKS,
-        num_q_blocks=NUM_Q_BLOCKS,
     )
     check_causal_lm_pytorch_vs_kv_vs_ort_vs_ai100(
         model_name=model_name,
@@ -225,13 +215,11 @@ def test_dummy_causal_all_blocking_pytorch_vs_kv_vs_ort_vs_ai100_CB(model_name, 
         continuous_batching=True,
     )
 
-    # head qkv_paged_attention blocking
+    # kv_paged_attention blocking
     qaic_config = dict(
-        enable_blocking=True,
-        blocking_mode="hqkv_paged",
-        head_block_size=HEAD_BLOCK_SIZE,
+        blocking_mode="kv",
+        paged_attention=True,
         num_kv_blocks=NUM_KV_BLOCKS,
-        num_q_blocks=NUM_Q_BLOCKS,
     )
     check_causal_lm_pytorch_vs_kv_vs_ort_vs_ai100(
         model_name=model_name,
